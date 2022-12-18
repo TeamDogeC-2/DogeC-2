d19119450ce3b11a36eb024103711366d3881d30
yerim <pppp5097@naver.com>
2022-12-18 20:31:37 +0900

    - init

---
+++ StudentSoup/src/main/frontend/src/App.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {useEffect, useState} from "react";
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState([]);
+
+  useEffect(() => {
+      axios.get("/hello")
+          .then(response => setMessage(response.data))
+          .catch(error => console.log(error))
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <ul>
+          {message.map((text, index) => <li key={`${index}-${text}`}>{text}</li>)}
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/StudentSoupApplication.java
@@ -0,0 +1,13 @@
+package ProjectDoge.StudentSoup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSoupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSoupApplication.class, args);
+	}
+
+}
