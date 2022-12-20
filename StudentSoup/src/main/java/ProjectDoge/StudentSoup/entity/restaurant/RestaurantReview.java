d4cde36e49683b3a072647a610f4c03adb14a91c
Bombo <bombo96@chungwoon.ac.kr>
2022-12-20 16:00:15 +0900

    - Feat : 음식점 리뷰 엔티티 추가

---
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/entity/restaurant/RestaurantReview.java
@@ -0,0 +1,75 @@
+package ProjectDoge.StudentSoup.entity.restaurant;
+
+import ProjectDoge.StudentSoup.entity.file.File;
+import ProjectDoge.StudentSoup.entity.member.Member;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "RESTAURANT_REVIEW")
+@Getter
+@Setter
+public class RestaurantReview {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "RESTAURANT_ID")
+    private Restaurant restaurant;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @Column(name = "WRITER_NICKNAME")
+    public String nickname;  // Member
+
+    private String content;
+
+    private String menuName;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "IMAGE_FILE_ID")
+    private File file;
+
+    private int likedCount;
+
+    private float starLiked;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date writeDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateDate;
+
+    //== 연관관계 메서드 ==//
+    public void setRestaurant(Restaurant restaurant){
+        this.restaurant = restaurant;
+        restaurant.getRestaurantReviews().add(this);
+    }
+
+    public void setMember(Member member){
+        this.member = member;
+        member.getRestaurantReviews().add(this);
+    }
+
+    //== 생성 메서드 ==//
+    public RestaurantReview createTestRestaurantReview(){
+        RestaurantReview restaurantReview = new RestaurantReview();
+        restaurantReview.setContent("레스토랑 리뷰 내용");
+        restaurantReview.setLikedCount(0);
+        restaurantReview.setStarLiked(0);
+        restaurantReview.setWriteDate(Timestamp.valueOf(LocalDateTime.now()));
+        restaurantReview.setUpdateDate(Timestamp.valueOf(LocalDateTime.now().plusHours(1)));
+
+        return restaurantReview;
+    }
+
+
+}
