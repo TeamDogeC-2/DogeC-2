20de96ed2b2877cd939bb26d2c30a6c89992f45e
Bombo <bombo96@chungwoon.ac.kr>
2022-12-20 16:10:15 +0900

    - Feat : 음식점 저장 DTO 추가

---
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/dto/restaurant/RestaurantFormDto.java
@@ -0,0 +1,58 @@
+package ProjectDoge.StudentSoup.dto.restaurant;
+
+import ProjectDoge.StudentSoup.entity.file.File;
+import ProjectDoge.StudentSoup.entity.restaurant.Restaurant;
+import ProjectDoge.StudentSoup.entity.restaurant.RestaurantCategory;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+public class RestaurantFormDto {
+    @NotEmpty(message = "레스토랑 입력은 필수입니다.")
+    private String name;
+
+    private String address;
+
+    private RestaurantCategory restaurantCategory;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    @NotNull(message = "시작시간 입력은 필수입니다.")
+    private LocalTime startTime;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    @NotNull(message = "종료시간 입력은 필수입니다.")
+    private LocalTime endTime;
+
+    private Long school;
+
+    private String coordinate;
+
+    private File file;
+
+    @NotEmpty(message = "가게번호 입력은 필수 입니다.")
+    private String tel;
+
+    private String tag;
+
+    private String detail;
+
+    //== 생성 메서드 ==//
+    public void setRestaurant(Restaurant restaurant){
+        this.setName(restaurant.getName());
+        this.setAddress(restaurant.getAddress());
+        this.setRestaurantCategory(restaurant.getRestaurantCategory());
+        this.setStartTime(restaurant.getStartTime());
+        this.setEndTime(restaurant.getEndTime());
+        this.setCoordinate(restaurant.getCoordinate());
+        this.setFile(restaurant.getFile());
+        this.setTel(restaurant.getTel());
+        this.setTag(restaurant.getTag());
+        this.setDetail(restaurant.getDetail());
+    }
+}
