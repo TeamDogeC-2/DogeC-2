26010da8feaf06537214790eb1f0e98791f9faa8
eunsung <esk114477@gmail.com>
2022-12-21 21:20:20 +0900

    -design: mainpage backgroundImage 추가

--- StudentSoup/src/main/frontend/src/pages/home/home.tsx
+++ StudentSoup/src/main/frontend/src/pages/home/home.tsx
@@ -3,9 +3,11 @@ import MainSearch from '../../components/mainsearch/MainSearch'
 
 const home = () => {
   return (
-    <div className='w-full h-full bg-cyan-300 z-50'>
+    <div className='w-full h-full bg-[url("./img/mainlogo.jpg")] bg-cover z-50'>
+      <div className='w-full h-full bg-gradient-to-t from-[rgba(0,0,0,0.35)] to-[rgba(0,0,0,0.1)] z-[51]'>
         <Navbar/>
         <MainSearch/>
+      </div>
     </div>
   )
 }
