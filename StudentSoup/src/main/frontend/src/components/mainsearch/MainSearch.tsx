e6ea014735d977904f26b4c2b79e20c75b1d9bb8
eunsung <esk114477@gmail.com>
2022-12-21 16:10:02 +0900

    -design : mainpage UI tailwindcss로 변경

--- StudentSoup/src/main/frontend/src/components/mainsearch/MainSearch.tsx
+++ StudentSoup/src/main/frontend/src/components/mainsearch/MainSearch.tsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import './mainsearch.css'
-
 const MainSearch = () => {
   return (
-    <div className='main'>
-      <div className='mainBody'>
-        <span className='mainTextTop'>
+    <div className='w-full h-[calc(100vh-88px)] bg-cyan-300 flex flex-col justify-center items-center'>
+      <div className='flex flex-col text-center relative bottom-[88px]'>
+        <span className='text-[45px] fw-400 leading-[59px] text-white'>
           대학생을 위한
         </span>
-        <span className='mainTextBottom'>
+        <span className='text-[65px] fw-400 leading-[93px] text-white drop-shadow-[0_3px_10px_rgba(0,0,0,0.25)]'>
           대학 주변 맛집 추천
         </span>
-        <div className='searchbar'>
-          <span className='searchIcon'>
+        <div className='mt-[28px] w-full h-[60px] rounded-[5px] bg-white'>
+          <span className='mx-[16px] my-[21px]'>
             Icon
           </span>
-          <input placeholder='지역 학교 명을 입력하세요.' className='searchInput'></input>
-          <button className='searchButton'>검색</button>
+          <input placeholder='지역 학교 명을 입력하세요.' className='w-[500px] h-[58px] text-[25px] fw-400 leading-[33px] text-[#A0A0A0] border-none pl-[23px]'></input>
+          <button className='w-[94px] h-[60px] text-[25px] fw-400 leading-[33px] text-white bg-[#FF611D] border-none rounded-[5px]'>검색</button>
         </div>
       </div>
     </div>
--- StudentSoup/src/main/frontend/src/components/navbar/Navbar.tsx
+++ StudentSoup/src/main/frontend/src/components/navbar/Navbar.tsx
@@ -1,43 +1,18 @@
-import {useCallback, useEffect, useRef, useState} from 'react'
-import './navbar.css'
-import Modal from '../modal/Modal'
-
 const Navbar = () => {
-  const [isModal, setModal] = useState<Boolean>(false);
-
-  const onClickToggleModal = useCallback(() => {
-    setModal(!isModal);
-  }, [isModal])
-  
-  const modalRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const clickOutside = (e: any) => {
-      if(isModal && !modalRef.current?.contains(e.target)){
-        setModal(false)
-      }
-    }
-    document.addEventListener('mousedown', clickOutside)
-    return () => {
-      document.removeEventListener('mousedown', clickOutside)
-    }
-  }, [isModal])
-
   return (
-    <div className='navbar'>
-      <div className='topLeft'>
+    <div className='w-full h-[88px] items-center sticky flex justify-between bg-[#D9D9D9]'>
+      <div className='flex items-center m-5 w-[162px] h-[76px]'>
         reddit
       </div>
-      <div className='topRight'>
-        <div className='FAQcontainer'>
-          <span className='FAQ'>
+      <div className='flex items-center mr-[49px] m-5'>
+        <div className='w-[65px] h-[60px] mr-[21px] flex justify-center items-center'>
+          <span className='text-[25px] leading-[35px] fw-400'>
             FAQ
           </span>
         </div>
-        <button className='Login' onClick={onClickToggleModal}>
+        <button className='w-[93px] h-[40px] bg-[#FF4F14] border-[1.2px] border-[#FF4D14] rounded-[41px] hover:cursor-pointer text-[20px]'>
           Login
         </button>
-        {isModal === true ? <Modal/> : null}
       </div>
     </div>
   )
