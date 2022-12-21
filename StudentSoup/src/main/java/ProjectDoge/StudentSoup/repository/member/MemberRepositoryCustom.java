7cdd230beb4ad11838a7e17a19698a35b0e22c7e
Bombo <bombo96@chungwoon.ac.kr>
2022-12-21 17:53:23 +0900

    - feat : 회원 쿼리문 파일 추가

---
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/member/MemberRepositoryCustom.java
@@ -0,0 +1,16 @@
+package ProjectDoge.StudentSoup.repository.member;
+
+import ProjectDoge.StudentSoup.dto.member.MemberSearch;
+import ProjectDoge.StudentSoup.entity.member.Member;
+
+import java.util.List;
+
+public interface MemberRepositoryCustom {
+
+    Member findById(String id);
+    List<Member> findByName(String name);
+    List<Member> findByNameAndSchool_SchoolName(String name, String schoolName);
+    List<Member> search(MemberSearch memberSearch);
+    Member findByEmail(String email);
+    Member findByEmailAndId(String email, String id);
+}
