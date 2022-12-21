2e7c5296ad5825501850d9d0c711c14a87c82f39
Bombo <bombo96@chungwoon.ac.kr>
2022-12-21 19:06:12 +0900

    - feat : 학교 등록 및 검증 테스트 추가

---
+++ StudentSoup/src/test/java/ProjectDoge/StudentSoup/school/SchoolEntityTest.java
@@ -0,0 +1,56 @@
+package ProjectDoge.StudentSoup.school;
+
+import ProjectDoge.StudentSoup.entity.school.School;
+import ProjectDoge.StudentSoup.exception.school.SchoolValidationException;
+import ProjectDoge.StudentSoup.service.SchoolService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+@Transactional
+public class SchoolEntityTest {
+    @PersistenceContext
+    EntityManager em;
+
+    @Autowired
+    private SchoolService schoolService;
+
+    @Test
+    void 학교등록테스트() throws Exception {
+        //given
+        School school = createSchool();
+        //when
+        Long schoolId = schoolService.join(school);
+        //then
+        assertThat(school).isEqualTo(schoolService.findOne(school.getId()));
+    }
+
+    @Test
+    void 학교중복검증테스트() throws Exception {
+        //given
+        School school1 = createSchool();
+        School school2 = createSchool();
+        //when
+        schoolService.join(school1);
+        //then
+        assertThatThrownBy(()-> schoolService.join(school2))
+                .isInstanceOf(SchoolValidationException.class);
+    }
+
+    private School createSchool(){
+        School school = new School();
+        school.setSchoolName("테스트 학교 추가");
+        school.setSchoolCoordinate("테스트 학교 좌표");
+
+        return school;
+    }
+}
