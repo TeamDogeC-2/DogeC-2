871dd61490e6fa166bfb5dda43f2f94f3315479f
eunsung <esk114477@gmail.com>
2022-12-22 17:21:19 +0900

    -design: register1 page UI 추가

--- StudentSoup/src/main/frontend/src/App.tsx
+++ StudentSoup/src/main/frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
 import Home from './pages/home/home';
+import Register1 from './pages/register/Register1';
 
 function App() {
   return (
-    <Home/>
+    <Register1/>
   );
 }
 
