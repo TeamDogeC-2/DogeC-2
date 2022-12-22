be14eb79d699a76039c1a39a0604172052e681de
Bombo <bombo96@chungwoon.ac.kr>
2022-12-22 18:42:01 +0900

    - refactor : 코드 수직거리 클린코딩

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/service/MemberService.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/service/MemberService.java
@@ -1,9 +1,7 @@
 package ProjectDoge.StudentSoup.service;
 
-import ProjectDoge.StudentSoup.dto.member.MemberFormADto;
 import ProjectDoge.StudentSoup.dto.member.MemberFormBDto;
 import ProjectDoge.StudentSoup.entity.member.Member;
-import ProjectDoge.StudentSoup.entity.member.MemberClassification;
 import ProjectDoge.StudentSoup.entity.school.Department;
 import ProjectDoge.StudentSoup.entity.school.School;
 import ProjectDoge.StudentSoup.exception.member.MemberValidationException;
@@ -29,13 +27,19 @@ public class MemberService {
     @Transactional
     public Long join(MemberFormBDto dto){
         log.info("회원 생성 메서드가 실행되었습니다.");
+
         School school = getMemberDtoSchool(dto.getSchoolId());
         Department department = getMemberDtoDepartment(dto.getDepartmentId());
+
         log.info("회원의 학교는 : {}, 회원의 학과는 : {}", school.getSchoolName(), department.getDepartmentName());
+
         validateDuplicateMember(dto);
+
         Member member = new Member().createMember(dto, school, department);
         memberRepository.save(member);
+
         log.info("회원이 생성되었습니다. [{}][{}] ",member.getId(), member.getNickname());
+
         return member.getMemberId();
     }
 
@@ -56,6 +60,7 @@ public class MemberService {
 
     public void validateDuplicateMemberNickname(String nickname){
         log.info("회원 닉네임 중복 검증 메소드가 실행되었습니다.");
+
         Member findMember = memberRepository.findByNickname(nickname);
         if(findMember != null){
             log.info("회원 닉네임이 중복 예외가 발생했습니다.");
@@ -66,6 +71,7 @@ public class MemberService {
 
     public void validateDuplicateMemberEmail(String email){
         log.info("회원 이메일 중복 검증 메소드가 실행되었습니다.");
+
         Member findMember = memberRepository.findByEmail(email);
         if(findMember != null){
             log.info("회원 이메일 중복 예외가 발생했습니다.");
@@ -76,7 +82,7 @@ public class MemberService {
 
     public void validateDuplicateMemberId(String memberId) {
         log.info("회원 아이디 중복 검증 메소드가 실행되었습니다.");
-        // EXCEPTION
+
         Member findMember = memberRepository.findById(memberId);
         if (findMember != null) {
             log.info("회원이 존재하는 예외가 발생했습니다.");
