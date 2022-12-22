b6458a91f1ffb0d9e3e09ca742383b6277ee1779
Bombo <bombo96@chungwoon.ac.kr>
2022-12-22 19:20:39 +0900

    - feat : 회원 더미데이터 자동 생성 추가

---
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/init/TestDataInit.java
@@ -0,0 +1,117 @@
+package ProjectDoge.StudentSoup.init;
+
+import ProjectDoge.StudentSoup.dto.department.DepartmentFormDto;
+import ProjectDoge.StudentSoup.dto.member.MemberFormBDto;
+import ProjectDoge.StudentSoup.entity.member.GenderType;
+import ProjectDoge.StudentSoup.entity.school.Department;
+import ProjectDoge.StudentSoup.entity.school.School;
+import ProjectDoge.StudentSoup.repository.department.DepartmentRepository;
+import ProjectDoge.StudentSoup.repository.school.SchoolRepository;
+import ProjectDoge.StudentSoup.service.DepartmentService;
+import ProjectDoge.StudentSoup.service.MemberService;
+import ProjectDoge.StudentSoup.service.SchoolService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+    private final MemberService memberService;
+    private final SchoolRepository schoolRepository;
+    private final SchoolService schoolService;
+    private final DepartmentRepository departmentRepository;
+    private final DepartmentService departmentService;
+
+
+    @PostConstruct
+    public void init(){
+        initSchoolAndDepartment();
+        initMember();
+    }
+
+    private void initSchoolAndDepartment(){
+        initSchool();
+        initDepartment();
+    }
+
+    private void initSchool(){
+        School school1 = new School();
+        school1.setSchoolName("테스트1 학교");
+        school1.setSchoolCoordinate("테스트1 좌표");
+
+        School school2 = new School();
+        school2.setSchoolName("테스트2 학교");
+        school2.setSchoolCoordinate("테스트2 좌표");
+        schoolService.join(school1);
+        schoolService.join(school2);
+    }
+
+    private void initDepartment(){
+        School school1 = schoolRepository.findBySchoolName("테스트1 학교");
+        School school2 = schoolRepository.findBySchoolName("테스트2 학교");
+        DepartmentFormDto dto1 = new DepartmentFormDto();
+        dto1.setDepartmentName("테스트1 학과1");
+        dto1.setSchoolId(school1.getId());
+
+        DepartmentFormDto dto2 = new DepartmentFormDto();
+        dto2.setDepartmentName("테스트1 학과2");
+        dto2.setSchoolId(school1.getId());
+
+        DepartmentFormDto dto3 = new DepartmentFormDto();
+        dto3.setDepartmentName("테스트2 학과1");
+        dto3.setSchoolId(school2.getId());
+
+        DepartmentFormDto dto4 = new DepartmentFormDto();
+        dto4.setDepartmentName("테스트2 학과2");
+        dto4.setSchoolId(school2.getId());
+
+        departmentService.join(school1.getId(), dto1);
+        departmentService.join(school1.getId(), dto2);
+        departmentService.join(school2.getId(), dto3);
+        departmentService.join(school2.getId(), dto4);
+    }
+
+    private void initMember(){
+        School school1 = schoolRepository.findBySchoolName("테스트1 학교");
+        School school2 = schoolRepository.findBySchoolName("테스트2 학교");
+
+        List<Department> departments1 = departmentRepository.findBySchool_Id(school1.getId());
+        List<Department> departments2 = departmentRepository.findBySchool_Id(school2.getId());
+
+        MemberFormBDto dto1 = createMemberFormDto("test1", "test123!", "테스트1", "test1@naver.com",
+                GenderType.MAN, school1.getId(), departments1.get(0).getId());
+        MemberFormBDto dto2 = createMemberFormDto("test2", "test123!", "테스트2", "test2@naver.com",
+                GenderType.MAN, school1.getId(), departments1.get(0).getId());
+        MemberFormBDto dto3 = createMemberFormDto("test3", "test123!", "테스트3", "test3@naver.com",
+                GenderType.MAN, school1.getId(), departments1.get(1).getId());
+        MemberFormBDto dto4 = createMemberFormDto("test4", "test123!", "테스트4", "test4@naver.com",
+                GenderType.WOMAN, school2.getId(), departments2.get(0).getId());
+        MemberFormBDto dto5 = createMemberFormDto("test5", "test123!", "테스트5", "test5@naver.com",
+                GenderType.WOMAN, school2.getId(), departments2.get(1).getId());
+        MemberFormBDto dto6 = createMemberFormDto("test6", "test123!", "테스트6", "test6@naver.com",
+                GenderType.WOMAN, school2.getId(), departments2.get(1).getId());
+
+        memberService.join(dto1);
+        memberService.join(dto2);
+        memberService.join(dto3);
+        memberService.join(dto4);
+        memberService.join(dto5);
+        memberService.join(dto6);
+    }
+
+    private MemberFormBDto createMemberFormDto(String id, String pwd, String nickname, String email,
+                                               GenderType gender, Long schoolId, Long departmentId){
+        MemberFormBDto dto = new MemberFormBDto();
+        dto.setId(id);
+        dto.setPwd(pwd);
+        dto.setNickname(nickname);
+        dto.setEmail(email);
+        dto.setGender(gender);
+        dto.setSchoolId(schoolId);
+        dto.setDepartmentId(departmentId);
+        return dto;
+    }
+}
