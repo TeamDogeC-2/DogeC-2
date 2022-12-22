a37b6d47808457c351830f87ec4fbb53fbd7481c
Bombo <bombo96@chungwoon.ac.kr>
2022-12-22 15:11:07 +0900

    - feat : 로그인 시 전달 객체 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/dto/member/MemberDto.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/dto/member/MemberDto.java
@@ -1,17 +1,16 @@
 package ProjectDoge.StudentSoup.dto.member;
 
 import ProjectDoge.StudentSoup.entity.member.GenderType;
+import ProjectDoge.StudentSoup.entity.member.Member;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
-import javax.validation.constraints.Email;
-
 
 @ToString
 @Getter
 @Setter
 public class MemberDto {
+    private Long memberId;
 
     private String id;
 
@@ -19,12 +18,24 @@ public class MemberDto {
 
     private GenderType gender;
 
-    @Email(message = "이메일 입력은 필수입니다.")
     private String email;
 
-    private Long school;
+    private Long schoolId;
     private String schoolName;
 
-    private Long department;
+    private Long departmentId;
     private String departmentName;
+
+    private MemberDto getLoginMemberDto(Member member){
+        this.memberId = member.getMemberId();
+        this.schoolId = member.getSchool().getId();
+        this.schoolName = member.getSchool().getSchoolName();
+        this.departmentId = member.getDepartment().getId();
+        this.departmentName = member.getDepartment().getDepartmentName();
+        this.id = member.getId();
+        this.nickname = member.getNickname();
+        this.gender = member.getGender();
+        this.email = member.getEmail();
+        return this;
+    }
 }
