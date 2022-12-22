2a518e45007ace91d10767a853054feb5ae91b2a
eunsung <esk114477@gmail.com>
2022-12-22 13:35:58 +0900

    -design: navbar hover시 흰색 줄 나오도록 수정

--- StudentSoup/src/main/frontend/src/pages/home/home.tsx
+++ StudentSoup/src/main/frontend/src/pages/home/home.tsx
@@ -4,7 +4,7 @@ import MainSearch from '../../components/mainsearch/MainSearch'
 const home = () => {
   return (
     <div className='w-full h-full bg-[url("./img/mainlogo.jpg")] bg-cover z-50'>
-      <div className='w-full h-full bg-gradient-to-t from-[rgba(0,0,0,0.35)] to-[rgba(0,0,0,0.1)] z-[51]'>
+      <div className='w-full h-full bg-gradient-to-t from-[rgba(0,0,0,0.45)] to-[rgba(0,0,0,0.1)] z-[51]'>
         <Navbar/>
         <MainSearch/>
       </div>
