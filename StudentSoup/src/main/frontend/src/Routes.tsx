a035fe169ddfe0262475fa3c6b966fa64744c92a
yeeeerim <pppp5097@naver.com>
2022-12-24 01:31:51 +0900

    - fix : 병합 후 페이지 라우팅 누락된 부분 추가

--- StudentSoup/src/main/frontend/src/Routes.tsx
+++ StudentSoup/src/main/frontend/src/Routes.tsx
@@ -1,11 +1,13 @@
 import { Switch, Route } from "react-router-dom";
 import Err404 from "./components/err404/err404";
 import homeComponent from "./components/home/homeComponent";
+import Register from "./components/register/register1";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={homeComponent} />
+      <Route exact path="/register/1" component={Register} />
       <Route component={Err404} />
     </Switch>
   );
