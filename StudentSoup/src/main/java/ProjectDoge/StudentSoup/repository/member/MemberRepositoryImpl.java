b4cb9fb6480b2a11990536794a4b33ce2c01fe08
Bombo <bombo96@chungwoon.ac.kr>
2022-12-23 18:46:51 +0900

    - refactor : 회원 아이디로 회원 검색 시 Optional로 리턴 변경

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/member/MemberRepositoryImpl.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/member/MemberRepositoryImpl.java
@@ -4,10 +4,10 @@ import ProjectDoge.StudentSoup.dto.member.MemberSearch;
 import ProjectDoge.StudentSoup.entity.member.Member;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Optional;
 
 import static ProjectDoge.StudentSoup.entity.member.QMember.member;
 import static ProjectDoge.StudentSoup.entity.school.QDepartment.department;
@@ -21,12 +21,13 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
     }
 
     @Override
-    public Member findById(String id) {
-        JPQLQuery<Member> query = queryFactory.select(member)
+    public Optional<Member> findById(String id) {
+        Member query = queryFactory.select(member)
                 .from(member)
-                .where(member.id.eq(id));
+                .where(member.id.eq(id))
+                .fetchOne();
 
-        return query.fetchOne();
+        return Optional.ofNullable(query);
     }
 
     @Override
