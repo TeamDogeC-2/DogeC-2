f8102224c4511a7edd44312a2866990f901de5aa
yeeeerim <pppp5097@naver.com>
2022-12-24 15:45:00 +0900

    - fix : 병합 후 폴더 구조 조정 및 라우터 설정

--- StudentSoup/src/main/frontend/src/Routes.tsx
+++ StudentSoup/src/main/frontend/src/Routes.tsx
@@ -1,6 +1,7 @@
 import { Switch, Route } from "react-router-dom";
 import Err404 from "./components/err404/err404";
 import homeComponent from "./components/home/homeComponent";
+import Login from "./components/login/login";
 import Register from "./components/register/register1";
 
 const Routes = () => {
@@ -8,6 +9,7 @@ const Routes = () => {
     <Switch>
       <Route exact path="/" component={homeComponent} />
       <Route exact path="/register/1" component={Register} />
+      <Route exact path="/login" component={Login} />
       <Route component={Err404} />
     </Switch>
   );
--- StudentSoup/src/main/frontend/src/components/login/loginform.tsx
+++ StudentSoup/src/main/frontend/src/components/login/loginform.tsx
@@ -1,74 +1,98 @@
-import React  from 'react';
-import axios from 'axios';
-import Reddit from '../../img/Reddit - png.svg'
+import React from "react";
+// import Reddit from "../../img/Reddit - png.svg";
 
 const onClickLogin = () => {
-    console.log('click login')
-}
+  console.log("click login");
+};
 
 const onClickSignup = () => {
-    console.log('click sign up')
-}
+  console.log("click sign up");
+};
 
 function LoginForm() {
-    return(
-        //navber 추가하기
-        <div className='flex flex-col justify-center items-center'>
-            <div className='mt-[88px] flex justify-center'>
-                <img src={Reddit} />
-            </div>
-            <div className='flex flex-col justify-center items-center'>
-                <div className='flex flex-col justify-center'>
-                    <input
-                        id="id"
-                        name="id"
-                        placeholder="아이디 또는 이메일을 입력해주세요"
-                        className='w-[558px] h-[60px] mt-[20px] p-[15px] border border-1 border-[#BCBCBC] rounded-[5px] text-[16px] text-[#A0A0A0]'
-                    />
-                    <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        placeholder="비밀번호를 입력해주세요"
-                        className='w-[558px] h-[60px] mt-[10px] p-[15px] border border-1 border-[#BCBCBC] rounded-[5px] text-[16px] text-[#A0A0A0]'
-                    />
-                </div>
-                <div className='w-[558px] mt-[25px] flex justify-between items-center'>
-                    <div className='flex items-center'>
-                        <input type='checkbox' id='saveId' name='saveId' className='w-[29px] h-[29px] border-none rounded-full cursor-pointer accent-[#FF611D]' checked/>
-                        <label htmlFor="saveID" className='ml-[13px] text-[#3E3E3E]'>아이디 저장</label>
-                    </div>
-                    <div>
-                        <a className='text-[#3E3E3E] cursor-pointer'>아이디/비밀번호 찾기</a>
-                    </div>
-                </div>
-            </div>
-            <div className='mt-[58px] flex flex-col justify-center items-center'>
-                <button
-                    type='button'
-                    onClick={onClickLogin}
-                    className='w-[558px] h-[60px] rounded-[20px] text-[20px] text-white bg-[#FF611D]'>로그인
-                </button>
-                <button
-                    type='button'
-                    onClick={onClickSignup}
-                    className='w-[558px] h-[60px] mt-[10px] rounded-[20px] text-[20px] border border-[#FF611D] text-[#FF611D] bg-white'>회원가입
-                </button>
-            </div>
-            <div className='mt-[58px] flex flex-col justify-center items-center'>
-                <div className='flex justify-center items-center'>
-                    <span className='w-[54.5px] h-0 border bg-[#C6C6C6]'></span>
-                    <label className='px-[34px] text-[16px] text-[#A0A0A0]'>간편 로그인/가입</label>
-                    <span className='w-[54.5px] h-0 border bg-[#C6C6C6]'></span>
-                </div>
-                <div className='mt-[37px] space-x-[31px]'>
-                    <button name='sns1' className='w-[62px] h-[62px] rounded-full bg-[#D9D9D9]'></button>
-                    <button name='sns2' className='w-[62px] h-[62px] rounded-full bg-[#D9D9D9]'></button>
-                    <button name='sns3' className='w-[62px] h-[62px] rounded-full bg-[#D9D9D9]'></button>
-                </div>
-            </div>
+  return (
+    //navber 추가하기
+    <div className="flex flex-col justify-center items-center">
+      <div className="mt-[88px] flex justify-center">
+        {/* <img src={Reddit} alt="" /> */}
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center">
+          <input
+            id="id"
+            name="id"
+            placeholder="아이디 또는 이메일을 입력해주세요"
+            className="w-[558px] h-[60px] mt-[20px] p-[15px] border border-1 border-[#BCBCBC] rounded-[5px] text-[16px] text-[#A0A0A0]"
+          />
+          <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="비밀번호를 입력해주세요"
+            className="w-[558px] h-[60px] mt-[10px] p-[15px] border border-1 border-[#BCBCBC] rounded-[5px] text-[16px] text-[#A0A0A0]"
+          />
         </div>
-    );
+        <div className="w-[558px] mt-[25px] flex justify-between items-center">
+          <div className="flex items-center">
+            <input
+              type="checkbox"
+              id="saveId"
+              name="saveId"
+              className="w-[29px] h-[29px] border-none rounded-full cursor-pointer accent-[#FF611D]"
+              checked
+            />
+            <label htmlFor="saveID" className="ml-[13px] text-[#3E3E3E]">
+              아이디 저장
+            </label>
+          </div>
+          <div>
+            <a className="text-[#3E3E3E] cursor-pointer">
+              아이디/비밀번호 찾기
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="mt-[58px] flex flex-col justify-center items-center">
+        <button
+          type="button"
+          onClick={onClickLogin}
+          className="w-[558px] h-[60px] rounded-[20px] text-[20px] text-white bg-[#FF611D]"
+        >
+          로그인
+        </button>
+        <button
+          type="button"
+          onClick={onClickSignup}
+          className="w-[558px] h-[60px] mt-[10px] rounded-[20px] text-[20px] border border-[#FF611D] text-[#FF611D] bg-white"
+        >
+          회원가입
+        </button>
+      </div>
+      <div className="mt-[58px] flex flex-col justify-center items-center">
+        <div className="flex justify-center items-center">
+          <span className="w-[54.5px] h-0 border bg-[#C6C6C6]"></span>
+          <label className="px-[34px] text-[16px] text-[#A0A0A0]">
+            간편 로그인/가입
+          </label>
+          <span className="w-[54.5px] h-0 border bg-[#C6C6C6]"></span>
+        </div>
+        <div className="mt-[37px] space-x-[31px]">
+          <button
+            name="sns1"
+            className="w-[62px] h-[62px] rounded-full bg-[#D9D9D9]"
+          ></button>
+          <button
+            name="sns2"
+            className="w-[62px] h-[62px] rounded-full bg-[#D9D9D9]"
+          ></button>
+          <button
+            name="sns3"
+            className="w-[62px] h-[62px] rounded-full bg-[#D9D9D9]"
+          ></button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginForm;
