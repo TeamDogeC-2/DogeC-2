defb707cd569d25e4df22e2699e190ff81f2a57a
yeeeerim <pppp5097@naver.com>
2022-12-24 18:51:34 +0900

    - feat : 페이지 간 이동 추가

--- StudentSoup/src/main/frontend/src/Routes.tsx
+++ StudentSoup/src/main/frontend/src/Routes.tsx
@@ -3,12 +3,14 @@ import Err404 from "./components/err404/err404";
 import homeComponent from "./components/home/homeComponent";
 import Login from "./components/login/login";
 import Register from "./components/register/register1";
+import Register3 from "./components/register/register3";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={homeComponent} />
       <Route exact path="/register/1" component={Register} />
+      <Route exact path="/register/3" component={Register3} />
       <Route exact path="/login" component={Login} />
       <Route component={Err404} />
     </Switch>
