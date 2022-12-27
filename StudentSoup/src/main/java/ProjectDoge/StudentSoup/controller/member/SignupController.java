8d01c18afbe31576f37a0cef6d50068983c7426d
Bombo <bombo96@chungwoon.ac.kr>
2022-12-27 12:30:21 +0900

    - feat : 로그 추가 및 /registry/3 페이지 호출 시 학교 목록 전달 [GET]

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/member/SignupController.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/member/SignupController.java
@@ -1,13 +1,16 @@
 package ProjectDoge.StudentSoup.controller.member;
 
 import ProjectDoge.StudentSoup.dto.member.MemberFormADto;
+import ProjectDoge.StudentSoup.dto.school.SchoolSignUpDto;
+import ProjectDoge.StudentSoup.entity.school.School;
 import ProjectDoge.StudentSoup.service.MemberService;
+import ProjectDoge.StudentSoup.service.SchoolService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -16,11 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class SignupController {
 
     private final MemberService memberService;
+    private final SchoolService schoolService;
 
     @PostMapping("/signUp/2")
     public MemberFormADto signUpCheck(@RequestBody MemberFormADto dto){
+        log.info("signUpCheck가 호출되었습니다.");
         memberService.validateDuplicateMemberId(dto.getId());
         return dto;
+    }
+
+    @GetMapping("/signUp/3")
+    public List<SchoolSignUpDto> signUpSchoolList(){
+        log.info("signUpSchoolList 가 호출되었습니다.");
+        List<School> schools = schoolService.findAll();
+        List<SchoolSignUpDto> result = schools.stream()
+                .map(school -> new SchoolSignUpDto(school))
+                .collect(Collectors.toList());
+        log.info("불러온 학교 목록 DTO : [{}]", result);
+        return result;
     }
 
 
