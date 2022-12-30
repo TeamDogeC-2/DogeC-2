39044e516a3861490f9a1dc07f4d71717ca570e8
Bombo <bombo96@chungwoon.ac.kr>
2022-12-30 19:18:24 +0900

    - feat : 회원 이미지 업데이트 기능 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/service/MemberService.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/service/MemberService.java
@@ -7,6 +7,7 @@ import ProjectDoge.StudentSoup.entity.file.ImageFile;
 import ProjectDoge.StudentSoup.entity.member.Member;
 import ProjectDoge.StudentSoup.entity.school.Department;
 import ProjectDoge.StudentSoup.entity.school.School;
+import ProjectDoge.StudentSoup.exception.member.MemberIdNotSentException;
 import ProjectDoge.StudentSoup.exception.member.MemberNotFoundException;
 import ProjectDoge.StudentSoup.exception.member.MemberNotSamePassword;
 import ProjectDoge.StudentSoup.exception.member.MemberValidationException;
@@ -187,7 +188,32 @@ public class MemberService {
         }
     }
 
+    @Transactional
+    public MemberDto memberProfileUpdate(Long memberId, MultipartFile multipartFile){
+        log.info("멤버 프로필이미지 업데이트가 시작되었습니다.");
+        Long fileId = fileService.join(multipartFile);
+        Member member = findOne(memberId);
+        return createProfileUpdateMemberDto(fileId, member);
+    }
+
+    private MemberDto createProfileUpdateMemberDto(Long fileId, Member member) {
+        if(fileId != null){
+            ImageFile imageFile = fileService.findOne(fileId);
+            member.setImageFile(imageFile);
+            log.info("멤버 프로필이미지가 업데이트 되었습니다. fileName : [{}]", imageFile.getFileOriginalName());
+            return new MemberDto().getMemberDto(member);
+        } else {
+            log.info("전송된 프로필 이미지가 없으므로, 프로필 이미지가 업데이트되지 않았습니다.");
+            return new MemberDto().getMemberDto(member);
+        }
+    }
+
     public Member findOne(Long memberId) {
+        if(memberId == null){
+            log.info("멤버 프로필 업데이트 도중 memberId가 전송되지 않았습니다.");
+            throw new MemberIdNotSentException("memberId가 전송되지 않았습니다.");
+        }
+
         return memberRepository.findById(memberId)
                 .orElseThrow(() -> {
                     throw new MemberNotFoundException("회원을 찾지 못하였습니다.");
