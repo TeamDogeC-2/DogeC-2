395cec2f7e1aa8e79ac8a1588fe975738b541216
Bombo <bombo96@chungwoon.ac.kr>
2023-01-01 12:57:04 +0900

    - feat : 회원 정보 수정 시 비밀번호 검증 추가

---
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/member/MemberUpdateController.java
@@ -0,0 +1,28 @@
+package ProjectDoge.StudentSoup.controller.member;
+
+import ProjectDoge.StudentSoup.dto.member.MemberFormADto;
+import ProjectDoge.StudentSoup.dto.member.MemberUpdateValidationDto;
+import ProjectDoge.StudentSoup.entity.member.Member;
+import ProjectDoge.StudentSoup.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberUpdateController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/edit/{memberId}/validation")
+    public String editIdCheck(@PathVariable("memberId") Long memberId, @Validated @RequestBody MemberUpdateValidationDto dto, BindingResult bindingResult){
+        Member member = memberService.findOne(dto.getMemberId());
+        memberService.validationCoincideMemberIdPwd(member, dto.getPwd());
+
+        return "ok";
+    }
+}
