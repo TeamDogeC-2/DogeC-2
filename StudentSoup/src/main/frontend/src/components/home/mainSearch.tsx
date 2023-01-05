b046d1afc7b903bc84c00788d73c2d973f8f7eef
eunsung <esk114477@gmail.com>
2023-01-05 12:57:44 +0900

    -feat : axios 추가 및 데이터 검색 기능 추가

--- StudentSoup/src/main/frontend/src/components/home/mainSearch.tsx
+++ StudentSoup/src/main/frontend/src/components/home/mainSearch.tsx
@@ -1,7 +1,33 @@
+import axios from "axios";
+import { ChangeEvent, useEffect, useState } from "react";
+import cn from "clsx";
+
 const MainSearch = () => {
+
+  const [searchSchool, setSearchSchool] = useState<any[]>();
+  const [posts, setPosts] = useState<any[]>();
+
+  const getSchool = async () => {
+    const response = await axios.get('/home')
+    setPosts(response.data)
+  }
+
+  useEffect(() => {
+    getSchool();
+  }, [])
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.value) return setSearchSchool(posts);
+    const resultArray = posts?.filter(post => post.schoolName.includes(e.target.value))
+    setSearchSchool(resultArray)
+    console.log(e.target.value)
+    console.log(searchSchool)
+  }
+
   return (
-    <div className='w-full h-[calc(100vh-88px)] flex flex-col justify-center items-center'>
-      <div className='flex flex-col text-center relative bottom-[88px]'>
+    <div className='w-full h-[calc(100vh-88px)] flex flex-col mt-[290px] items-center'>
+      <div className={cn('flex flex-col text-center relative bottom-[88px]',
+      'after:flex after:flex-col after:text-center after:relative after:bottom-[88px]')}>
         <span className='text-[45px] fw-400 leading-[59px] text-white'>
           대학생을 위한
         </span>
@@ -12,9 +38,16 @@ const MainSearch = () => {
           <span className='mx-[16px] my-[21px]'>
             Icon
           </span>
-          <input placeholder='지역 학교 명을 입력하세요.' className='w-[500px] h-[58px] text-[25px] fw-400 leading-[33px] text-[#A0A0A0] border-none pl-[23px]'></input>
+          <input onChange={handleChange} name="text" placeholder='지역 학교 명을 입력하세요.' className='w-[500px] h-[58px] text-[25px] fw-400 leading-[33px] text-[#A0A0A0] border-none pl-[23px]'></input>
           <button className='w-[94px] h-[60px] text-[25px] fw-400 leading-[33px] text-white bg-[#FF611D] border-none rounded-[5px]'>검색</button>
         </div>
+      {searchSchool?.map((school) => (
+        <div key={school.schoolId} className="w-[654px] h-[58px] rounded-[5px] bg-white">
+          <span className="flex text-[16px] mt-[15px] ml-[20px] items-center font-medium">
+            {school.schoolName}
+          </span>
+        </div>
+      ))}
       </div>
     </div>
   )
