80664e2bb4b9148f689cac76a92dc22b5e776144
Bombo <bombo96@chungwoon.ac.kr>
2023-01-06 18:48:31 +0900

    - feat : 학교 <-> 음식점 거리좌표 서비스 로직 엔티티에서 삭제

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/entity/restaurant/Restaurant.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/entity/restaurant/Restaurant.java
@@ -128,33 +128,4 @@ public class Restaurant {
             this.likedCount -= 1;
         }
     }
-
-    // 학교로 부터 음식점까지 거리좌표 계산
-    public int getDistance(){
-        String[] schoolCoordinate = this.school.getSchoolCoordinate().split(",");
-        String[] restaurantCoordinate = this.getCoordinate().split(",");
-        double schoolLatitude = Double.parseDouble(schoolCoordinate[0]);
-        double schoolLongitude = Double.parseDouble(schoolCoordinate[1]);
-        double restaurantLatitude = Double.parseDouble(restaurantCoordinate[0]);
-        double restaurantLongitude = Double.parseDouble(restaurantCoordinate[1]);
-
-        double theta = schoolLongitude - restaurantLongitude;
-        double dist = Math.sin(degToRad(schoolLatitude)) * Math.sin(degToRad(restaurantLatitude)) + Math.cos(degToRad(schoolLatitude)) * Math.cos(degToRad(restaurantLatitude)) * Math.cos(degToRad(theta));
-
-        dist = Math.acos(dist);
-        dist = radToDeg(dist);
-        dist = dist * 60 * 1.1515;
-
-        dist *= 1609.344;
-
-        return (int)round(dist);
-    }
-
-    private double degToRad(double deg) {
-        return (deg * Math.PI / 180.0);
-    }
-
-    private double radToDeg(double rad) {
-        return (rad * 180 / Math.PI);
-    }
 }
