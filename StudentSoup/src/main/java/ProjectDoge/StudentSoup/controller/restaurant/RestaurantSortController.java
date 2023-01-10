180fd3b12dd94e95b44c4dfd157bf99a6c17680b
Bombo <bombo96@chungwoon.ac.kr>
2023-01-10 19:37:35 +0900

    - feat : 음식점 정렬 호출 컨트롤러 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/restaurant/RestaurantSortController.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/restaurant/RestaurantSortController.java
@@ -1,11 +1,13 @@
 package ProjectDoge.StudentSoup.controller.restaurant;
 
+import ProjectDoge.StudentSoup.dto.restaurant.RestaurantDto;
 import ProjectDoge.StudentSoup.dto.restaurant.RestaurantSort;
-import ProjectDoge.StudentSoup.service.restaurant.RestaurantSortService;
+import ProjectDoge.StudentSoup.service.restaurant.RestaurantCallService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -13,16 +15,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/restaurants")
 public class RestaurantSortController {
 
-    private final RestaurantSortService restaurantSortService;
+    private final RestaurantCallService restaurantCallService;
 
     /**
-     *
      * @param category
-     * @param sorted 0 normal(등록 순), 1(별점), 2(좋아요), 3(리뷰), 4(거리)
+     * @param sorted   0 normal(등록 순), 1(별점), 2(좋아요), 3(리뷰), 4(거리)
      * @return
      */
     @GetMapping("/{category}/{sorted}")
-    public String sortByRestaurants(@PathVariable("category") String category, @PathVariable("sorted") int sorted, @RequestBody RestaurantSort restaurantSort){
+    public List<RestaurantDto> sortByRestaurants(@PathVariable("category") String category,
+                                                 @PathVariable("sorted") int sorted,
+                                                 @RequestBody RestaurantSort restaurantSort) {
 
         log.info("category : [{}], sorted : [{}], memberId : [{}], schoolId : [{}]",
                 category,
@@ -30,6 +33,12 @@ public class RestaurantSortController {
                 restaurantSort.getMemberId(),
                 restaurantSort.getSchoolId());
 
-        return "ok";
+        List<RestaurantDto> dto = restaurantCallService.
+                restaurantSortedCall(restaurantSort.getSchoolId(),
+                        restaurantSort.getMemberId(),
+                        category,
+                        sorted);
+
+        return dto;
     }
 }
