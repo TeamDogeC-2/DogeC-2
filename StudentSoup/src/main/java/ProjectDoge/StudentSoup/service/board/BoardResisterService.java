d303551171f1fbf0ebf19d369685c8d378debdd8
xogns4909 <xogns4909@naver.com>
2023-01-10 15:42:43 +0900

    -feat : 보드 생성 서비스 로직 추가

---
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/service/board/BoardResisterService.java
@@ -0,0 +1,33 @@
+package ProjectDoge.StudentSoup.service.board;
+
+import ProjectDoge.StudentSoup.dto.board.BoardFormDto;
+import ProjectDoge.StudentSoup.entity.board.Board;
+import ProjectDoge.StudentSoup.entity.file.ImageFile;
+import ProjectDoge.StudentSoup.entity.member.Member;
+import ProjectDoge.StudentSoup.service.file.FileService;
+import ProjectDoge.StudentSoup.service.member.MemberFindService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BoardResisterService {
+
+    private final MemberFindService memberFindService;
+
+    private final FileService fileService;
+    @Transactional
+    public Long join(Long memberId,BoardFormDto boardFormDto, MultipartFile multipartFile){
+        log.info("게시글 생성 메소드가 실행되었습니다.");
+        Member member = memberFindService.findOne(memberId);
+        Long fileId = fileService.join(multipartFile);
+        ImageFile file = fileService.findOne(fileId);
+        Board board = new Board().createBoard(boardFormDto, member, member.getSchool(), file, member.getDepartment());
+        log.info("게시글이 저장되었습니다.[{}]",board.getId());
+        return board.getId();
+    }
+}
