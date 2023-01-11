0eb2988557ec24b43a76fa021c1a1ec50c24c297
xogns4909 <xogns4909@naver.com>
2023-01-11 15:10:33 +0900

    -feat : 게시판 메인페이지 DTO 생성

---
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/dto/board/BoardMainDto.java
@@ -0,0 +1,27 @@
+package ProjectDoge.StudentSoup.dto.board;
+
+import ProjectDoge.StudentSoup.entity.board.BoardCategory;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardMainDto {
+    private Long boardId;
+
+    private BoardCategory boardCategory;
+
+    private String title;
+
+    private String updateDate;
+
+    private int likedCount;
+
+    public BoardMainDto(Long boardId, BoardCategory boardCategory, String title, String updateDate, int likedCount) {
+        this.boardId = boardId;
+        this.boardCategory = boardCategory;
+        this.title = title;
+        this.updateDate = updateDate;
+        this.likedCount = likedCount;
+    }
+}
