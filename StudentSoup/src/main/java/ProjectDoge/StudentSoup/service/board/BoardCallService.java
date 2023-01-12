3054549e6cf9b4f02b7a23dae81a191c2d938e8d
xogns4909 <xogns4909@naver.com>
2023-01-12 16:54:25 +0900

    -refactor : LoginMember 확인 메서드 모듈화

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/service/board/BoardCallService.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/service/board/BoardCallService.java
@@ -5,6 +5,7 @@ import ProjectDoge.StudentSoup.dto.board.BoardMainDto;
 import ProjectDoge.StudentSoup.entity.board.Board;
 import ProjectDoge.StudentSoup.entity.board.BoardLike;
 import ProjectDoge.StudentSoup.exception.member.MemberIdNotSentException;
+import ProjectDoge.StudentSoup.exception.member.MemberNotFoundException;
 import ProjectDoge.StudentSoup.repository.board.BoardLikeRepository;
 import ProjectDoge.StudentSoup.repository.board.BoardRepository;
 import lombok.RequiredArgsConstructor;
@@ -26,7 +27,8 @@ public class BoardCallService {
     boolean boardNotLiked = false;
 
     public List<BoardMainDto> getBoardInSchool(Long schoolId,Long memberId){
-        if(memberId == null) throw new MemberIdNotSentException("맴버 아이디가 전송되지 않았습니다.");
+        log.info("게시판 클릭시 게시글 호출 로직이 실행되었습니다.");
+        isLoginMember(memberId);
         List<BoardMainDto> boardMainDtoList = new ArrayList<>();
         List<Board> boards = boardRepository.findBySchoolId(schoolId);
         for(Board board : boards){
@@ -36,6 +38,7 @@ public class BoardCallService {
     }
 
     public BoardDto getBoardDetail(Long boardId,Long memberId){
+        log.info("게시글 클릭시 게시글 호출 로직이 실행되었습니다.");
         Board board = boardFindService.findOne(boardId);
         BoardLike boardLike = boardLikeRepository.findByBoardIdAndMemberId(boardId, memberId).orElse(null);
         if(boardLike==null){
@@ -44,6 +47,15 @@ public class BoardCallService {
         return getLikeBoardDto(board);
 
     }
+    private void isLoginMember(Long memberId){
+        log.info("회원이 로그인이 되었는지 확인하는 로직이 실행되었습니다.");
+        if(memberId == null){
+            log.info("회원의 기본키가 전달이 되지 않았거나 로그인이 되어있지 않은 상태입니다.");
+            throw new MemberNotFoundException("회원이 로그인이 되어있지 않은 상태이거나, 기본키가 전달 되지 않았습니다.");
+        }
+        log.info("회원이 로그인이 되어있는 상태입니다.");
+    }
+
     private BoardDto getLikeBoardDto(Board board) {
         return new BoardDto(board,boardLiked);
     }
