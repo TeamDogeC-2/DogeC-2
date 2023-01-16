80a4469fc66d9eea6da21ca69fbb7828350c443f
xogns4909 <xogns4909@naver.com>
2023-01-16 14:40:39 +0900

    refactor : boardCategory 수정

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/entity/board/BoardCategory.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/entity/board/BoardCategory.java
@@ -8,8 +8,14 @@ import lombok.RequiredArgsConstructor;
 public enum BoardCategory {
 
     FREE("자유게시판"),
-    EMPLOYMENT("취업게시판"),
-    CONSULTING("상담게시판");
+    EMPLOYMENT("취업/상담게시판"),
+
+    TIP("팁게시판"),
+    ANNOUNCEMENT("공지사항");
+
+
+
+
 
     private final String boardCategory;
 }
