8b333f304822e392304e327abcbf92ba495ccd92
xogns4909 <xogns4909@naver.com>
2023-01-16 15:55:29 +0900

    -feat : board정렬 케이스 enum 추가

---
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/dto/board/BoardSortedCase.java
@@ -0,0 +1,15 @@
+package ProjectDoge.StudentSoup.dto.board;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum BoardSortedCase {
+    NORMAL(0),
+
+    LIKED(1);
+
+
+    private final int value;
+}
