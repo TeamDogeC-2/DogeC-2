cd41d678441f7bb2db992198971a7dc6b1d9e30a
eunsung <esk114477@gmail.com>
2023-01-17 15:34:57 +0900

    -feat : mypage profile 변경 기능 추가

--- StudentSoup/src/main/frontend/src/components/mypage/mypageHome.tsx
+++ StudentSoup/src/main/frontend/src/components/mypage/mypageHome.tsx
@@ -1,16 +1,53 @@
 import School from '../../img/school.png';
 import Major from '../../img/major.png';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import Human from '../../img/circle_human.png';
+import { useRef } from 'react';
+import axios from 'axios';
 
 const MypageHome = () => {
+  const uploadImage = useRef<any>(null);
+  const imageUploader = useRef<any>(null);
 
+  const IMAGE_FILE_ID = sessionStorage.getItem('fileName');
+
+  const memberId = sessionStorage.getItem('memberId');
   const savedName = sessionStorage.getItem('nickname');
   const savedSchool = sessionStorage.getItem('schoolName');
   const savedDepart = sessionStorage.getItem('departmentName');
-  console.log(savedName);
-  console.log(savedSchool);
-  console.log(savedDepart);
+
+  const handleImageUpload = (e: any) => {
+    const [file] = e.target.files;
+    console.log(file);
+    if (file) {
+      const reader = new FileReader();
+      const { current } = uploadImage;
+      current.file = file;
+      reader.onload = (e: any) => {
+        current.src = e.target.result;
+        sessionStorage.setItem('fileName', current.src);
+      };
+      reader.readAsDataURL(file);
+      axios
+        .post(
+          '/members/edit/image',
+          {
+            memberId: memberId,
+            multipartFile: file,
+          },
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          },
+        )
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  };
 
   return (
     <div className="flex-[9] z-[1] bg-zinc-100">
@@ -21,8 +58,11 @@ const MypageHome = () => {
         <div className="w-[451px] relative bottom-[140px]">
           <div className="flex flex-col items-center w-[319px] h-[425px] ml-[75px] mr-[57px] bg-white shadow-lg rounded-[5px] z-[2]">
             <div className="mt-[35px]">
-              <div className='w-[122px] h-[122px] bg-[url("./img/circle_fill_gray.jpg")] bg-cover relative top-[4px] rounded-full'></div>
-              <div className='w-[61px] h-[65px] bg-[url("./img/human.jpg")] bg-cover relative bottom-[90px] left-[30px] mb-[10px]'></div>
+              <img
+                ref={uploadImage}
+                src={IMAGE_FILE_ID ?? Human}
+                className='w-[122px] h-[122px] bg-[url("./img/circle_human.png")] rounded-full mb-[70px]'
+              />
             </div>
             <div className="relative bottom-[60px]">
               <span className="text-[28px] leading-39px text-[#353535]">{savedName}</span>
@@ -32,9 +72,7 @@ const MypageHome = () => {
                 <div className="mr-[10px]">
                   <img src={School} alt="" />
                 </div>
-                <span className="text-[12px] leading-[17px] text-[#8D8D8D]">
-                  {savedSchool}
-                </span>
+                <span className="text-[12px] leading-[17px] text-[#8D8D8D]">{savedSchool}</span>
               </div>
               <div className="flex flex-row items-center text-left">
                 <div className="mr-[10px]">
@@ -43,9 +81,21 @@ const MypageHome = () => {
                 <span className="text-[12px] leading-[17px] text-[#8D8D8D]">{savedDepart}</span>
               </div>
             </div>
-            <button className="w-[262px] h-[44px] bg-[#FF611D] text-white rounded-[11px] text-[15px] leading-[21px] relative bottom-[10px]">
-              내 프로필 편집
-            </button>
+            <div className="w-[262px] h-[44px]">
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageUpload}
+                ref={imageUploader}
+                className="hidden"
+              />
+              <button
+                onClick={() => imageUploader.current.click()}
+                className="w-full h-full bg-[#FF611D] text-white rounded-[11px] text-[15px] leading-[21px] relative bottom-[10px]"
+              >
+                내 프로필 편집
+              </button>
+            </div>
             <div>
               <span className="text-[10px] leading-[14px] text-[#8D8D8D]">
                 가입일 : 2022년 12월 26일
