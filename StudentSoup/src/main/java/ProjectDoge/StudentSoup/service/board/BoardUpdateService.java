dd4c06a5d0bb66fba4688b2c381d8d18ed2521ea
xogns4909 <xogns4909@naver.com>
2023-01-18 18:47:35 +0900

    -feat :NotOwnMember 예외처리 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/service/board/BoardUpdateService.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/service/board/BoardUpdateService.java
@@ -6,6 +6,7 @@ import ProjectDoge.StudentSoup.dto.board.BoardUpdateDto;
 import ProjectDoge.StudentSoup.entity.board.Board;
 import ProjectDoge.StudentSoup.entity.board.BoardLike;
 import ProjectDoge.StudentSoup.entity.file.ImageFile;
+import ProjectDoge.StudentSoup.exception.board.NotOwnMemberException;
 import ProjectDoge.StudentSoup.repository.board.BoardLikeRepository;
 import ProjectDoge.StudentSoup.repository.file.FileRepository;
 import ProjectDoge.StudentSoup.service.file.FileService;
@@ -44,6 +45,9 @@ public class BoardUpdateService {
     public BoardDto editBoard(BoardFormDto boardFormDto, Long boardId,Long memberId, List<MultipartFile> multipartFiles){
         log.info("게시판 업데이트 서비스가 실행되었습니다.");
         Board board = boardFindService.findOne(boardId);
+        if(board.getMember().getMemberId() != memberId){
+            throw new NotOwnMemberException("게시글 작성자와 수정자가 일치하지 않습니다.");
+        }
         deleteImageFile(multipartFiles, board);
         List<ImageFile> imageFiles = new ArrayList<>();
         createImageFiles(multipartFiles,imageFiles);
