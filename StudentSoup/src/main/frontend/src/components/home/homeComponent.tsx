e81c582182ef799471d2db6712204184466e73b2
eunsung <esk114477@gmail.com>
2023-01-19 18:00:28 +0900

    -style : 메인페이지 스크롤 잠금

--- StudentSoup/src/main/frontend/src/components/home/homeComponent.tsx
+++ StudentSoup/src/main/frontend/src/components/home/homeComponent.tsx
@@ -1,10 +1,21 @@
+import { useEffect } from 'react';
 import Navbar from '../common/navbar';
 import MainSearch from './mainSearch';
 
 const Home = () => {
+  function disableScrolling() {
+    var x = window.scrollX;
+    var y = window.scrollY;
+    window.onscroll = function () {
+      window.scrollTo(x, y);
+    };
+  }
+  useEffect(() => {
+    disableScrolling();
+  }, []);
   return (
-    <div className='w-full h-full bg-[url("./img/mainlogo.jpg")] bg-cover z-50'>
-      <div className="w-full h-full bg-gradient-to-t from-[rgba(0,0,0,0.45)] to-[rgba(0,0,0,0.1)] z-[51]">
+    <div className='w-full h-full bg-[url("./img/mainlogo.jpg")] bg-cover z-50 no-scrollbar'>
+      <div className="w-full h-full bg-gradient-to-t from-[rgba(0,0,0,0.45)] to-[rgba(0,0,0,0.1)] z-[51] no-scrollbar">
         <Navbar />
         <MainSearch />
       </div>
