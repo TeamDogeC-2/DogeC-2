8fe20983fdd2e586af87445605658aee8c50eb38
eunsung <esk114477@gmail.com>
2023-01-19 16:38:22 +0900

    -fix : 메인화면 학교list 선택 버그 수정

--- StudentSoup/src/main/frontend/src/components/home/mainSearch.tsx
+++ StudentSoup/src/main/frontend/src/components/home/mainSearch.tsx
@@ -6,6 +6,12 @@ const MainSearch = () => {
   const [searchSchool, setSearchSchool] = useState<any[]>();
   const [posts, setPosts] = useState<any[]>();
 
+  const [inputSchool, setInputSchool] = useState<string>('');
+  // const [clickSchool, setClickSchool] = useState<string>('');
+  // const [schoolIndex, setSchoolIndex] = useState<any[]>();
+
+  // const [ableClick, setAbleClick] = useState<boolean>(false);
+
   const getSchool = () => {
     axios
       .get('/home')
@@ -23,19 +29,33 @@ const MainSearch = () => {
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
+    setInputSchool(value);
     if (value.length === 0) {
       setSearchSchool(posts);
       return;
     }
     const resultArray = posts?.filter(post => post.schoolName.includes(e.target.value));
     setSearchSchool(resultArray);
-    console.log(value);
-    console.log(searchSchool);
+
+    // if (inputSchool === clickSchool) {
+    //   setAbleClick(true);
+    // } else {
+    //   setAbleClick(false);
+    // }
+  };
+
+  const handleClick = (e: any) => {
+    setInputSchool(e.target.innerText);
+    // setClickSchool(e.target.innerText);
+    // setSchoolIndex(e.target.id);
+    const resultArray = posts?.filter(post => post.schoolName.includes(e.target.innerText));
+    setSearchSchool(resultArray);
   };
 
   return (
     <div className="w-full h-[calc(100vh-88px)] flex flex-col mt-[290px] items-center">
       <div
+        // ref={selectRef}
         className={cn(
           'flex flex-col text-center relative bottom-[88px]',
           'after:flex after:flex-col after:text-center after:relative after:bottom-[88px]',
@@ -50,6 +70,7 @@ const MainSearch = () => {
           <input
             onChange={handleChange}
             name="text"
+            value={inputSchool}
             placeholder="지역 학교 명을 입력하세요."
             className="w-[500px] h-[58px] text-[25px] fw-400 leading-[33px] text-[#A0A0A0] border-none pl-[23px]"
           ></input>
@@ -59,7 +80,11 @@ const MainSearch = () => {
         </div>
         {searchSchool?.map(school => (
           <div key={school.schoolId} className="w-[654px] h-[58px] rounded-[5px] bg-white">
-            <span className="flex text-[16px] mt-[15px] ml-[20px] items-center font-medium">
+            <span
+              onClick={handleClick}
+              id={school.schoolId}
+              className="flex text-[16px] mt-[15px] ml-[20px] items-center font-medium"
+            >
               {school.schoolName}
             </span>
           </div>
