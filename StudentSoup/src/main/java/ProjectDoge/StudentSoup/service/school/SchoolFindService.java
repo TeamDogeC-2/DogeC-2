96308b0da6378b44f7f64d2afd53d55cff964c6d
Bombo <bombo96@chungwoon.ac.kr>
2023-01-20 14:26:38 +0900

    - feat : School FindOne 서비스 Using schoolName 서비스 로직 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/service/school/SchoolFindService.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/service/school/SchoolFindService.java
@@ -25,6 +25,14 @@ public class SchoolFindService {
             throw new SchoolNotFoundException("학교를 찾지 못하였습니다.");
         });
     }
+
+    public School findOne(String schoolName){
+        return schoolRepository.findBySchoolName(schoolName).orElseThrow(() ->{
+            log.info("findOne 메소드가 실행되었습니다. [{}]", schoolName);
+            throw new SchoolNotFoundException("학교를 찾지 못하였습니다.");
+        });
+    }
+
     private void checkSchoolIdSent(Long schoolId) {
         if(schoolId == null){
             log.info("schoolId가 전송되지 않았습니다.");
