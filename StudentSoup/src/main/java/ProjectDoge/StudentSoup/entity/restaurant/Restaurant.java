b1346a35d5baab783ebc936392e723da73e99d07
Bombo <bombo96@chungwoon.ac.kr>
2023-01-20 16:29:29 +0900

    - feat : 음식점 별점 업데이트 비즈니스 로직 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/entity/restaurant/Restaurant.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/entity/restaurant/Restaurant.java
@@ -156,6 +156,7 @@ public class Restaurant {
         return (rad * 180 / Math.PI);
     }
 
+    // 좋아요 업데이트 로직
     public void addLikedCount(){
         this.likedCount += 1;
     }
@@ -164,8 +165,14 @@ public class Restaurant {
             this.likedCount -= 1;
         }
     }
+    // 조회수 업데이트 로직
     public void addViewCount() {
         this.viewCount += 1;
+    }
+
+    // 별점 업데이트 로직
+    public void updateStarLiked(float starLiked){
+        this.starLiked = starLiked;
     }
 
 }
