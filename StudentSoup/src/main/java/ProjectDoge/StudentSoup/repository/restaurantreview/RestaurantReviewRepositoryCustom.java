591d42de65042a36db0808ae77c0643962041f71
Bombo <bombo96@chungwoon.ac.kr>
2023-01-22 00:26:09 +0900

    - feat : 음식점 리뷰 조회 페이징 처리 쿼리 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/restaurantreview/RestaurantReviewRepositoryCustom.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/restaurantreview/RestaurantReviewRepositoryCustom.java
@@ -1,8 +1,18 @@
 package ProjectDoge.StudentSoup.repository.restaurantreview;
 
+import ProjectDoge.StudentSoup.entity.restaurant.RestaurantReview;
+import com.querydsl.jpa.impl.JPAQuery;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface RestaurantReviewRepositoryCustom {
 
     Double avgByRestaurantId(Long restaurantId);
 
     Long countByRestaurantId(Long restaurantId);
+
+    List<RestaurantReview> findByRestaurantIdSorted(Long restaurantId, String sorted, Pageable pageable);
+
+    JPAQuery<Long> pagingCountByRestaurantId(Long restaurantId);
 }
--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/restaurantreview/RestaurantReviewRepositoryImpl.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/restaurantreview/RestaurantReviewRepositoryImpl.java
@@ -1,10 +1,16 @@
 package ProjectDoge.StudentSoup.repository.restaurantreview;
 
+import ProjectDoge.StudentSoup.entity.restaurant.RestaurantReview;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
+import static ProjectDoge.StudentSoup.entity.member.QMember.member;
+import static ProjectDoge.StudentSoup.entity.restaurant.QRestaurant.restaurant;
 import static ProjectDoge.StudentSoup.entity.restaurant.QRestaurantReview.restaurantReview;
 
 @RequiredArgsConstructor
@@ -34,5 +40,39 @@ public class RestaurantReviewRepositoryImpl implements RestaurantReviewRepositor
                 .fetch();
 
         return result.get(0);
+    }
+
+    @Override
+    public List<RestaurantReview> findByRestaurantIdSorted(Long restaurantId, String sorted, Pageable pageable) {
+
+        return queryFactory
+                .select(restaurantReview)
+                .from(restaurantReview)
+                .leftJoin(restaurantReview.member, member)
+                .fetchJoin()
+                .leftJoin(restaurantReview.restaurant, restaurant)
+                .fetchJoin()
+                .where(restaurantReview.restaurant.id.eq(restaurantId))
+                .orderBy(checkSortedRestaurantReviewCondition(sorted))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
+
+    private OrderSpecifier<?> checkSortedRestaurantReviewCondition(String sorted){
+        if(sorted.equals("newest"))
+            return restaurantReview.writeDate.desc();
+        else if(sorted.equals("liked"))
+            return restaurantReview.likedCount.desc();
+
+        return restaurantReview.writeDate.desc();
+    }
+
+    @Override
+    public JPAQuery<Long> pagingCountByRestaurantId(Long restaurantId) {
+        return queryFactory
+                .select(restaurantReview.count())
+                .from(restaurantReview)
+                .where(restaurantReview.restaurant.id.eq(restaurantId));
     }
 }
