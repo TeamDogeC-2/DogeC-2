563e78133cfcc4dfc082b7e62681d1e590d49a61
Bombo <bombo96@chungwoon.ac.kr>
2023-01-22 00:38:27 +0900

    - feat : 음식점 리뷰 조회 시 요청 데이터 객체 추가

---
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/dto/restaurant/RestaurantReviewCallReqDto.java
@@ -0,0 +1,10 @@
+package ProjectDoge.StudentSoup.dto.restaurant;
+
+import lombok.Data;
+
+@Data
+public class RestaurantReviewCallReqDto {
+
+    private Long restaurantId;
+    private Long memberId;
+}
