90bc30d0054e173516e99bef2241e6bb7a880ad6
Bombo <bombo96@chungwoon.ac.kr>
2023-01-24 00:09:07 +0900

    - feat : 음식점 리뷰 삭제 요청 dto 추가

---
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/dto/restaurantreview/RestaurantReviewDeleteDto.java
@@ -0,0 +1,10 @@
+package ProjectDoge.StudentSoup.dto.restaurantreview;
+
+import lombok.Data;
+
+@Data
+public class RestaurantReviewDeleteDto {
+
+    private Long restaurantId;
+    private Long memberId;
+}
