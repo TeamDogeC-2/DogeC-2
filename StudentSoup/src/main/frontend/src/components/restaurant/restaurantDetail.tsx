481691f14e6058083543182d5c3125c6cc763331
spsp8426 <spsp8426@naver.com>
2023-01-23 16:16:27 +0900

    FEAT : 음식점 세부사항 렌더링 구현, kakaomap 구현 , 좋아요 버튼 구현

--- StudentSoup/src/main/frontend/src/components/restaurant/restaurantDetail.tsx
+++ StudentSoup/src/main/frontend/src/components/restaurant/restaurantDetail.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+/* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+import React, { useState, useEffect } from 'react';
 import RestaurantNavbar from '../common/restaurantNavbar';
 import { ReactComponent as Star } from '../../img/Star.svg';
-import { ReactComponent as Heart } from '../../img/Heart.svg';
 import { ReactComponent as Share } from '../../img/Share.svg';
 import { ReactComponent as Review } from '../../img/Review.svg';
-import { ReactComponent as Camera } from '../../img/camera.svg';
 import { ReactComponent as Location } from '../../img/location.svg';
 import { ReactComponent as Phone } from '../../img/Phone.svg';
 import { ReactComponent as Clock } from '../../img/Clock.svg';
@@ -14,45 +14,152 @@ import MenuInfopage from './menuInfo';
 import Reviewpage from './reviewInfo';
 import Sharepage from './pictureInfo';
 import cn from 'clsx';
+import axios from 'axios';
+import { useLocation } from 'react-router-dom';
+const kakao = (window as any).kakao;
 
 const restaurant = () => {
+  const saveMemberId = sessionStorage.getItem('memberId');
+  const [set, isSet] = useState<any>([]);
+  const state = useLocation<any>();
   const [click, setClick] = useState<any>(1);
+  const [latitude, setLatitude] = useState<any>();
+  const [longitude, setLongitude] = useState<any>();
+  const [heart, setHeart] = useState<boolean>();
+  console.log(state.pathname);
+
+  const restaurantNumber = state.state[0];
+  const schoolName = state.state[1];
+
   const clickPage = (e: any) => {
     setClick(e);
   };
+  const url = `/restaurant/${restaurantNumber}`;
+  useEffect(() => {
+    axios
+      .post(url, {
+        schoolName,
+        restaurantId: restaurantNumber,
+        memberId: saveMemberId,
+      })
+      .then(res => {
+        isSet(res.data.restaurant);
+        setLatitude(Number(res.data.restaurant.latitude));
+        setLongitude(Number(res.data.restaurant.longitude));
+        setHeart(res.data.restaurant.like);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, [heart]);
+  const MapLocation = [longitude, latitude];
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(...MapLocation),
+      level: 2,
+    };
+    const map = new kakao.maps.Map(container, options);
+  });
+  const handleHeartCount = async () => {
+    if (!saveMemberId) {
+      alert('로그인후 이용 가능한 기능입니다.');
+    } else {
+      await axios.post(`/restaurant/${saveMemberId}/like`, {
+        restaurantId: restaurantNumber,
+        memberId: saveMemberId,
+      });
+      setHeart(!heart);
+    }
+  };
+  /* 공유 버튼 보류 */
+  // const handleCopyClipBoard = async (text: string) => {
+  //   try {
+  //     await navigator.clipboard.writeText(text);
+
+  //     alert('url 주소를 복사하였습니다.');
+  //   } catch (error) {
+  //     alert('url 주소가 생성되지 않았습니다.');
+  //   }
+  // };
   return (
     <>
       <RestaurantNavbar />
       <div className="w-[full] h-[535px] flex m-[49px] justify-center">
         <div className="w-[281px] h-[532px]  bg-[#FFFFFF] shadow-[0px_2px_10px_rgba(0,0,0,0.1)] rounded-[5px] mr-[14px]">
-          <div className="w-[238px] h-[239px] bg-[#FF6347] m-[21px]"></div>
-          <div className="ml-[22px] h-[50px] font-[900] text-[28px] leading-[40px] flex items-center">
-            금돈
+          <div id="map" className="w-[238px] h-[239px] m-[21px]"></div>
+          <div className="ml-[22px] whitespace-normal h-[50px] font-bold text-[25px] leading-[40px] flex items-center">
+            {set.name}
           </div>
           <div className="ml-[22px] h-[16px] font-[400] leading-[21px] text-[16px] flex items-center text-[#717171]">
-            제물포역 | 돈가스, 함박스테이크
+            {set.tag}
           </div>
           <div className="flex flex-row">
             <Star className="ml-[22px] mt-[10px]" />
             <div className="ml-[5.92px] mt-[10px] h-[16px] font-bold text-[16px] leading-[23px] flex items-center text-[#515151]">
-              4.6
+              {set.starLiked}
             </div>
             <div className="ml-[10px] mt-[10px] h-[16px] font-[400] text-[13px] leading-[17px] flex items-center text-[#9C9C9C]">
-              258개 평점(428명 참여)
+              {set.reviewCount}개의 리뷰
             </div>
           </div>
           <div className="ml-[22px] mr-[21px] mt-[23px] border-[1px] border-[#DEDEDE] bg-[#DEDEDE]"></div>
           <div className="flex flex-row w-[238px]">
             <div className="flex flex-col">
-              <Heart className="ml-[47px] mt-[17px] mb-[12.26px]" />
-              <div className="ml-[35px] w-[39px] h-[11px] font-[400] text-[13px] leading-[17px] flex items-center text-[#515151]">
+              {heart ? (
+                <svg
+                  className="ml-[47px] mt-[17px] mb-[12.26px]"
+                  width="16"
+                  height="14"
+                  viewBox="0 0 16 14"
+                  fill="#FF611D"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M13.4222 2.0379C13.0933 1.70886 12.7028 1.44783 12.273 1.26975C11.8432 1.09166 11.3826 1 10.9173 1C10.4521 1 9.99142 1.09166 9.56163 1.26975C9.13183 1.44783 8.74134 1.70886 8.41245 2.0379L7.72988 2.72046L7.04732 2.0379C6.38298 1.37356 5.48195 1.00034 4.54244 1.00034C3.60292 1.00034 2.70189 1.37356 2.03756 2.0379C1.37322 2.70224 1 3.60327 1 4.54278C1 5.48229 1.37322 6.38333 2.03756 7.04766L2.72012 7.73023L7.72988 12.74L12.7396 7.73023L13.4222 7.04766C13.7513 6.71877 14.0123 6.32827 14.1904 5.89848C14.3684 5.46868 14.4601 5.00801 14.4601 4.54278C14.4601 4.07755 14.3684 3.61688 14.1904 3.18708C14.0123 2.75729 13.7513 2.36679 13.4222 2.0379Z"
+                    stroke="#FF611D"
+                    strokeWidth="1.2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  className="ml-[47px] mt-[17px] mb-[12.26px]"
+                  width="16"
+                  height="14"
+                  viewBox="0 0 16 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M13.4222 2.0379C13.0933 1.70886 12.7028 1.44783 12.273 1.26975C11.8432 1.09166 11.3826 1 10.9173 1C10.4521 1 9.99142 1.09166 9.56163 1.26975C9.13183 1.44783 8.74134 1.70886 8.41245 2.0379L7.72988 2.72046L7.04732 2.0379C6.38298 1.37356 5.48195 1.00034 4.54244 1.00034C3.60292 1.00034 2.70189 1.37356 2.03756 2.0379C1.37322 2.70224 1 3.60327 1 4.54278C1 5.48229 1.37322 6.38333 2.03756 7.04766L2.72012 7.73023L7.72988 12.74L12.7396 7.73023L13.4222 7.04766C13.7513 6.71877 14.0123 6.32827 14.1904 5.89848C14.3684 5.46868 14.4601 5.00801 14.4601 4.54278C14.4601 4.07755 14.3684 3.61688 14.1904 3.18708C14.0123 2.75729 13.7513 2.36679 13.4222 2.0379Z"
+                    stroke="#515151"
+                    strokeWidth="1.2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              )}
+
+              <div
+                onClick={handleHeartCount}
+                className="ml-[35px] w-[39px] h-[11px] font-[400] text-[13px] leading-[17px] flex items-center text-[#515151] cursor-pointer"
+              >
                 좋아요
               </div>
             </div>
             <div className="ml-[24px] mt-[17px] border-[1px] border-[#DEDEDE] rotate-[90] bg-[#DEDEDE]"></div>
             <div className="flex flex-col">
               <Share className="ml-[35px] mt-[18.24px]" />
-              <div className="ml-[30px] w-[26px] mt-[9px] h-[11px] font-[400] text-[13px] leading-[17px] flex items-center">
+              <div
+                /* 공유 버튼 보류
+                 // onClick={async () => {
+                //   await handleCopyClipBoard(`http://localhost:3000${state.pathname}`);
+                // }} */
+
+                className="ml-[30px] w-[26px] mt-[9px] h-[11px] font-[400] text-[13px] leading-[17px] flex items-center"
+              >
                 공유
               </div>
             </div>
@@ -71,16 +178,13 @@ const restaurant = () => {
           </div>
         </div>
         <div className="w-[744px] h-[563px] bg-[#FFFFFF] shadow-[0px_2px_10px_rgba(0,0,0,0.1)] rounded-[5px]">
-          <div className="grid grid-cols-3">
-            <div className="ml-[21px] mt-[21px] w-[268px] h-[290px] bg-[#29088A]"></div>
-            <div className="ml-[45px] mt-[21px] w-[214px] h-[143px] bg-[#31B404]"></div>
-            <div className="ml-[16px] mt-[21px] w-[214px] h-[143px] bg-[#F8E0E0]"></div>
-            <div className="ml-[293px] mt-[-142px] w-[214px] h-[143px] bg-[#F5A9D0]"></div>
-            <div className="ml-[264px] mt-[-142px] w-[214px] h-[143px] bg-[rgba(23,23,23,0.55)]">
-              <Camera className="ml-[97px] mt-[53px]" />
-              <div className="ml-[55px] mt-[5.75px] font-[400] leading-[21px] flex items-center text-[#FEFEFE]">
-                230장 모두보기
-              </div>
+          <div className="grid grid-row-2 grid-cols-[268px_minmax(214px,_0fr)_1px] gap-1 ml-[20px] mt-[20px]">
+            <div className="row-span-2 w-[268px] h-[290px] bg-[#29088A]"></div>
+            <div className="w-[214px] h-[143px] bg-[#31B404]"></div>
+            <div className="w-[214px] h-[143px] bg-[#F8E0E0]"></div>
+            <div className="w-[214px] h-[143px] bg-[#F5A9D0]"></div>
+            <div className="w-[214px] h-[143px] bg-[rgba(23,23,23,0.55)]">
+              <div className="ml-[55px] mt-[5.75px] font-[400] leading-[21px] flex items-center text-[#FEFEFE]"></div>
             </div>
           </div>
           <div className="ml-[21px] mt-[32px] font-[400] text-[24px] flex items-center">
@@ -89,47 +193,43 @@ const restaurant = () => {
           <div className="flex flex-row">
             <Location className="ml-[21px] mt-[20.15px]" />
             <div className="ml-[10.33px] mt-[18px] h-[16px] font-[400] text-[16px] leading-[21px] flex items-center text-[#515151]">
-              인천 미추홀구 숙골로87번길28
+              {set.address}
             </div>
-            <div className="ml-[15.78px] mt-[19px] w-[14px] h-[14px] bg-[#1C1B6A] rounded-full">
-              <div className="ml-[3px] mt-[1px] w-[4px] h-[9px] text-[13px] leading-[18px] font-[400] flex items-center text-[#FFFFFF]">
-                1
-              </div>
-            </div>
+
             <div className="ml-[5px] mt-[17px] flex flex-row ">
               <div className=" h-[16px] font-[400] leading-[21px] flex items-center text-[#515151]">
-                제물포역
+                {set.schoolName}
               </div>
               <div className="ml-[5px] h-[16px] font-[400] leading-[21px] flex items-center text-[#7B7B7B]">
-                2번 출구에서
+                에서
               </div>
               <div className="ml-[5px] h-[16px] font-[400] leading-[21px] flex items-center text-[#FF611D]">
-                637m
+                {set.distance}
               </div>
             </div>
           </div>
           <div className="flex flex-row">
             <Phone className="ml-[21px] mt-[19px]" />
             <div className="ml-[9.61px] mt-[17px] h-[16px] font-[400] text-[16px] leading-[21px] flex items-center text-[#515151]">
-              0507-1391-1009
+              {set.tel}
             </div>
           </div>
           <div className="flex flex-row">
             <Clock className="ml-[21px] mt-[21px]" />
             <div className="ml-[8px] mt-[18px] h-[16px] font-[400] text-[16px] leading-[21px] flex items-center text-[#515151]">
-              영업시간 AM 09:00- PM 21:00
+              영업시간 AM {set.startTime}- PM {set.endTime}
             </div>
           </div>
           <div className="flex flex-row">
             <PlusCircle className="ml-[21px] mt-[20px]" />
             <div className="ml-[9px] mt-[17px] h-[16px] font-[400] text-[16px] leading-[21px] flex items-center text-[#515151]">
-              주차,포장,배달,무선 인터넷 남/녀 화장실 구분
+              {set.detail}
             </div>
           </div>
           <div className="flex flex-row">
             <InfoHeart className="ml-[21px] mt-[19px]" />
             <div className="ml-[8px] mt-[16px] h-[16px] font-[400] leading-[21px] flex items-center text-[#515151]">
-              이 식당에 1235명의 좋아요한 사용자가 있습니다.
+              이 식당에 {set.likedCount}명의 좋아요한 사용자가 있습니다.
             </div>
           </div>
         </div>
