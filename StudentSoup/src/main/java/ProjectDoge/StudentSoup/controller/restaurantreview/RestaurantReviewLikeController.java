f9d2dffc924dd451a480a4276cfab85b42f9119c
Bombo <bombo96@chungwoon.ac.kr>
2023-01-23 14:49:29 +0900

    - feat : 음식점 리뷰 좋아요 api 호출 컨트롤러 추가

---
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/restaurantreview/RestaurantReviewLikeController.java
@@ -0,0 +1,29 @@
+package ProjectDoge.StudentSoup.controller.restaurantreview;
+
+import ProjectDoge.StudentSoup.dto.restaurantreview.RestaurantReviewLikeReqDto;
+import ProjectDoge.StudentSoup.service.restaurantreview.RestaurantReviewLikeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/restaurant/{restaurantId}")
+public class RestaurantReviewLikeController {
+
+    private final RestaurantReviewLikeService restaurantReviewLikeService;
+
+    @PostMapping("/{restaurantReviewId}/like")
+    public ResponseEntity<ConcurrentHashMap<String, Object>> restaurantReviewLike(
+            @PathVariable Long restaurantId,
+            @PathVariable Long restaurantReviewId,
+            @RequestBody RestaurantReviewLikeReqDto dto){
+
+        ConcurrentHashMap<String, Object> resultMap = restaurantReviewLikeService.restaurantReviewLike(dto.getRestaurantReviewId(), dto.getMemberId());
+        return ResponseEntity.ok(resultMap);
+    }
+}
