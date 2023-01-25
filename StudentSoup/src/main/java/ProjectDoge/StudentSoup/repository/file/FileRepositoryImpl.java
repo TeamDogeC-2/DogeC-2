8d7da9af7cd0ea21999a8ba49da8829eb2463c4d
Bombo <bombo96@chungwoon.ac.kr>
2023-01-25 19:11:26 +0900

    - feat : 각 음식점에 리뷰에 달린 이미지 호출 로직 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/file/FileRepositoryImpl.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/file/FileRepositoryImpl.java
@@ -1,7 +1,38 @@
 package ProjectDoge.StudentSoup.repository.file;
 
+import ProjectDoge.StudentSoup.entity.file.QImageFile;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+
+import java.util.List;
+
+import static ProjectDoge.StudentSoup.entity.file.QImageFile.imageFile;
 
 @RequiredArgsConstructor
 public class FileRepositoryImpl implements FileRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+    @Override
+    public Page<String> callImageFileByRestaurantReviewId(Long restaurantId, Pageable pageable) {
+
+        List<String> content = queryFactory
+                .select(imageFile.fileName)
+                .from(imageFile)
+                .where(imageFile.restaurant.id.eq(restaurantId), imageFile.restaurantReview.id.isNotNull())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+
+        JPAQuery<Long> count = queryFactory
+                .select(imageFile.fileName.count())
+                .from(imageFile)
+                .where(imageFile.restaurant.id.eq(restaurantId), imageFile.restaurantReview.id.isNotNull());
+
+        return PageableExecutionUtils.getPage(content, pageable, count::fetchOne);
+    }
 }
