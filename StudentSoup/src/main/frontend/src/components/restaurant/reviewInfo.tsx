b0e3fef4f05ac9b979864afaabda2d13524857ec
spsp8426 <spsp8426@naver.com>
2023-01-26 00:34:12 +0900

    FEAT : 별점 점수 기능 추가와 이름 추가 및 디자인 수정

--- StudentSoup/src/main/frontend/src/components/restaurant/reviewInfo.tsx
+++ StudentSoup/src/main/frontend/src/components/restaurant/reviewInfo.tsx
@@ -1,10 +1,20 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import React, { useState, useEffect } from 'react';
 import { ReactComponent as SmallStar } from '../../img/reviewSmallStar.svg';
 import { ReactComponent as MoreInfo } from '../../img/moreicon.svg';
+import { ReactComponent as ReviewEdit } from '../../img/reviewedit.svg';
 import cn from 'clsx';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
 
-const review = () => {
-  const AVR_RATE = 4.6;
+const review = (props: any) => {
+  const name = props.name;
+  const totalStar = props.starLiked;
+  const state = useLocation<any>();
+  const restaurantNumber = state.state[0];
+  const saveMemberId = sessionStorage.getItem('memberId');
+  const url = `/restaurant/${restaurantNumber}/reviews`;
+  const AVR_RATE = totalStar;
   const STAR_IDX_ARR = ['first', 'second', 'third', 'fourth', 'last'];
   const [starArr, setStarArr] = useState([0, 0, 0, 0, 0]);
   const [click, setClick] = useState<any>(2);
@@ -26,16 +36,32 @@ const review = () => {
   useEffect(() => {
     setStarArr(calcStarRates);
   }, []);
+  const [set, isSet] = useState<any>();
+  useEffect(() => {
+    axios
+      .post(url, {
+        restaurantId: restaurantNumber,
+        memberId: saveMemberId,
+      })
+      .then(res => {
+        console.log(res.data);
+        isSet(res.data.content);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <>
       <div className="flex flex-row">
-        <div className="ml-[28px] mt-[25px] h-[16px] font-bold text-[24px] leading-[33px] flex items-center">
-          금돈
+        <div className="ml-[28px] mt-[25px] w-[181px] h-[16px] font-bold text-[24px] leading-[33px] flex items-center">
+          {name}
         </div>
         {STAR_IDX_ARR.map((item, idx) => {
           return (
             <span
-              className="flex inline-flex items-center ml-[1px] mt-[19.4px]"
+              className="flex inline-flex items-center ml-[1px] mt-[12.4px]"
               key={`${item}_${idx}`}
             >
               <svg
@@ -63,35 +89,48 @@ const review = () => {
         <div className="ml-[5px] mt-[28px] w-[201px] h-[16px] font-[400] text-[14px] leading-[18px] flex items-center text-[#9F9F9F]">
           총 302명이 리뷰를 작성했어요.
         </div>
-        <div
-          onClick={() => {
-            clickPage(1);
-          }}
-          className={cn('ml-[94px] mt-[19px] w-[70px] h-[29px] border-[1px] rounded-[10px]', {
-            'border-[#FF611D] text-[#FF611D]': click === 1,
-            'border-[#9C9C9C] text-[#9C9C9C]': click !== 1,
-          })}
-        >
-          <div className="ml-[11.5px] mt-[2px] font-[400] text-[16px] leading-[21px] flex items-center">
-            최신순
-          </div>
-        </div>
-        <div
-          onClick={() => {
-            clickPage(2);
-          }}
-          className={cn('ml-[7px] mt-[19px] w-[70px] h-[29px] border-[1px]  rounded-[10px]', {
-            'border-[#FF611D] text-[#FF611D]': click === 2,
-            'border-[#9C9C9C] text-[#9C9C9C]': click !== 2,
-          })}
-        >
-          <div className="ml-[11.5px] mt-[2px] font-[400] text-[16px] leading-[21px] flex items-center">
-            추천순
+        <div className="mt-[16px] mr-[36px] w-[177px] h-[38px] bg-[#FF611D] rounded-[22px]">
+          <div className="flex flex-row">
+            <ReviewEdit className="ml-[16px] mt-[12.39px]" />
+            <div className="ml-[6.51px] mr-[17.46px] mt-[10.39px] w-[120px] h-[16px] leading-[20px] flex items-center fw-400 text-[14px] text-[#FFFFFF]">
+              리뷰 작성하러 가기
+            </div>
           </div>
         </div>
       </div>
-      <div className="ml-[25px] mt-[29px] w-[687px] h-[1px] border border-[#D5D5D5] bg-[#D5D5D5] "></div>
+      <div className="ml-[25px] mt-[20px] w-[687px] h-[1px] border border-[#D5D5D5] bg-[#D5D5D5] "></div>
       <div>
+        <div className="flex flex-row">
+          <div
+            onClick={() => {
+              clickPage(1);
+            }}
+            className={cn('ml-[21px] mt-[13px] w-[70px] h-[29px] border-[1px] rounded-[10px]', {
+              'border-[#FF611D] text-[#FF611D]': click === 1,
+              'border-[#9C9C9C] text-[#9C9C9C]': click !== 1,
+            })}
+          >
+            <div className="ml-[11.5px] mt-[2px] font-[400] text-[16px] leading-[21px] flex items-center">
+              최신순
+            </div>
+          </div>
+          <div
+            onClick={() => {
+              clickPage(2);
+            }}
+            className={cn('ml-[7px] mt-[13px] w-[70px] h-[29px] border-[1px]  rounded-[10px]', {
+              'border-[#FF611D] text-[#FF611D]': click === 2,
+              'border-[#9C9C9C] text-[#9C9C9C]': click !== 2,
+            })}
+          >
+            <div className="ml-[11.5px] mt-[2px] font-[400] text-[16px] leading-[21px] flex items-center">
+              추천순
+            </div>
+          </div>
+          <div className="mt-[20px] ml-[195px] mr-[26px] w-[351px] h-[16px] fw-400 text-[13px] leading-[17px] flex items-center text-[#9F9F9F]">
+            ※홍보 및 비방 등 부적절한 평가는 평점 산정에서 제외될수있습니다.
+          </div>
+        </div>
         <div className="grid grid-cols-3">
           <div className="ml-[25px] mt-[18px] w-[220px] h-[312px] border-[1px] border-[#D2D2D2] shadow-[1px_1px_4px_rgba(0,0,0,0.07)] rounded-[5px]">
             <div className="w-[39px] h-[40px] ml-[13px] mt-[15px] border rounded-full border-[#CCCCCC] bg-[#CCCCCC]">
@@ -161,6 +200,12 @@ const review = () => {
                 2022.12.14
               </div>
             </div>
+          </div>
+        </div>
+        <div className="mt-[13px] mb-[20px] ml-[649px] font-[400] text-[16px] leading-[22px] flex items-center cursor-pointer">
+          더보기
+          <div className="ml-[5px] w-[14px] h-[14px] rounded-full border border-[#FF611D] bg-[#FF611D]">
+            <MoreInfo className="ml-[2.22px] mt-[3.5px]" />
           </div>
         </div>
       </div>
