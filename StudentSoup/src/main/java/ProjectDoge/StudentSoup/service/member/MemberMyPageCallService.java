f60754325a128681f30a14b24c9667ce4a2ba020
Bombo <bombo96@chungwoon.ac.kr>
2023-01-26 21:18:00 +0900

    - feat : 마이페이지 음식점 리뷰 조회 서비스 로직 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/service/member/MemberMyPageCallService.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/service/member/MemberMyPageCallService.java
@@ -2,9 +2,11 @@ package ProjectDoge.StudentSoup.service.member;
 
 import ProjectDoge.StudentSoup.dto.member.MemberMyPageBoardDto;
 import ProjectDoge.StudentSoup.dto.member.MemberMyPageBoardReviewDto;
+import ProjectDoge.StudentSoup.dto.member.MemberMyPageRestaurantReviewDto;
 import ProjectDoge.StudentSoup.exception.member.MemberIdNotSentException;
 import ProjectDoge.StudentSoup.repository.board.BoardRepository;
 import ProjectDoge.StudentSoup.repository.boardreview.BoardReviewRepository;
+import ProjectDoge.StudentSoup.repository.restaurantreview.RestaurantReviewRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -18,18 +20,25 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberMyPageCallService {
     private final BoardRepository boardRepository;
     private final BoardReviewRepository boardReviewRepository;
+    private final RestaurantReviewRepository restaurantReviewRepository;
 
 
     @Transactional(readOnly = true, rollbackFor = Exception.class)
     public Page<MemberMyPageBoardReviewDto> callMyPageBoardReview(Long memberId, Pageable pageable){
         isNotNullMemberId(memberId);
-        return boardReviewRepository.callByMemberIdForMyPage(memberId, pageable);
+        return boardReviewRepository.findByMemberIdForMyPage(memberId, pageable);
     }
 
     @Transactional(readOnly = true, rollbackFor = Exception.class)
     public Page<MemberMyPageBoardDto> callMyPageBoard(Long memberId, Pageable pageable){
         isNotNullMemberId(memberId);
-        return boardRepository.callByMemberIdForMyPage(memberId, pageable);
+        return boardRepository.findByMemberIdForMyPage(memberId, pageable);
+    }
+
+    @Transactional(readOnly = true, rollbackFor = Exception.class)
+    public Page<MemberMyPageRestaurantReviewDto> callMyRestaurantReview(Long memberId, String cond, Pageable pageable){
+        isNotNullMemberId(memberId);
+        return restaurantReviewRepository.findByMemberIdForMyPage(memberId, cond, pageable);
     }
 
     private void isNotNullMemberId(Long memberId) {
