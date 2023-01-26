3f882a8c656e07ab80eac401e694feacdd681ed7
Bombo <bombo96@chungwoon.ac.kr>
2023-01-26 15:59:07 +0900

    - feat : 회원 마이페이지 게시판 댓글 조회 서비스 쿼리 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/boardreview/BoardReviewRepositoryCustom.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/boardreview/BoardReviewRepositoryCustom.java
@@ -1,7 +1,13 @@
 package ProjectDoge.StudentSoup.repository.boardreview;
 
+import ProjectDoge.StudentSoup.dto.member.MemberMyPageBoardReviewDto;
+import ProjectDoge.StudentSoup.entity.board.BoardReview;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface BoardReviewRepositoryCustom{
+import java.util.List;
+
+public interface BoardReviewRepositoryCustom {
+
+    List<MemberMyPageBoardReviewDto> callByMemberIdForMyPage(Long memberId, Pageable pageable);
 }
--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/boardreview/BoardReviewRepositoryImpl.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/boardreview/BoardReviewRepositoryImpl.java
@@ -1,5 +1,32 @@
 package ProjectDoge.StudentSoup.repository.boardreview;
 
 
+import ProjectDoge.StudentSoup.dto.member.MemberMyPageBoardReviewDto;
+import ProjectDoge.StudentSoup.dto.member.QMemberMyPageBoardReviewDto;
+import ProjectDoge.StudentSoup.entity.board.BoardReview;
+import ProjectDoge.StudentSoup.entity.board.QBoardReview;
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+import static ProjectDoge.StudentSoup.entity.board.QBoardReview.boardReview;
+
+@RequiredArgsConstructor
 public class BoardReviewRepositoryImpl implements BoardReviewRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<MemberMyPageBoardReviewDto> callByMemberIdForMyPage(Long memberId, Pageable pageable) {
+
+        return queryFactory.select(new QMemberMyPageBoardReviewDto(boardReview.content, boardReview.writeDate, boardReview.likedCount))
+                .from(boardReview)
+                .where(boardReview.member.memberId.eq(memberId))
+                .offset(pageable.getPageNumber())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
 }
