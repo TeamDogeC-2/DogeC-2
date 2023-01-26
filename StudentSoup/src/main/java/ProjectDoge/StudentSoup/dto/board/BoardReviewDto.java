076c4648522bfebc5c73c2f7ad6830a73b3dcb43
xogns4909 <xogns4909@naver.com>
2023-01-26 11:55:45 +0900

    -refactor : boardReview Dto 수정

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/dto/board/BoardReviewDto.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/dto/board/BoardReviewDto.java
@@ -1,5 +1,6 @@
 package ProjectDoge.StudentSoup.dto.board;
 
+import ProjectDoge.StudentSoup.entity.board.BoardReview;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,5 +20,14 @@ public class BoardReviewDto {
 
     private int level;
 
+    public BoardReviewDto createBoardReviewDto(BoardReview boardReview) {
+        this.boardReviewId = boardReview.getReviewId();
+        this.content = boardReview.getContent();
+        this.likeCount = boardReview.getLikedCount();
+        this.seq = boardReview.getSeq();
+        this.depth = boardReview.getDepth();
+        this.level = boardReview.getLevel();
+        return this;
+    }
 
 }
--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/dto/board/BoardReviewUpdateDto.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/dto/board/BoardReviewUpdateDto.java
@@ -9,26 +9,19 @@ import lombok.Setter;
 @Setter
 public class BoardReviewUpdateDto {
 
-
+    private Long boardReviewId;
     private Long boardId;
 
     private Long memberId;
 
     private  String content;
 
-    private int seq;
-
-    private int depth;
-
-    private int level;
 
     public BoardReviewUpdateDto createBoardReviewUpdateDto(BoardReview boardReview) {
+        this.boardReviewId = boardReview.getReviewId();
         this.boardId = boardReview.getBoard().getId();
         this.memberId = boardReview.getMember().getMemberId();
         this.content = boardReview.getContent();
-        this.seq = boardReview.getSeq();
-        this.depth = boardReview.getDepth();
-        this.level = boardReview.getLevel();
     return  this;
     }
 }
