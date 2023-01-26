d831460745f3bd2aa7f3538bfd8ffd62f9291954
Bombo <bombo96@chungwoon.ac.kr>
2023-01-26 20:46:59 +0900

    - docs : 마이 페이지 게시판 조회 응답 dto 추가

---
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/dto/member/MemberMyPageBoardDto.java
@@ -0,0 +1,23 @@
+package ProjectDoge.StudentSoup.dto.member;
+
+import com.querydsl.core.annotations.QueryProjection;
+
+public class MemberMyPageBoardDto {
+
+    private Long boardId;
+    private String writeDate;
+    private int likedCount;
+    private int viewCount;
+
+
+    public MemberMyPageBoardDto(){
+    }
+
+    @QueryProjection
+    public MemberMyPageBoardDto(Long boardId, String writeDate, int likedCount, int viewCount){
+        this.boardId = boardId;
+        this.writeDate = writeDate;
+        this.likedCount = likedCount;
+        this.viewCount = viewCount;
+    }
+}
