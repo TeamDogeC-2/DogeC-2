f148a13d30a47b0f3d8e4549816b0fdc7d5287ba
spsp8426 <spsp8426@naver.com>
2023-01-28 00:12:40 +0900

    FEAT : 리뷰 예외처리 추가 및 리뷰리스트 분리

--- StudentSoup/src/main/frontend/src/components/restaurant/reviewInfo.tsx
+++ StudentSoup/src/main/frontend/src/components/restaurant/reviewInfo.tsx
@@ -36,17 +36,7 @@ const review = (props: any) => {
     setClicked(clickStates);
   };
   const score = clicked.filter(Boolean).length;
-  const reviewButtonClick = () => {
-    isReviewClick(!reviewclick);
-    if (reviewclick) {
-      if (confirm('게시글 작성을 취소하시겠습니까? (작성중이던 글은 삭제됩니다.)')) {
-        setTextValue('');
-        isReviewClick(!reviewclick);
-      } else {
-        isReviewClick(reviewclick);
-      }
-    }
-  };
+
   const calcStarRates = () => {
     const tempStarRatesArr = [0, 0, 0, 0, 0];
     let starVerScore = (AVR_RATE * 120) / 5;
@@ -70,23 +60,44 @@ const review = (props: any) => {
     setCount(e.target.value.length);
     setMaxCount(maxCount);
   };
+  const reviewButtonClick = () => {
+    if (!saveMemberId) {
+      alert('로그인후 이용 가능한 기능입니다.');
+    } else isReviewClick(!reviewclick);
+    if (reviewclick) {
+      if (confirm('게시글 작성을 취소하시겠습니까? (작성중이던 글은 삭제됩니다.)')) {
+        setRating(0);
+        setHover(0);
+        setClicked([false, false, false, false, false]);
+        setTextValue('');
+        isReviewClick(!reviewclick);
+      } else {
+        isReviewClick(reviewclick);
+      }
+    }
+  };
   const cancelReviewValue = (e: any) => {
     if (confirm('게시글 작성을 취소하시겠습니까? (작성중이던 글은 삭제됩니다.)')) {
+      setRating(0);
+      setHover(0);
+      setClicked([false, false, false, false, false]);
       setTextValue('');
       isReviewClick(!reviewclick);
     } else {
       isReviewClick(reviewclick);
     }
   };
-  console.log(score);
   const handleReviewValue = (e: any) => {
     if (count < 5) {
       alert('5자 이상 입력해야 합니다.');
     } else if (score === 0) {
-      alert('별점은 최소 1점 입니다.');
+      alert('별점은 최소 1점부터 가능합니다.');
     } else {
       alert('리뷰 작성이 완료되었습니다.');
       isReviewClick(!reviewclick);
+      setRating(0);
+      setHover(0);
+      setClicked([false, false, false, false, false]);
       setTextValue('');
     }
   };
@@ -259,86 +270,7 @@ const review = (props: any) => {
         ) : (
           ''
         )}
-        <div className="grid grid-cols-3">
-          <div className="ml-[25px] mt-[18px] w-[220px] h-[312px] border-[1px] border-[#D2D2D2] shadow-[1px_1px_4px_rgba(0,0,0,0.07)] rounded-[5px]">
-            <div className="w-[39px] h-[40px] ml-[13px] mt-[15px] border rounded-full border-[#CCCCCC] bg-[#CCCCCC]">
-              <div className="ml-[44px] mt-[6px] w-[82px] h-[10px] font-[400] text-[12px] leading-[20px] flex items-center">
-                냠냠쿠키
-              </div>
-              <SmallStar className="ml-[44px] mt-[5.23px]" />
-              <div className="ml-[5px] mt-[23.12px] w-[180px] h-[120px] border border-[#DDDDDD] rounded-[10px] text-center bg-[#DDDDDD]">
-                이미지
-              </div>
-              <div className="ml-[5px] mt-[11px] w-[184px] h-[62px] font-[400] text-[12px] leading-[16px] text-[#6B6B6B]">
-                진짜진짜로 맛있어요 ㅠㅠ 치즈카츠의 치즈는 진짜 모짜렐라치즈 맛으로 고소하면서 쫙좍
-                늘어나고 가츠동에 밥 양념이...
-              </div>
-              <div className="flex flex-row">
-                <div className="ml-[7px] mt-[18px] h-[16px] font-[400] text-[11px] leading-[14px] flex items-center text-[#A5A5A5]">
-                  2022.12.14
-                  <ReviewSmallHeart className="ml-[95px]" />
-                  <div className="mb-[1px] ml-[3.62px] h-[9px] font-normal text-[11px] leading-[14px] flex items-center text-[#A5A5A5]">
-                    52
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="ml-[12px] mt-[18px] w-[220px] h-[312px] border-[1px] border-[#D2D2D2] shadow-[1px_1px_4px_rgba(0,0,0,0.07)] rounded-[5px]">
-            <div className="w-[39px] h-[40px] ml-[13px] mt-[15px] border rounded-full border-[#CCCCCC] bg-[#CCCCCC]">
-              <div className="ml-[44px] mt-[6px] w-[82px] h-[10px] font-[400] text-[12px] leading-[20px] flex items-center">
-                냠냠쿠키
-              </div>
-              <SmallStar className="ml-[44px] mt-[5.23px]" />
-              <div className="ml-[5px] mt-[23.12px] w-[180px] h-[120px] border border-[#DDDDDD] rounded-[10px] text-center bg-[#DDDDDD]">
-                이미지
-              </div>
-              <div className="ml-[5px] mt-[11px] w-[184px] h-[62px] font-[400] text-[12px] leading-[16px] text-[#6B6B6B]">
-                진짜진짜로 맛있어요 ㅠㅠ 치즈카츠의 치즈는 진짜 모짜렐라치즈 맛으로 고소하면서 쫙좍
-                늘어나고 가츠동에 밥 양념이...
-              </div>
-              <div className="flex flex-row">
-                <div className="ml-[7px] mt-[18px] h-[16px] font-[400] text-[11px] leading-[14px] flex items-center text-[#A5A5A5]">
-                  2022.12.14
-                  <ReviewSmallHeart className="ml-[95px]" />
-                  <div className="mb-[1px] ml-[3.62px] h-[9px] font-normal text-[11px] leading-[14px] flex items-center text-[#A5A5A5]">
-                    52
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="mt-[18px] w-[220px] h-[312px] border-[1px] border-[#D2D2D2] shadow-[1px_1px_4px_rgba(0,0,0,0.07)] rounded-[5px]">
-            <div className="w-[39px] h-[40px] ml-[13px] mt-[15px] border rounded-full border-[#CCCCCC] bg-[#CCCCCC]">
-              <div className="ml-[44px] mt-[6px] w-[82px] h-[10px] font-[400] text-[12px] leading-[20px] flex items-center">
-                냠냠쿠키
-              </div>
-              <SmallStar className="ml-[44px] mt-[5.23px]" />
-              <div className="ml-[5px] mt-[23.12px] w-[180px] h-[120px] border border-[#DDDDDD] rounded-[10px] text-center bg-[#DDDDDD]">
-                이미지
-              </div>
-              <div className="ml-[5px] mt-[11px] w-[184px] h-[62px] font-[400] text-[12px] leading-[16px] text-[#6B6B6B]">
-                진짜진짜로 맛있어요 ㅠㅠ 치즈카츠의 치즈는 진짜 모짜렐라치즈 맛으로 고소하면서 쫙좍
-                늘어나고 가츠동에 밥 양념이...
-              </div>
-              <div className="flex flex-row">
-                <div className="ml-[7px] mt-[18px] h-[16px] font-[400] text-[11px] leading-[14px] flex items-center text-[#A5A5A5]">
-                  2022.12.14
-                  <ReviewSmallHeart className="ml-[95px]" />
-                  <div className="mb-[1px] ml-[3.62px] h-[9px] font-normal text-[11px] leading-[14px] flex items-center text-[#A5A5A5]">
-                    52
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="mt-[13px] mb-[20px] ml-[649px] font-[400] text-[16px] leading-[22px] flex items-center cursor-pointer">
-          더보기
-          <div className="w-[14px] h-[14px] rounded-full border border-[#FF611D] bg-[#FF611D]">
-            <MoreInfo className="ml-[2.22px] mt-[3.5px]" />
-          </div>
-        </div>
+        <ReviewWrite />
       </div>
     </>
   );
