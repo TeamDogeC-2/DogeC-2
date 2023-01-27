fdd42f00b3ede86ead2aa20319fee3f08bace916
yerim <pppp5097@naver.com>
2023-01-27 23:42:31 +0900

    - feat : 게시판 페이지 좌측 메뉴 UI 구현

---
+++ StudentSoup/src/main/frontend/src/components/board/boardMain.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import MypageNavbar from '../common/mypageNavbar';
+import BoardSidebar from './boardSidebar';
+
+export const enum BORDER_MENU {
+  ALL = '전체게시판', // 전체게시판
+  FREE = '자유게시판', // 자유게시판
+  CONSULTING = '취업/상담게시판', // 취업/상담 게시판
+  TIP = 'TIP 게시판', // TIP 게시판
+  NOTICE = '공지사항', // 공지사항
+}
+
+const BoardMain = () => {
+  const [menu, setMenu] = useState<BORDER_MENU>(BORDER_MENU.ALL);
+
+  return (
+    <>
+      <MypageNavbar />
+      <div className="flex flex-row">
+        <div className="z-[2]">
+          <BoardSidebar menu={menu} setMenu={setMenu} />
+        </div>
+        <div className="w-full"></div>
+      </div>
+    </>
+  );
+};
+
+export default BoardMain;
