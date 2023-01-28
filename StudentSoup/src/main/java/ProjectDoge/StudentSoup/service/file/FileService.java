f6692648816cf4aea69afe92e4f0c4627f223ec0
Bombo <bombo96@chungwoon.ac.kr>
2023-01-28 16:32:46 +0900

    - feat : File 저장 서비스 인터페이스 추가

---
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/service/file/FileService.java
@@ -0,0 +1,19 @@
+package ProjectDoge.StudentSoup.service.file;
+
+import ProjectDoge.StudentSoup.dto.file.UploadFileDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface FileService {
+
+    Long join(MultipartFile multipartFile);
+
+    List<UploadFileDto> createUploadFileDtoList(List<MultipartFile> multipartFileList);
+
+    UploadFileDto storeFile(MultipartFile multipartFile) throws IOException;
+
+    String createStoreFileName(String originalFileName);
+
+}
