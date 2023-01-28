46afafc624bf4a6f8e9817ac31e46a7300061046
spsp8426 <spsp8426@naver.com>
2023-01-28 15:16:54 +0900

    FEAT : 이미지 데이터 추가

--- StudentSoup/src/main/frontend/src/components/restaurant/reviewList.tsx
+++ StudentSoup/src/main/frontend/src/components/restaurant/reviewList.tsx
@@ -7,6 +7,7 @@ import StarTest from './reviewStarView';
 
 const reviewWrite = () => {
   const [reviewList, setReviewList] = useState<any>([]);
+  const [memberImage, setMemberImage] = useState<any>([]);
   const state = useLocation<any>();
   const restaurantNumber = state.state[0];
   const saveMemberId = sessionStorage.getItem('memberId');
@@ -18,13 +19,14 @@ const reviewWrite = () => {
         memberId: saveMemberId,
       })
       .then(res => {
+        console.log(res.data.content);
+        setMemberImage(res.data.content.memberProfileImageName);
         setReviewList(res.data.content);
       })
       .catch(err => {
         console.error(err);
       });
   }, []);
-
   return (
     <>
       <div className="ml-[25px] mt-[20px] w-[687px] h-[1px] border border-[#D5D5D5] bg-[#D5D5D5] "></div>
@@ -35,25 +37,36 @@ const reviewWrite = () => {
               key={school.restaurantReviewId}
               className="ml-[20px] mt-[18px] w-[220px] h-[312px] border-[1px] border-[#D2D2D2] shadow-[1px_1px_4px_rgba(0,0,0,0.07)] rounded-[5px]"
             >
-              <div className="w-[39px] h-[40px] ml-[13px] mt-[15px] border rounded-full border-[#CCCCCC] bg-[#CCCCCC]">
-                <div className="ml-[44px] mt-[6px] w-[82px] h-[10px] font-[400] text-[12px] leading-[20px] flex items-center">
-                  {school.nickName}
-                </div>
-                <StarTest {...school} school={school} />
-                <div className="ml-[5px] mt-[23.12px] w-[180px] h-[120px] border border-[#DDDDDD] rounded-[10px] text-center bg-[#DDDDDD]">
-                  이미지
-                </div>
-                <div className="ml-[5px] mt-[11px] w-[184px] h-[62px] font-[400] text-[12px] leading-[16px] text-[#6B6B6B]">
-                  {school.content}
-                </div>
-                <div className="flex flex-row w-[200px]">
-                  <div className="mt-[18px] w-[57px] h-[16px] font-normal text-[11px] leading-[14px] flex items-center text-[#A5A5A5]">
-                    {school.writeDate}
+              <div className="flex flex-row">
+                {school.memberProfileImageName ? (
+                  <img
+                    key={school.restaurantReviewId}
+                    src={`/image/${school.memberProfileImageName}`}
+                    className="w-[39px] h-[40px] ml-[13px] mt-[15px] border rounded-full border-[#9C9C9C]"
+                  />
+                ) : (
+                  <div className="w-[39px] h-[40px] ml-[13px] mt-[15px] border rounded-full border-[#9C9C9C] bg-[#9C9C9C]"></div>
+                )}
+                <div className="">
+                  <div className="ml-[4px] mt-[21px] w-[82px] h-[10px] font-[400] text-[12px] leading-[20px] flex items-center">
+                    {school.nickName}
                   </div>
-                  <ReviewSmallHeart className="ml-[99px] mt-[18px]" />
-                  <div className="ml-[3.31px] mt-[15px] w-[18px] h-[16px] font-normal text-[11px] leading-[14px] flex items-center text-[#A5A5A5]">
-                    {school.likedCount}
-                  </div>
+                  <StarTest {...school} school={school} />
+                </div>
+              </div>
+              <div className="ml-[20px] mt-[13.12px] w-[180px] h-[120px] border border-[#DDDDDD] rounded-[10px] text-center bg-[#DDDDDD]">
+                이미지
+              </div>
+              <div className="ml-[20px] mt-[11px] w-[184px] h-[62px] font-[400] text-[12px] leading-[16px] text-[#6B6B6B]">
+                {school.content}
+              </div>
+              <div className="flex flex-row w-[209px]">
+                <div className="ml-[20px] mt-[18px] h-[16px] font-normal text-[11px] leading-[14px] flex items-center text-[#A5A5A5]">
+                  {school.writeDate}
+                </div>
+                <ReviewSmallHeart className="ml-[99px] mt-[18px]" />
+                <div className="ml-[3.31px] mt-[15px] w-[18px] h-[16px] font-normal text-[11px] leading-[14px] flex items-center text-[#A5A5A5]">
+                  {school.likedCount}
                 </div>
               </div>
             </div>
