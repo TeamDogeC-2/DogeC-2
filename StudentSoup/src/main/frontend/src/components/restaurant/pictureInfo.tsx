f1c6a45e5fdd5b6e2b03c7b4800d9ca52c076f80
spsp8426 <spsp8426@naver.com>
2023-01-30 00:48:46 +0900

    FEAT : 음식점 사진 보기 추가

--- StudentSoup/src/main/frontend/src/components/restaurant/pictureInfo.tsx
+++ StudentSoup/src/main/frontend/src/components/restaurant/pictureInfo.tsx
@@ -1,54 +1,86 @@
-import React from 'react';
-import { ReactComponent as RightIcon } from '../../img/icon_right.svg';
-import { ReactComponent as LeftIcon } from '../../img/icon_left.svg';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { ReactComponent as MoreInfo } from '../../img/moreicon.svg';
+import { useLocation } from 'react-router-dom';
 
 const pictureInfo = () => {
+  const state = useLocation<any>();
+  const restaurantNumber = state.state[0];
+  const [image, setImage] = useState<any>([]);
+  const [size, setSize] = useState<number>(6);
+  const [totalSize, setTotalSize] = useState<any>();
+  const [moreButtonClick, setMoreButtonClick] = useState<number>(0);
+  const url = `/restaurant/${restaurantNumber}/reviews/image`;
+  useEffect(() => {
+    axios
+      .get(url, { params: { size } })
+      .then(res => {
+        setImage(res.data.content);
+        setTotalSize(res.data.totalElements);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, [size]);
+  const handleClickButton = () => {
+    setMoreButtonClick(moreButtonClick + 1);
+    setSize(size + 6);
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  });
+  const handleScroll = () => {
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollTop = document.documentElement.scrollTop;
+    const clientHeight = document.documentElement.clientHeight;
+
+    if (scrollTop + clientHeight >= scrollHeight && moreButtonClick !== 0) {
+      setMoreButtonClick(moreButtonClick + 1);
+      setSize(size + 6);
+      if (size >= totalSize) {
+        setSize(totalSize);
+      }
+    }
+  };
+  console.log(size);
+
   return (
     <>
-      <div className="grid grid-cols-[232px_minmax(232px,_0fr)_1px] ml-[25px]">
-        <div className="mt-[14px] w-[226px] h-[190px] border border-[#6F2D] bg-[#6F2D]">1</div>
-        <div className="mt-[14px] w-[226px] h-[190px] border border-[#6F2D] bg-[#6F2D]">2</div>
-        <div className="mt-[14px] w-[226px] h-[190px] border border-[#6F2D] bg-[#6F2D]">3</div>
-        <div className="mt-[6px] w-[226px] h-[190px] border border-[#6F2D] bg-[#6F2D]">4</div>
-        <div className="mt-[6px] w-[226px] h-[190px] border border-[#6F2D] bg-[#6F2D]">5</div>
-        <div className="mt-[6px] w-[226px] h-[190px] border border-[#6F2D] bg-[#6F2D]">6</div>
-      </div>
-      <div className="mt-[22px] mb-[20px] ml-[649px] font-[400] text-[16px] leading-[22px] flex items-center cursor-pointer">
-        더보기
-        <div className="w-[14px] h-[14px] rounded-full border border-[#FF611D] bg-[#FF611D]">
-          <MoreInfo className="ml-[2.22px] mt-[3.5px]" />
-        </div>
-      </div>
-      {/* <div className="flex flex-row">
-        <LeftIcon className="ml-[234px] mt-[55.63px]" />
-        <div className="ml-[17.46px] mt-[43px] w-[38px] h-[38px] border border-[#FF611D] rounded-full">
-          <div className="ml-[12.3px] mt-[4px] font-[400] text-[20px] leading-[26px] flex text-[#FF611D]">
-            1
+      {image.length ? (
+        <>
+          <div className="grid grid-cols-[226px_minmax(226px,_0fr)_226px] mt-[14px] ml-[25px] gap-[6px]">
+            {image.map((school: any) => (
+              <>
+                <img
+                  key={school}
+                  src={`/image/${school}`}
+                  className='className="mt-[14px] w-[226px] h-[190px]'
+                />
+              </>
+            ))}
           </div>
-        </div>
-        <div className="ml-[9.5px] mt-[43px] w-[38px] h-[38px] border border-[#FF611D] rounded-full">
-          <div className="ml-[12.3px] mt-[4px] font-[400] text-[20px] leading-[26px] flex text-[#FF611D]">
-            2
-          </div>
-        </div>
-        <div className="ml-[9.5px] mt-[43px] w-[38px] h-[38px] border border-[#FF611D] rounded-full">
-          <div className="ml-[12.3px] mt-[4px] font-[400] text-[20px] leading-[26px] flex text-[#FF611D]">
-            3
-          </div>
-        </div>
-        <div className="ml-[9.5px] mt-[43px] w-[38px] h-[38px] border border-[#FF611D] rounded-full">
-          <div className="ml-[12.3px] mt-[4px] font-[400] text-[20px] leading-[26px] flex text-[#FF611D]">
-            4
-          </div>
-        </div>
-        <div className="ml-[9.5px] mt-[43px] w-[38px] h-[38px] border border-[#FF611D] rounded-full">
-          <div className="ml-[12.3px] mt-[4px] font-[400] text-[20px] leading-[26px] flex text-[#FF611D]">
-            5
-          </div>
-        </div>
-        <RightIcon className="ml-[15px] mt-[53.63px]" />
-      </div> */}
+          {moreButtonClick === 0 && totalSize > 6 ? (
+            <>
+              <div
+                onClick={handleClickButton}
+                className="mt-[14px] mb-[20px] ml-[649px] font-[400] text-[16px] leading-[22px] flex items-center cursor-pointer"
+              >
+                더보기
+                <div className="w-[14px] h-[14px] rounded-full border border-[#FF611D] bg-[#FF611D]">
+                  <MoreInfo className="ml-[2.22px] mt-[3.5px]" />
+                </div>
+              </div>
+            </>
+          ) : (
+            <div className="h-[50px]"></div>
+          )}
+        </>
+      ) : (
+        <div>이미지가 없습니다. 리뷰를 달아보세요!</div>
+      )}
     </>
   );
 };
