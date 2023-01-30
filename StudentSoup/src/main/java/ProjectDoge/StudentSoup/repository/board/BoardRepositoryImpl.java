0cad31b99392d0e99748ee78297fd1e6e69fdbfd
xogns4909 <xogns4909@naver.com>
2023-01-30 13:33:03 +0900

    -refactor : 날짜 정렬 기법 수정

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/board/BoardRepositoryImpl.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/board/BoardRepositoryImpl.java
@@ -150,7 +150,7 @@ public class BoardRepositoryImpl implements BoardRepositoryCustom {
                 .from(board)
                 .where(board.school.id.eq(schoolId),
                         board.boardCategory.eq(BoardCategory.TIP))
-                .orderBy(board.likedCount.desc(),board.writeDate.asc())
+                .orderBy(board.likedCount.desc(),board.writeDate.desc())
                 .offset(0)
                 .limit(4)
                 .fetch();
@@ -215,7 +215,7 @@ public class BoardRepositoryImpl implements BoardRepositoryCustom {
         else if(BoardSortedCase.VIEW.getValue() == sorted){
             return board.view.desc();
         }
-        return board.updateDate.asc();
+        return board.writeDate.desc();
     }
 
     @Override
