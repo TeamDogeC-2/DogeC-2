6c4783b09623c08b095091a53828d5e8aa188272
Bombo <bombo96@chungwoon.ac.kr>
2023-01-30 11:28:17 +0900

    - docs : 음식점 추가 시 주소 및 배달 여부 추가

--- StudentSoup/src/test/java/ProjectDoge/StudentSoup/restaurant/RestaurantMenuEntityTest.java
+++ StudentSoup/src/test/java/ProjectDoge/StudentSoup/restaurant/RestaurantMenuEntityTest.java
@@ -65,13 +65,12 @@ public class RestaurantMenuEntityTest {
     Long schoolId = 0L;
 
 
-
     @BeforeEach
     void 학교등록_음식점등록() throws Exception {
         SchoolFormDto school = createSchool();
         schoolId = schoolRegisterService.join(school);
         RestaurantFormDto restaurantDto = createRestaurant();
-        restaurantId = restaurantRegisterService.join(restaurantDto, multipartFile);
+        restaurantId = restaurantRegisterService.join(restaurantDto);
 
         MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
         mockMvc.perform(MockMvcRequestBuilders
@@ -80,9 +79,9 @@ public class RestaurantMenuEntityTest {
     }
 
     @Test
-    void 메뉴등록_테스트() throws Exception{
+    void 메뉴등록_테스트() throws Exception {
         //given
-        RestaurantMenuFormDto restaurantMenuFormDto = createRestaurantMenuDto(restaurantId,"메뉴1", RestaurantMenuCategory.Main,10000);
+        RestaurantMenuFormDto restaurantMenuFormDto = createRestaurantMenuDto(restaurantId, "메뉴1", RestaurantMenuCategory.Main, 10000);
         //when
         Long restaurantMenuId = restaurantMenuRegisterService.join(restaurantMenuFormDto, multipartFile);
         Restaurant restaurant = restaurantFindService.findOne(restaurantId);
@@ -90,33 +89,34 @@ public class RestaurantMenuEntityTest {
         //then
         assertThat(restaurantMenuId).isEqualTo(restaurantMenu.getId());
     }
+
     @Test
-    void 메뉴등록시_음식점없음(){
+    void 메뉴등록시_음식점없음() {
         //given
         Long errorRestaurantId = 0L;
-        RestaurantMenuFormDto restaurantMenuFormDto = createRestaurantMenuDto(errorRestaurantId,"메뉴1", RestaurantMenuCategory.Main,10000);
+        RestaurantMenuFormDto restaurantMenuFormDto = createRestaurantMenuDto(errorRestaurantId, "메뉴1", RestaurantMenuCategory.Main, 10000);
         //then
-        assertThatThrownBy(()->restaurantMenuRegisterService.join(restaurantMenuFormDto, multipartFile))
+        assertThatThrownBy(() -> restaurantMenuRegisterService.join(restaurantMenuFormDto, multipartFile))
                 .isInstanceOf(RestaurantNotFoundException.class);
     }
 
     @Test
-    void 메뉴중복테스트(){
+    void 메뉴중복테스트() {
         //given
-        RestaurantMenuFormDto restaurantMenuFormDto = createRestaurantMenuDto(restaurantId,"메뉴1", RestaurantMenuCategory.Main,10000);
+        RestaurantMenuFormDto restaurantMenuFormDto = createRestaurantMenuDto(restaurantId, "메뉴1", RestaurantMenuCategory.Main, 10000);
         restaurantMenuRegisterService.join(restaurantMenuFormDto, multipartFile);
-        RestaurantMenuFormDto duplicateRestaurantMenuFormDto = createRestaurantMenuDto(restaurantId,"메뉴1", RestaurantMenuCategory.Main,10000);
+        RestaurantMenuFormDto duplicateRestaurantMenuFormDto = createRestaurantMenuDto(restaurantId, "메뉴1", RestaurantMenuCategory.Main, 10000);
         //then
-        assertThatThrownBy(()->restaurantMenuRegisterService.join(duplicateRestaurantMenuFormDto, multipartFile))
+        assertThatThrownBy(() -> restaurantMenuRegisterService.join(duplicateRestaurantMenuFormDto, multipartFile))
                 .isInstanceOf(RestaurantMenuValidationException.class);
     }
 
     private RestaurantMenuFormDto createRestaurantMenuDto(Long restaurantId, String name, RestaurantMenuCategory category, int cost) {
-       RestaurantMenuFormDto restaurantMenuFormDto = new RestaurantMenuFormDto().createRestaurantMenuDto(restaurantId,name,category,cost);
-       return restaurantMenuFormDto;
+        RestaurantMenuFormDto restaurantMenuFormDto = new RestaurantMenuFormDto().createRestaurantMenuDto(restaurantId, name, category, cost);
+        return restaurantMenuFormDto;
     }
 
-    private SchoolFormDto createSchool(){
+    private SchoolFormDto createSchool() {
         SchoolFormDto dto = new SchoolFormDto();
         dto.setSchoolName("테스트 학교");
         dto.setSchoolCoordinate("테스트 학교 좌표");
@@ -125,13 +125,13 @@ public class RestaurantMenuEntityTest {
 
 
     private RestaurantFormDto createRestaurant() {
-        RestaurantFormDto dto = createRestaurantDto("음식점1","주소", RestaurantCategory.ASIAN,LocalTime.now(),LocalTime.now(),schoolId,"좌표값",new ImageFile(),"전화번호","태그","디테일");
-        return  dto;
+        RestaurantFormDto dto = createRestaurantDto("음식점1", "주소", RestaurantCategory.ASIAN, LocalTime.now(), LocalTime.now(), schoolId, "좌표값", new ImageFile(), "전화번호", "태그", "디테일", "Y");
+        return dto;
     }
 
-    private RestaurantFormDto createRestaurantDto(String name, String address, RestaurantCategory category , LocalTime startTime, LocalTime endTime, Long schoolId, String coordinate, ImageFile file, String tel, String tag, String detail) {
+    private RestaurantFormDto createRestaurantDto(String name, String address, RestaurantCategory category, LocalTime startTime, LocalTime endTime, Long schoolId, String coordinate, ImageFile file, String tel, String tag, String detail, String isDelivery) {
         RestaurantFormDto dto = new RestaurantFormDto();
-        dto.createRestaurantFormDto(name,address,category,startTime,endTime,schoolId,coordinate,file,tel,tag,detail);
+        dto.createRestaurantFormDto(name, address, category, startTime, endTime, schoolId, coordinate, file, tel, tag, detail, isDelivery);
         return dto;
     }
 }
