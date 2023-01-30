20f4d2c8aa5b28f51fb3e30df2d7107206a06718
kinjin <inclined37@gmail.com>
2023-01-30 12:50:26 +0900

    DOCS: 필요없는 주석과 불필요한 반복된 코드 제거

--- StudentSoup/src/main/frontend/src/components/restaurant/restaurantMain.tsx
+++ StudentSoup/src/main/frontend/src/components/restaurant/restaurantMain.tsx
@@ -2,11 +2,6 @@ import { useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import cn from 'clsx';
 
-interface Category {
-  id: string;
-  name: string;
-}
-
 const RestaurantMain = () => {
   const history = useHistory();
   const state = useLocation<any>();
@@ -24,7 +19,7 @@ const RestaurantMain = () => {
           </div>
           <button
             id="filter-button"
-            className="w-[110px] h-[39px] flex justify-center items-center gap-x-1 rounded-[23.5px] text-[20px] font-semibold text-[#FF611D] bg-white"
+            className="dropdown w-[110px] h-[39px] flex justify-center items-center gap-x-1 rounded-[23.5px] text-[20px] font-semibold text-[#FF611D] bg-white"
           >
             <svg
               width="17"
@@ -43,7 +38,7 @@ const RestaurantMain = () => {
         </div>
         <div className="w-[1039px] pb-[47px] mb-[58px] mx-auto rounded-[10px] drop-shadow-md bg-white">
           <div className="w-[810px] mx-auto py-[31px] flex flex-wrap gap-x-[20px] gap-y-[32px]">
-            {/* TODO: 컴포넌트 반복문 리팩토링 할 예정 / 활성된 카테고리만 주황색바탕 흰글씨로 class 추가 */}
+            {/* TODO: 활성된 카테고리만 주황색바탕 흰글씨로 class 추가 */}
             <input
               type="button"
               className={cn(
@@ -149,69 +144,6 @@ const RestaurantMain = () => {
           <div className="w-[810px] mx-auto grid grid-cols-2 gap-x-[46px] justify-center place-content-stretch">
             <div>
               {' '}
-              {/* onClick={}> */}
-              <div className="w-[382px] h-[225px] rounded-[10px] bg-gray-100">가게이미지</div>
-              <div className="mt-[20px] text-[28px] text-[#262626]">
-                가게 이름 적는 곳<span className="px-2 text-[28px] text-[#FF611D]">4.3</span>
-                <div className="text-[20px] text-[#696969]">
-                  제물포역/양식, 파스타, 피자, 리조또
-                </div>
-                <div className="flex gap-x-1 items-center text-[20px] text-[#262626]">
-                  <span>
-                    <svg
-                      width="14"
-                      height="10"
-                      viewBox="0 0 14 10"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="inline-block"
-                    >
-                      <path
-                        d="M7.28072 0.729492C4.24718 0.729492 1.65654 2.61635 0.606934 5.2798C1.65654 7.94325 4.24718 9.83011 7.28072 9.83011C10.3143 9.83011 12.9049 7.94325 13.9545 5.2798C12.9049 2.61635 10.3143 0.729492 7.28072 0.729492ZM7.28072 8.31334C5.60621 8.31334 4.24718 6.95432 4.24718 5.2798C4.24718 3.60529 5.60621 2.24626 7.28072 2.24626C8.95524 2.24626 10.3143 3.60529 10.3143 5.2798C10.3143 6.95432 8.95524 8.31334 7.28072 8.31334ZM7.28072 3.45968C6.27359 3.45968 5.4606 4.27267 5.4606 5.2798C5.4606 6.28694 6.27359 7.09993 7.28072 7.09993C8.28786 7.09993 9.10085 6.28694 9.10085 5.2798C9.10085 4.27267 8.28786 3.45968 7.28072 3.45968Z"
-                        fill="black"
-                      />
-                    </svg>
-                    조회수
-                  </span>
-                  <span>
-                    <svg
-                      width="15"
-                      height="13"
-                      viewBox="0 0 15 13"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="inline-block"
-                    >
-                      <path
-                        d="M11.7028 8.08683C11.5215 8.25855 11.4382 8.5069 11.4795 8.75047L12.1017 12.1166C12.1542 12.4019 12.031 12.6906 11.7868 12.8555C11.5474 13.0265 11.229 13.047 10.9679 12.9102L7.86799 11.3298C7.76021 11.2737 7.64052 11.2436 7.51804 11.2401H7.32836C7.26257 11.2497 7.19818 11.2702 7.13939 11.3017L4.03879 12.8897C3.88551 12.9649 3.71194 12.9916 3.54186 12.9649C3.12751 12.8883 2.85105 12.5024 2.91894 12.0953L3.54186 8.72926C3.58315 8.48364 3.49986 8.23392 3.31859 8.05946L0.791218 5.66488C0.579846 5.46442 0.506355 5.16339 0.602943 4.89178C0.69673 4.62085 0.936099 4.42313 1.22516 4.37865L4.70371 3.88537C4.96827 3.85869 5.20064 3.70133 5.31963 3.46872L6.85243 0.396816C6.88882 0.328399 6.93572 0.265456 6.99241 0.212091L7.0554 0.1642C7.08829 0.128623 7.12609 0.0992039 7.16808 0.0752581L7.24438 0.0478915L7.36336 0H7.65802C7.92119 0.0266824 8.15286 0.18062 8.27394 0.410499L9.82704 3.46872C9.93902 3.69244 10.1567 3.84774 10.408 3.88537L13.8865 4.37865C14.1805 4.4197 14.4261 4.61811 14.5234 4.89178C14.6151 5.16613 14.536 5.46716 14.3205 5.66488L11.7028 8.08683Z"
-                        fill="#FFB21D"
-                      />
-                    </svg>
-                    별점
-                  </span>
-                  <span>
-                    <svg
-                      width="13"
-                      height="12"
-                      viewBox="0 0 13 12"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="inline-block"
-                    >
-                      <path
-                        d="M6.14663 11.7162L7.01024 10.93C10.0775 8.14861 12.1025 6.31419 12.1025 4.06286C12.1025 2.22844 10.6612 0.787109 8.82679 0.787109C7.79046 0.787109 6.79583 1.26954 6.14663 2.03189C5.49744 1.26954 4.5028 0.787109 3.46647 0.787109C1.63205 0.787109 0.190722 2.22844 0.190722 4.06286C0.190722 6.31419 2.21573 8.14861 5.28302 10.936L6.14663 11.7162Z"
-                        fill="#FF611D"
-                      />
-                    </svg>
-                    좋아요수
-                  </span>
-                </div>
-              </div>
-            </div>
-            {/* 임시로 두 번 반복 -> 나중에 반복문 작성해줄 예정 */}
-            <div>
-              {' '}
-              {/* onClick={}> */}
               <div className="w-[382px] h-[225px] rounded-[10px] bg-gray-100">가게이미지</div>
               <div className="mt-[20px] text-[28px] text-[#262626]">
                 가게 이름 적는 곳<span className="px-2 text-[28px] text-[#FF611D]">4.3</span>
