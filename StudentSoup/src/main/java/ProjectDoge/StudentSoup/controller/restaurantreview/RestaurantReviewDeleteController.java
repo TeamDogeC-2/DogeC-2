ae6742c42ff28ec018bf43ce5ae5db5403ec03f6
Bombo <bombo96@chungwoon.ac.kr>
2023-01-31 19:13:24 +0900

    - feat : 음식점 리뷰 삭제 응답 데이터 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/restaurantreview/RestaurantReviewDeleteController.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/restaurantreview/RestaurantReviewDeleteController.java
@@ -1,6 +1,7 @@
 package ProjectDoge.StudentSoup.controller.restaurantreview;
 
 import ProjectDoge.StudentSoup.dto.restaurantreview.RestaurantReviewDeleteDto;
+import ProjectDoge.StudentSoup.dto.restaurantreview.RestaurantReviewRegRespDto;
 import ProjectDoge.StudentSoup.service.restaurantreview.RestaurantReviewDeleteService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,13 +19,16 @@ public class RestaurantReviewDeleteController {
     private final RestaurantReviewDeleteService restaurantReviewDeleteService;
 
     @DeleteMapping("/review")
-    public ResponseEntity<ConcurrentHashMap<String, String>> deleteRestaurantReview(
+    public ResponseEntity<ConcurrentHashMap<String, Object>> deleteRestaurantReview(
             @PathVariable Long restaurantId,
             @RequestBody RestaurantReviewDeleteDto dto){
         log.info("리뷰 삭제가 호출되었습니다.");
-        ConcurrentHashMap<String, String> resultMap = restaurantReviewDeleteService.deleteRestaurantReview(
+        ConcurrentHashMap<String, Object> resultMap = restaurantReviewDeleteService.deleteRestaurantReview(
                 dto.getRestaurantReviewId(),
                 dto.getMemberId());
+
+        RestaurantReviewRegRespDto respDto = restaurantReviewDeleteService.starUpdate(restaurantId);
+        resultMap.put("data", respDto);
 
         return ResponseEntity.ok(resultMap);
     }
