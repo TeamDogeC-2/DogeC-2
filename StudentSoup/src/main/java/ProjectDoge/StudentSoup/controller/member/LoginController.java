346b31f0530087bd6314eabacd6cc726dbff3931
Bombo <bombo96@chungwoon.ac.kr>
2023-02-02 13:30:48 +0900

    - feat : 회원 로그인 시에 서버의 세션에 로그인 한 멤버의 회원 등급 저장

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/member/LoginController.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/member/LoginController.java
@@ -2,6 +2,7 @@ package ProjectDoge.StudentSoup.controller.member;
 
 import ProjectDoge.StudentSoup.dto.member.MemberDto;
 import ProjectDoge.StudentSoup.dto.member.MemberLoginRequestDto;
+import ProjectDoge.StudentSoup.interceptor.SessionConst;
 import ProjectDoge.StudentSoup.service.member.MemberLoginService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @RestController
@@ -21,7 +23,11 @@ public class LoginController {
     private final MemberLoginService memberLoginService;
 
     @PostMapping("/login")
-    public MemberDto login(@RequestBody MemberLoginRequestDto dto){
-        return memberLoginService.login(dto.getId(), dto.getPwd());
+    public MemberDto login(@RequestBody MemberLoginRequestDto dto, HttpServletRequest request){
+
+        MemberDto loginDto = memberLoginService.login(dto.getId(), dto.getPwd());
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginDto.getMemberClassification());
+        return loginDto;
     }
 }
