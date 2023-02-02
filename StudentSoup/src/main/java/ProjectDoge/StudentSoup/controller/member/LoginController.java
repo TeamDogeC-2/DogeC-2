ece80d5a535666055e8d97d5c4fe74715fbcd886
Bombo <bombo96@chungwoon.ac.kr>
2023-02-02 14:13:54 +0900

    - feat : 세션의 생명 주기 서비스 이용 중 30분 적용 및 로그아웃 시 세션 삭제 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/member/LoginController.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/member/LoginController.java
@@ -4,8 +4,10 @@ import ProjectDoge.StudentSoup.dto.member.MemberDto;
 import ProjectDoge.StudentSoup.dto.member.MemberLoginRequestDto;
 import ProjectDoge.StudentSoup.interceptor.SessionConst;
 import ProjectDoge.StudentSoup.service.member.MemberLoginService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +30,25 @@ public class LoginController {
         MemberDto loginDto = memberLoginService.login(dto.getId(), dto.getPwd());
         HttpSession session = request.getSession();
         session.setAttribute(SessionConst.LOGIN_MEMBER, loginDto.getMemberClassification());
+        session.setMaxInactiveInterval(1800); // 세션의 생명주기 30분으로 고정
         return loginDto;
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Result<String>> logout(HttpServletRequest request){
+        HttpSession session = request.getSession(false);
+        if(session != null) {
+            session.invalidate();
+        }
+        return ResponseEntity.ok(new Result<String>("ok"));
+    }
+
+    @Getter
+    static class Result<T> {
+        private final T data;
+
+        public Result(T data){
+            this.data = data;
+        }
     }
 }
