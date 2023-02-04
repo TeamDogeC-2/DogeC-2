64ec45a99c430c5029712c38199c9ddb5fdcf8c2
yerim <pppp5097@naver.com>
2023-02-04 15:41:47 +0900

    - docs : eslint 규칙 추가

--- StudentSoup/src/main/frontend/.eslintrc.js
+++ StudentSoup/src/main/frontend/.eslintrc.js
@@ -32,5 +32,6 @@ module.exports = {
     '@typescript-eslint/restrict-template-expressions': 'off',
     '@typescript-eslint/no-misused-promises': 'off',
     '@typescript-eslint/no-confusing-void-expression': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
   },
 };
