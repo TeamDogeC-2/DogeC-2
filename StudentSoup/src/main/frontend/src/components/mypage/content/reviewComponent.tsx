766f45e774246de38efa069611c309881c33fec3
kinjin <inclined37@gmail.com>
2023-02-05 00:25:02 +0900

    FIX: 마이페이지 작성 리뷰 별점 추가

--- StudentSoup/src/main/frontend/src/components/mypage/content/reviewComponent.tsx
+++ StudentSoup/src/main/frontend/src/components/mypage/content/reviewComponent.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import cn from 'clsx';
 import axios from 'axios';
+import { ReactComponent as Star } from '../../../img/restaurantDetailStar.svg';
 
 const ReviewComponent = () => {
   const state = useLocation<any>();
@@ -12,6 +13,7 @@ const ReviewComponent = () => {
   const [likedCount, setLikedCount] = useState<number>();
   const [starLiked, setStarLiked] = useState<number>();
   const [size, setSize] = useState<number>(6);
+  const starArr = [0, 1, 2, 3, 4];
 
   const url = '/mypage/restaurantReview';
   useEffect(() => {
@@ -39,7 +41,29 @@ const ReviewComponent = () => {
         <div key={review.memberId} className='h-[50px] px-[34px] flex items-center text-[#353535] border-b border-[#D9D9D9]'>
           <span className='w-[60%] truncate text-[#909090]'>{review.content}</span>
           <span className='w-[20%] text-center text-[#909090]'>{review.likedCount}</span>
-          <span className='w-[20%] text-center text-[#909090]'>{review.starLiked}</span>
+          <span key={review.starLiked} className='w-[20%] text-center text-[#909090]'>
+            {starArr?.map((starLiked, index) => {
+              index += 1;
+              return (
+                <>
+                <span key={review.starLiked}>
+                  <svg
+                    key={review.starLiked}
+                    className={
+                      index <= review.starLiked ? 'px-[1.2px] fill-[#FFB21D]' : 'px-[1.2px] fill-[#CDCDCD]'
+                    }
+                    width="11"
+                    height="10"
+                    viewBox="0 0 11 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                      <Star />
+                    </svg>
+                </span>
+                </>
+              )
+            })}
+          </span>
         </div>
         </>
       ))}
