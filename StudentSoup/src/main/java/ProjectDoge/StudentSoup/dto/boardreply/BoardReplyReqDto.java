38359ac85ba4ce0266ed339830f152e33f3cd9f8
Bombo <bombo96@chungwoon.ac.kr>
2023-02-05 14:23:22 +0900

    - docs : 댓글, 대댓글 생성 시 요청 객체 필드 수정

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/dto/boardreply/BoardReplyReqDto.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/dto/boardreply/BoardReplyReqDto.java
@@ -10,7 +10,18 @@ public class BoardReplyReqDto {
     private Long boardId;
     private Long memberId;
     private String content;
+    private Integer seq;
+    private Integer depth;
     private int level;
+
+    public BoardReplyReqDto(Long boardId, Long memberId, String content, Integer seq, Integer depth, int level) {
+        this.boardId = boardId;
+        this.memberId = memberId;
+        this.content = content;
+        this.seq = seq;
+        this.depth = depth;
+        this.level = level;
+    }
 
     public BoardReplyReqDto createBoardReply(Long boardId, Long memberId, String content, int level) {
         this.boardId = boardId;
