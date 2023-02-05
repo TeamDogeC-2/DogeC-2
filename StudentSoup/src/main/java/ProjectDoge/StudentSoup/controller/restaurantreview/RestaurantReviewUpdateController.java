9ddb3111d4130bc4b872b158b05ed31bce3ccc7f
Bombo <bombo96@chungwoon.ac.kr>
2023-02-05 19:48:49 +0900

    - feat : 음식점 리뷰 업데이트 컨트롤러 호출 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/restaurantreview/RestaurantReviewUpdateController.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/restaurantreview/RestaurantReviewUpdateController.java
@@ -1,28 +1,41 @@
 package ProjectDoge.StudentSoup.controller.restaurantreview;
 
 import ProjectDoge.StudentSoup.dto.restaurantreview.RestaurantReviewUpdateDto;
-import ProjectDoge.StudentSoup.entity.restaurant.RestaurantReview;
-import ProjectDoge.StudentSoup.service.restaurantreview.RestaurantReviewFindService;
+import ProjectDoge.StudentSoup.dto.restaurantreview.RestaurantReviewUpdateReqDto;
 import ProjectDoge.StudentSoup.service.restaurantreview.RestaurantReviewUpdateService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/restaurantReview/{restaurantReviewId}")
+@RequestMapping("/restaurantReview/{memberId}/{restaurantReviewId}")
 public class RestaurantReviewUpdateController {
 
     private final RestaurantReviewUpdateService restaurantReviewUpdateService;
-    private final RestaurantReviewFindService restaurantReviewFindService;
 
     @GetMapping
-    public RestaurantReviewUpdateDto restaurantReviewUpdateForm(@PathVariable Long restaurantReviewId){
-        RestaurantReview review = restaurantReviewFindService.findOne(restaurantReviewId);
-        return new RestaurantReviewUpdateDto(review);
+    public RestaurantReviewUpdateDto restaurantReviewUpdateForm(@PathVariable Long memberId, @PathVariable Long restaurantReviewId){
+        return restaurantReviewUpdateService.findUpdateRestaurantReviewForm(restaurantReviewId, memberId);
+    }
+
+    @PatchMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<ConcurrentHashMap<String, Object>> restaurantReviewUpdate(
+            @PathVariable Long memberId,
+            @PathVariable Long restaurantReviewId,
+            RestaurantReviewUpdateReqDto dto)
+    {
+        restaurantReviewUpdateService.updateRestaurantReview(restaurantReviewId, dto);
+        ConcurrentHashMap<String, Object> resultMap = new ConcurrentHashMap<>();
+
+        resultMap.put("result", "update");
+        resultMap.put("restaurantReviewId", restaurantReviewId);
+
+        return ResponseEntity.ok(resultMap);
     }
 }
