4dcc9bb44a3a92bd6cefc43768e5d39121652b5c
yerim <pppp5097@naver.com>
2023-02-05 18:06:48 +0900

    - feat : 게시판 리스트 타이틀 부분 수정

--- StudentSoup/src/main/frontend/src/components/board/content/titleComponent.tsx
+++ StudentSoup/src/main/frontend/src/components/board/content/titleComponent.tsx
@@ -1,4 +1,6 @@
 import cn from 'clsx';
+import _ from 'lodash';
+import { categoryList } from '../data/_constant';
 
 interface PropsType {
   boardCategory: string;
@@ -14,16 +16,20 @@ export const enum RANGE {
 const TitleComponent = (props: PropsType) => {
   const { boardCategory, range, setRange } = props;
 
+  const boardCategoryLabel = _.find(categoryList, category => {
+    return category.value === boardCategory;
+  })?.label;
+
   return (
     <div className="flex justify-between">
-      <div className="text-[24px] font-[700]">{boardCategory}</div>
+      <div className="text-[24px] font-[700]">{boardCategoryLabel}</div>
       <div className="flex">
         <div
           onClick={() => {
             setRange(RANGE.SCHOOL);
           }}
           className={cn(
-            'cursor-pointer rounded-[45px] drop-shadow-sm  w-[110px] h-[36px] text-center border-solid border text-[16px] leading-[30px] mr-[13px]',
+            'cursor-pointer rounded-[45px] drop-shadow-sm  w-[110px] h-[36px] text-center border-solid border text-[16px] leading-[33px] mr-[13px]',
             {
               'bg-[#FF611D] text-[#fff] border-[#FF611D]': range === RANGE.SCHOOL,
               'bg-[#fff] border-[#BCBCBC] text-[#FF611D]': range !== RANGE.SCHOOL,
