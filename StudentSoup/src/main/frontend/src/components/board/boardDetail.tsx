f72c327e4f579f0721da83d8346c49e7970689b9
spsp8426 <spsp8426@naver.com>
2023-02-06 14:19:25 +0900

    style : import 베스트 댓글 이름 변경

--- StudentSoup/src/main/frontend/src/components/board/boardDetail.tsx
+++ StudentSoup/src/main/frontend/src/components/board/boardDetail.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import MypageNavbar from '../common/mypageNavbar';
 import BoardReviewList from './boardReviewList';
-import BoardBestReplyHeart from './boardBestReplyHeart';
+import BoardBestReplyHeart from './boardBestReply';
 import axios from 'axios';
 import { ReactComponent as BoardWriteIcon } from '../../img/BoardWriteIcon.svg';
 import { ReactComponent as BoardWriteIconHeart } from '../../img/boardWriteIconHeart.svg';
