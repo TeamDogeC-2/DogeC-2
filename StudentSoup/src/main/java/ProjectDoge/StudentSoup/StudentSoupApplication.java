fee5bca9b4aa0106c393e4b4bd283a78cfa13877
Bombo <bombo96@chungwoon.ac.kr>
2023-02-08 23:52:56 +0900

    - fix : ec2 S3 연결

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/StudentSoupApplication.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/StudentSoupApplication.java
@@ -6,11 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentSoupApplication {
 
-	static {
-		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
-		System.setProperty("java.net.preferIPv4Stack", "true");
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(StudentSoupApplication.class, args);
 	}
