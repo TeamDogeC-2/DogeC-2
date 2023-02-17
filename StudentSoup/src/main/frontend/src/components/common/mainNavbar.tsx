94a51c706da88148ee345d488c3ebb43b041ea15
eunsung <esk114477@gmail.com>
2023-02-17 17:44:08 +0900

    -feat : 모바일 헤더 메뉴 클릭 이벤트 추가

--- StudentSoup/src/main/frontend/src/components/common/mainNavbar.tsx
+++ StudentSoup/src/main/frontend/src/components/common/mainNavbar.tsx
@@ -1,14 +1,36 @@
-import React from 'react';
 import './mainNavbar.scss';
 import { Link } from 'react-router-dom';
-import { Desktop, Mobile } from '../../mediaQuery';
+import { DesktopHeader, MobileHeader } from '../../mediaQuery';
 import mainLogo from '../../img/mainLogo.svg';
+import { useEffect, useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight, faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const mainNavbar = () => {
+  const [click, isClick] = useState<boolean>(false);
+
+  const searchRef = useRef<any>();
+
+  const handleClickMenu = (e: any) => {
+    e.stopPropagation();
+    isClick(!click);
+  };
+
+  useEffect(() => {
+    const onCheckClickOutside = (e: MouseEvent) => {
+      if (click && searchRef.current && !searchRef.current.contains(e.target as Node)) {
+        isClick(!click);
+      }
+    };
+    document.addEventListener('mousedown', onCheckClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', onCheckClickOutside);
+    };
+  }, [click]);
   return (
     <>
-      <Desktop>
-        <nav className="NavbarItems">
+      <DesktopHeader>
+        <nav className="navbar-items">
           <Link to="/" className="navbar-logo-links">
             <img src={mainLogo} className="navbar-logo" />
           </Link>
@@ -30,10 +52,54 @@ const mainNavbar = () => {
             </li>
           </ul>
         </nav>
-      </Desktop>
-      <Mobile>
-        <div>모바일 화면 입니다</div>
-      </Mobile>
+      </DesktopHeader>
+      <MobileHeader>
+        <nav className="mobile-navbar-items">
+          <Link to="/" className="navbar-logo-links">
+            <img src={mainLogo} className="navbar-logo" />
+          </Link>
+          <div className="mobile-nav-menu">
+            {click ? (
+              <FontAwesomeIcon icon={faXmark} className="mobile-nav-menu-icon" />
+            ) : (
+              <FontAwesomeIcon
+                icon={faBars}
+                className="mobile-nav-menu-icon"
+                onMouseDown={handleClickMenu}
+              />
+            )}
+          </div>
+          <ul
+            ref={searchRef}
+            className={click ? 'mobile-nav-menu-list active' : 'mobile-nav-menu-list'}
+          >
+            <li>
+              <Link to="/notice" className="mobile-nav-link">
+                <div className="mobile-nav-list">
+                  <i className="mobile-nav-listItme">공지사항</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-nav-icons" />
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/notice" className="mobile-nav-link">
+                <div className="mobile-nav-list">
+                  <i className="mobile-nav-listItme">고객센터</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-nav-icons" />
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/notice" className="mobile-nav-link">
+                <div className="mobile-nav-list">
+                  <i className="mobile-nav-listItme">로그인</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-nav-icons" />
+                </div>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </MobileHeader>
     </>
   );
 };
