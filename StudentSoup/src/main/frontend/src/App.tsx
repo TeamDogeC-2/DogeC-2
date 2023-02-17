27617f90c2677aa33d1f1043effe3e73c275c8ff
구승모 <seungmo@guseungmoui-noteubug.local>
2023-02-17 15:46:33 +0900

    초기 파일 설정

--- StudentSoup/src/main/frontend/src/App.tsx
+++ StudentSoup/src/main/frontend/src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Routes from './Routes';
-import { BrowserRouter } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
+import Router from './Router';
 
 const App = () => {
   return (
-    <RecoilRoot>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </RecoilRoot>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   );
 };
 
---
+++ StudentSoup/src/main/frontend/src/components/home/Home.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import MainNavbar from '../common/mainNavbar';
+import './home.scss';
+import MainLogo_white from '../../img/mainLogo_white.svg';
+import Search_icon from '../../img/search_icon.svg';
+const Home = () => {
+  return (
+    <>
+      <MainNavbar />
+      <div className="hero-text">
+        <img className="sfoo-image" src={MainLogo_white} />
+        <p>대학생들을 위한</p>
+        <h2 className="link-texts">대학 주변 맛집 추천</h2>
+        <div className="school_search_bar">
+          <img src={Search_icon} />
+          <input placeholder="지역 학교 명을 입력하세요."></input>
+          <button>검색</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
