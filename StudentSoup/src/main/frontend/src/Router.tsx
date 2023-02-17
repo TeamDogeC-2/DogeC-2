6848c8e9ec9a0c7bd3e921276812df9a2e642fd3
구승모 <seungmo@guseungmoui-noteubug.local>
2023-02-17 17:31:48 +0900

    add : 에러 페이지 라우터 추가

--- StudentSoup/src/main/frontend/src/Router.tsx
+++ StudentSoup/src/main/frontend/src/Router.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
+import Err404 from './components/err404/Err404';
 
 const Router = () => {
   return (
     <Routes>
+      <Route path="*" element={<Err404 />} />
       <Route path="/" element={<Home />} />
     </Routes>
   );
