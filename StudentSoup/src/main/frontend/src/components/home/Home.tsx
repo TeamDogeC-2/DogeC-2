f4985755332abce8d6f66abce92390efc62791ab
구승모 <seungmo@guseungmoui-noteubug.local>
2023-02-18 17:00:26 +0900

    FEAT : 학교 데이터 리스트 추가

--- StudentSoup/src/main/frontend/src/components/home/Home.tsx
+++ StudentSoup/src/main/frontend/src/components/home/Home.tsx
@@ -1,9 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import MainNavbar from '../common/mainNavbar';
 import './home.scss';
 import MainLogo_white from '../../img/mainLogo_white.svg';
 import Search_icon from '../../img/search_icon.svg';
+import { SchoolList, type SchoolListType } from './data/SchoolList';
+
 const Home = () => {
+  const [schoolComponent, setSchoolComponent] = useState<any>([]);
+  const [schoolName, setSchoolName] = useState<string>('');
+  const saveSchoolName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSchoolName(e.target.value);
+  };
+  const handleClickSearch = () => {
+    alert(schoolName);
+  };
+
+  useEffect(() => {
+    SchoolList()
+      .then(res => {
+        setSchoolComponent(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <>
       <MainNavbar />
@@ -13,8 +34,13 @@ const Home = () => {
         <h2 className="link-texts">대학 주변 맛집 추천</h2>
         <div className="school_search_bar">
           <img src={Search_icon} />
-          <input placeholder="지역 학교 명을 입력하세요."></input>
-          <button>검색</button>
+          <input onChange={saveSchoolName} placeholder="지역 학교 명을 입력하세요."></input>
+          <button onClick={handleClickSearch}>검색</button>
+          {schoolComponent.map((school: SchoolListType) => (
+            <div className="school-list" key={school.schoolId}>
+              {school.schoolName}
+            </div>
+          ))}
         </div>
       </div>
     </>
