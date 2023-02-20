e28cf05150dbf7991bce93da5339269347417d74
KanghoYoo <kangsick98@gmail.com>
2023-02-21 05:52:26 +0900

    - add : 회원가입 두번째 진행 페이지 라우터 추가

--- StudentSoup/src/main/frontend/src/Router.tsx
+++ StudentSoup/src/main/frontend/src/Router.tsx
@@ -5,6 +5,7 @@ import Err404 from './components/err404/Err404';
 import Login from './components/login/Login';
 import FindAccount from './components/login/FindAccount';
 import SignUp from './components/signup/SignUp';
+import SignUp2 from './components/signup/SignUp2';
 
 const Router = () => {
   return (
@@ -14,6 +15,7 @@ const Router = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/login/findAccount" element={<FindAccount />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/signup2" element={<SignUp2 />} />
     </Routes>
   );
 };
