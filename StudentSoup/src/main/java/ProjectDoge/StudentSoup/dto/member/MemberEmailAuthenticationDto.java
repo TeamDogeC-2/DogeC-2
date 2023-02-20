8538471b8a528a9b3f1f03601a9cab75999406b1
xogns4909 <xogns4909@naver.com>
2023-02-20 15:04:40 +0900

    -feat : 회원 이메일 인증 Dto 추가

---
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/dto/member/MemberEmailAuthenticationDto.java
@@ -0,0 +1,18 @@
+package ProjectDoge.StudentSoup.dto.member;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Data
+@Getter
+@Setter
+public class MemberEmailAuthenticationDto {
+
+    private String email;
+
+    private String authenticationNumber;
+
+}
