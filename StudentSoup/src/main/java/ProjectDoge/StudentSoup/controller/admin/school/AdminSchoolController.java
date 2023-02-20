b549cab85ffef1e3800158dd5660a7ac55b77de8
xogns4909 <xogns4909@naver.com>
2023-02-20 14:37:16 +0900

    -refactor : 학교 등록 컨트롤러 수정

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/admin/school/AdminSchoolController.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/admin/school/AdminSchoolController.java
@@ -8,18 +8,19 @@ import ProjectDoge.StudentSoup.service.admin.AdminSchoolService;
 import ProjectDoge.StudentSoup.service.school.SchoolFindService;
 import ProjectDoge.StudentSoup.service.school.SchoolRegisterService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
-@Controller
+@Slf4j
+@RestController
 @RequiredArgsConstructor
 public class AdminSchoolController {
     private final SchoolFindService schoolFindService;
@@ -27,46 +28,52 @@ public class AdminSchoolController {
     private final AdminSchoolService adminSchoolService;
     private final SchoolRepository schoolRepository;
 
-    @GetMapping("admin/school/new")
-    public String createSchool(Model model){
-        model.addAttribute("schoolForm",new SchoolFormDto());
-        return "/admin/school/createSchool";
+    @PostMapping("admin/school")
+    public ResponseEntity createSchool(@RequestBody SchoolFormDto schoolFormDto){
+        Long schoolId = schoolRegisterService.join(schoolFormDto);
+        return new ResponseEntity(schoolId,HttpStatus.valueOf(201));
     }
-    @PostMapping("admin/school/new")
-    public String createSchool(@Valid SchoolFormDto formDto, BindingResult bindingResult){
-        if(bindingResult.hasErrors()){
-            return "/admin/school/createSchool";
-        }
-        schoolRegisterService.join(formDto);
-        return "redirect:/admin/schools";
-    }
-    @GetMapping("/admin/schools")
-    public String schoolList(@ModelAttribute("schoolSearch") SchoolSearch schoolSearch, Model model) {
-        List<School> schools = schoolFindService.findAll();
-        model.addAttribute("schools", schools);
 
-        List<School> findSchools = adminSchoolService.AdminSearchSchools(schoolSearch);
-        model.addAttribute("findSchools", findSchools);
-
-        return "/admin/school/schoolList";
-    }
-    @GetMapping("/admin/school/edit")
-    public String editSchool(@RequestParam("schoolId")Long schoolId,Model model){
-        SchoolFormDto updateSchool = adminSchoolService.AdminFindUpdateSchool(schoolId);
-        model.addAttribute("schoolId",schoolId);
-        model.addAttribute("schoolForm",updateSchool);
-        return "/admin/school/updateSchool";
-    }
-    @PostMapping("/admin/school/edit")
-    public String editSchool(@RequestParam("schoolId")Long schoolId,@ModelAttribute("form") SchoolFormDto schoolFormDto){
-        adminSchoolService.AdminUpdateSchool(schoolId,schoolFormDto);
-        return "redirect:/admin/schools";
-    }
-    @GetMapping("/admin/school/delete")
-    public String deleteSchool(@RequestParam("schoolId")Long schoolId){
-        schoolRepository.deleteById(schoolId);
-        return "redirect:/admin/schools";
-    }
+//    @GetMapping("admin/school/new")
+//    public String createSchool(Model model){
+//        model.addAttribute("schoolForm",new SchoolFormDto());
+//        return "/admin/school/createSchool";
+//    }
+//    @PostMapping("admin/school/new")
+//    public String createSchool(@Valid SchoolFormDto formDto, BindingResult bindingResult){
+//        if(bindingResult.hasErrors()){
+//            return "/admin/school/createSchool";
+//        }
+//        schoolRegisterService.join(formDto);
+//        return "redirect:/admin/schools";
+//    }
+//    @GetMapping("/admin/schools")
+//    public String schoolList(@ModelAttribute("schoolSearch") SchoolSearch schoolSearch, Model model) {
+//        List<School> schools = schoolFindService.findAll();
+//        model.addAttribute("schools", schools);
+//
+//        List<School> findSchools = adminSchoolService.AdminSearchSchools(schoolSearch);
+//        model.addAttribute("findSchools", findSchools);
+//
+//        return "/admin/school/schoolList";
+//    }
+//    @GetMapping("/admin/school/edit")
+//    public String editSchool(@RequestParam("schoolId")Long schoolId,Model model){
+//        SchoolFormDto updateSchool = adminSchoolService.AdminFindUpdateSchool(schoolId);
+//        model.addAttribute("schoolId",schoolId);
+//        model.addAttribute("schoolForm",updateSchool);
+//        return "/admin/school/updateSchool";
+//    }
+//    @PostMapping("/admin/school/edit")
+//    public String editSchool(@RequestParam("schoolId")Long schoolId,@ModelAttribute("form") SchoolFormDto schoolFormDto){
+//        adminSchoolService.AdminUpdateSchool(schoolId,schoolFormDto);
+//        return "redirect:/admin/schools";
+//    }
+//    @GetMapping("/admin/school/delete")
+//    public String deleteSchool(@RequestParam("schoolId")Long schoolId){
+//        schoolRepository.deleteById(schoolId);
+//        return "redirect:/admin/schools";
+//    }
 
 
 }
