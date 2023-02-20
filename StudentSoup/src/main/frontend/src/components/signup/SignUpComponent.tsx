ead0a3cbdb436b2217a2595b008c04c395b4e49c
KanghoYoo <kangsick98@gmail.com>
2023-02-21 05:50:24 +0900

    - design: 회원가입 공통사용 부분 컴포넌트화

---
+++ StudentSoup/src/main/frontend/src/components/signup/SignUpComponent.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const SignUpComponent = (props: any) => {
+  return (
+    <div className="signup-main">
+      <div className="signup-header">
+        <h2>신규 회원가입</h2>
+        <div className="signup-progress">
+          <img src={props.process_1} alt="process-1" className="proceess-img" />
+          <img src={props.process_bar_1} alt="process-bar" className="proceess-bar" />
+          <img src={props.process_2} alt="process-2" className="proceess-img" />
+          <img src={props.process_bar_2} alt="process-bar" className="proceess-bar" />
+          <img src={props.process_3} alt="process-3" className="proceess-img" />
+          <p className="sentence-1">이용약관 동의</p>
+          <p className="sentence-2">회원가입</p>
+          <p className="sentence-3">개인정보 입력</p>
+        </div>
+      </div>
+      {props.children}
+    </div>
+  );
+};
+
+export default SignUpComponent;
