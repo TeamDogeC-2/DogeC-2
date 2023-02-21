b8eb5b50e450a7fcfbcd26832b099fb88c5acf1b
KanghoYoo <kangsick98@gmail.com>
2023-02-21 23:32:42 +0900

    - design : 회원가입 페이지 컴포넌트 UI props 값 추가 및 스타일 변경

--- StudentSoup/src/main/frontend/src/components/signup/SignUpComponent.tsx
+++ StudentSoup/src/main/frontend/src/components/signup/SignUpComponent.tsx
@@ -6,14 +6,14 @@ const SignUpComponent = (props: any) => {
     <div className="signup-main">
       <h1>신규 회원가입</h1>
       <div className="signup-progress">
-        <img src={props.process_1} alt="process-1" className="proceess-img" />
-        <img src={props.process_bar_1} alt="process-bar" className="proceess-bar" />
-        <img src={props.process_2} alt="process-2" className="proceess-img" />
-        <img src={props.process_bar_2} alt="process-bar" className="proceess-bar" />
-        <img src={props.process_3} alt="process-3" className="proceess-img" />
-        <p className="sentence-1">이용약관 동의</p>
-        <p className="sentence-2">회원가입</p>
-        <p className="sentence-3">개인정보 입력</p>
+        <img src={props.process_1} alt="activate-process-1" className="process-1" />
+        <img src={props.process_bar_1} alt="process-bar" className="process-bar-1" />
+        <img src={props.process_2} alt="process-2" className="process-2" />
+        <img src={props.process_bar_2} alt="process-bar" className="process-bar-2" />
+        <img src={props.process_3} alt="process-3" className="process-3" />
+        <p className="activate-sentence-1">이용약관 동의</p>
+        <p className={props.sentence_2}>회원가입</p>
+        <p className={props.sentence_3}>개인정보 입력</p>
       </div>
       {props.children}
     </div>
