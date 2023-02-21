e74c44e423fd6c0b3612251291f8e6011d322a9f
eunsung <esk114477@gmail.com>
2023-02-21 16:54:46 +0900

    -feat : 음식점 헤더 추가

---
+++ StudentSoup/src/main/frontend/src/components/restaurant/RestaurantNavbar.tsx
@@ -0,0 +1,154 @@
+import './restaurantNavbar.scss';
+import { Link } from 'react-router-dom';
+import {
+  DesktopHeader,
+  DesktopRestaurantHeader,
+  MobileHeader,
+  MobileRestaurantHeader,
+} from '../../mediaQuery';
+import mainLogo from '../../img/mainLogo.svg';
+import { useEffect, useRef, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight, faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+
+const RestaurantNavbar = () => {
+  const [click, isClick] = useState<boolean>(false);
+
+  const searchRef = useRef<any>();
+
+  const handleClickMenu = (e: any) => {
+    e.stopPropagation();
+    isClick(!click);
+  };
+
+  useEffect(() => {
+    const onCheckClickOutside = (e: MouseEvent) => {
+      if (click && searchRef.current && !searchRef.current.contains(e.target as Node)) {
+        isClick(!click);
+      }
+    };
+    document.addEventListener('mousedown', onCheckClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', onCheckClickOutside);
+    };
+  }, [click]);
+  return (
+    <>
+      <DesktopHeader>
+        <nav className="restaurant-navbar-items">
+          <div className="restaurant-navbar-left">
+            <Link to="/" className="restaurant-navbar-logo-links">
+              <img src={mainLogo} className="restaurant-navbar-logo" />
+            </Link>
+            <input
+              type="text"
+              placeholder="지역 또는 학교명을 입력하세요."
+              className="restaurant-navbar-input"
+            />
+          </div>
+          <ul className="restaurant-nav-menu">
+            <li className="restaurant-nav-li">
+              <Link to="/notice" className="restaurant-nav-links">
+                <i>공지사항</i>
+              </Link>
+            </li>
+            <li className="restaurant-nav-li">
+              <Link to="/help" className="restaurant-nav-links">
+                <i>고객센터</i>
+              </Link>
+            </li>
+            <li className="restaurant-nav-li">
+              <Link to="/login" className="restaurant-nav-links">
+                <i>로그인</i>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </DesktopHeader>
+      <MobileHeader>
+        <nav className="mobile-restaurant-navbar-items">
+          <Link to="/" className="restaurant-navbar-logo-links">
+            <img src={mainLogo} className="restaurant-navbar-logo" />
+          </Link>
+          <div className="mobile-restaurant-nav-menu">
+            {click ? (
+              <FontAwesomeIcon icon={faXmark} className="mobile-restaurant-nav-menu-icon" />
+            ) : (
+              <FontAwesomeIcon
+                icon={faBars}
+                className="mobile-restaurant-nav-menu-icon"
+                onMouseDown={handleClickMenu}
+              />
+            )}
+          </div>
+          <ul
+            ref={searchRef}
+            className={
+              click ? 'mobile-restaurant-nav-menu-list active' : 'mobile-restaurant-nav-menu-list'
+            }
+          >
+            <li className="mobile-restaurant-navbar-input-div">
+              <div className="mobile-restaurant-navbar-input-div">
+                <input
+                  type="text"
+                  placeholder="지역 또는 학교명을 입력하세요."
+                  className="mobile-restaurant-navbar-input"
+                />
+              </div>
+            </li>
+            <li>
+              <Link to="/notice" className="mobile-restaurant-nav-link">
+                <div className="mobile-restaurant-nav-list">
+                  <i className="mobile-restaurant-nav-listItme">공지사항</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-restaurant-nav-icons" />
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/notice" className="mobile-restaurant-nav-link">
+                <div className="mobile-restaurant-nav-list">
+                  <i className="mobile-restaurant-nav-listItme">고객센터</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-restaurant-nav-icons" />
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/notice" className="mobile-restaurant-nav-link">
+                <div className="mobile-restaurant-nav-list">
+                  <i className="mobile-restaurant-nav-listItme">로그인</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-restaurant-nav-icons" />
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/notice" className="mobile-restaurant-nav-link">
+                <div className="mobile-restaurant-nav-list">
+                  <i className="mobile-restaurant-nav-listItme">공지사항</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-restaurant-nav-icons" />
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/notice" className="mobile-restaurant-nav-link">
+                <div className="mobile-restaurant-nav-list">
+                  <i className="mobile-restaurant-nav-listItme">고객센터</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-restaurant-nav-icons" />
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/notice" className="mobile-restaurant-nav-link">
+                <div className="mobile-restaurant-nav-list">
+                  <i className="mobile-restaurant-nav-listItme">로그인</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-restaurant-nav-icons" />
+                </div>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </MobileHeader>
+    </>
+  );
+};
+
+export default RestaurantNavbar;
