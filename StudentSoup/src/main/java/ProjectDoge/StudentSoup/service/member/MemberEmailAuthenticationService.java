b913ffa699e9acbd6c6e199c7648cc911745ad03
xogns4909 <xogns4909@naver.com>
2023-02-22 13:15:31 +0900

    -feat : 인증번호 확인 서비스 로직 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/service/member/MemberEmailAuthenticationService.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/service/member/MemberEmailAuthenticationService.java
@@ -4,6 +4,10 @@ import ProjectDoge.StudentSoup.dto.member.EmailDto;
 import ProjectDoge.StudentSoup.dto.member.MemberEmailAuthenticationDto;
 import ProjectDoge.StudentSoup.entity.member.MemberEmailAuthentication;
 import ProjectDoge.StudentSoup.entity.school.School;
+import ProjectDoge.StudentSoup.exception.EmailAuthentication.AuthenticationEmailNotSentException;
+import ProjectDoge.StudentSoup.exception.EmailAuthentication.AuthenticationNumberNotSentException;
+import ProjectDoge.StudentSoup.exception.EmailAuthentication.AuthenticationNumberWrongException;
+import ProjectDoge.StudentSoup.exception.EmailAuthentication.AuthenticationTimeOverException;
 import ProjectDoge.StudentSoup.repository.memberEmailAuthentication.MemberEmailAuthenticationRepository;
 import ProjectDoge.StudentSoup.service.school.SchoolFindService;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +19,8 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
@@ -39,15 +45,44 @@ public class MemberEmailAuthenticationService {
         return resultMap;
     }
     @Transactional
-    public EmailDto sendMail(MemberEmailAuthenticationDto memberEmailAuthenticationDto){
+    public EmailDto join(String email){
         log.info("메일전송 메소드가 실행되었습니다.");
+        int index = email.indexOf("@");
+        if(email.substring(0,index).trim().length() == 0){
+            throw new AuthenticationEmailNotSentException("메일이 전송되지 않았습니다.");
+        }
         int authenticationNumber = createAuthenticationNumber();
-        memberEmailAuthenticationDto.setAuthenticationNumber(authenticationNumber);
-        MemberEmailAuthentication memberEmailAuthentication = new MemberEmailAuthentication().createMemberEmailAuthentication(memberEmailAuthenticationDto);
+        MemberEmailAuthentication memberEmailAuthentication = new MemberEmailAuthentication().createMemberEmailAuthentication(email,authenticationNumber);
         log.info("email ={}",memberEmailAuthentication.getEmail());
         memberEmailAuthenticationRepository.save(memberEmailAuthentication);
         EmailDto mail = createMail(memberEmailAuthentication);
         return mail;
+    }
+
+    public ConcurrentHashMap<String,String> checkAuthenticationNumber(MemberEmailAuthenticationDto memberEmailAuthenticationDto){
+        if(memberEmailAuthenticationDto.getAuthenticationNumber() == null){
+            throw new AuthenticationNumberNotSentException("인증번호가 전송되지 않았습니다.");
+        }
+        ConcurrentHashMap<String,String> resultMap = new ConcurrentHashMap();
+        MemberEmailAuthentication memberEmailAuthentication = memberEmailAuthenticationRepository.findByEmailAndAuthenticationNumber(memberEmailAuthenticationDto).orElse(null);
+        checkAuthenticationNumber(memberEmailAuthentication);
+        checkAuthenticationTime(memberEmailAuthentication);
+        resultMap.put("email", memberEmailAuthentication.getEmail());
+        resultMap.put("result","ok");
+        memberEmailAuthenticationRepository.delete(memberEmailAuthentication);
+
+        return resultMap;
+    }
+    private void checkAuthenticationNumber(MemberEmailAuthentication memberEmailAuthentication) {
+        if(memberEmailAuthentication == null){
+            throw new AuthenticationNumberWrongException("잘못된 인증 번호입니다.");
+        }
+    }
+
+    private void checkAuthenticationTime(MemberEmailAuthentication memberEmailAuthentication) {
+        if(memberEmailAuthentication.getCreateDate().isAfter(LocalDateTime.now().plusMinutes(5))){
+            throw new AuthenticationTimeOverException("인증 시간이 초과되었습니다.");
+        }
     }
 
     private int createAuthenticationNumber() {
@@ -66,13 +101,9 @@ public class MemberEmailAuthenticationService {
         log.info("회원 학교 인증 메일을 생성 시작하였습니다.");
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(dto.getEmail());
-        log.info("email{}",dto.getEmail());
         message.setFrom(adminEmail);
-        log.info("From{}",adminEmail);
         message.setSubject(dto.getTitle());
-        log.info("title{}",dto.getTitle());
         message.setText(dto.getMessage());
-        log.info("Text{}",dto.getMessage());
         mailSender.send(message);
         log.info("회원 학교 인증 메일이 전송되었습니다.");
     }
