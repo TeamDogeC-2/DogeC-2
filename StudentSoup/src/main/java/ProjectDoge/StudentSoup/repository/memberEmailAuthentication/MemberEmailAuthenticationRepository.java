359a02fb5b7dae96c8c3aa446ceda045174896e2
xogns4909 <xogns4909@naver.com>
2023-02-23 14:07:00 +0900

    -refactor : MemberAuthenticationRepository 삭제

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/memberEmailAuthentication/MemberEmailAuthenticationRepository.java
+++
@@ -1,9 +0,0 @@
-package ProjectDoge.StudentSoup.repository.memberEmailAuthentication;
-
-import ProjectDoge.StudentSoup.entity.member.MemberEmailAuthentication;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberEmailAuthenticationRepository extends JpaRepository<MemberEmailAuthentication,Long>,MemberEmailAuthenticationRepositoryCustom {
-}
