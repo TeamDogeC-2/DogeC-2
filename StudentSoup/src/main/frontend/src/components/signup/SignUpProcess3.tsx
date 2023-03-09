d36d13e6f3e380e4a773745614a1560d6bb09dcb
KanghoYoo <kangsick98@gmail.com>
2023-03-09 14:35:00 +0900

    - feat : 이메일 인증 요청API 및 회원가입 요청 API 추가, 조건부렌더링 기능 추가

--- StudentSoup/src/main/frontend/src/components/signup/SignUpProcess3.tsx
+++ StudentSoup/src/main/frontend/src/components/signup/SignUpProcess3.tsx
@@ -10,35 +10,83 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import useInput from '../../hooks/useInput';
 import axios from 'axios';
 
-export interface universityDataType {
+export interface UniversityDataType {
   schoolId: number;
   schoolName: string;
 }
-
-export interface majorDataType {
+export interface DepartmentType {
   departmentId: number;
   departmentName: string;
 }
 
+export interface MajorDataType {
+  departments: DepartmentType[];
+  domain: string;
+  message?: string;
+}
+
 const SignUpProcess3 = () => {
+  const [userGender, setUserGender] = useState('MAN');
   const [userNickname, onChangeUserNickname] = useInput('');
-  const [userEmail, onChangeUserEmail] = useInput('');
+  const [userEmail, setUserEmail] = useState('');
   const [userAuthenticationCode, onChangeUserAuthenticationCode] = useInput('');
-  const [universityData, setUniversityData] = useState<universityDataType[]>([]);
-  const [majorData, setMajorData] = useState<majorDataType[]>([]);
-  const [selectValue, setSelectValue] = useState('');
+  const [universityData, setUniversityData] = useState<UniversityDataType[]>([]);
+  const [majorData, setMajorData] = useState<MajorDataType | null>(null);
+  const [selectUniversity, setSelectUniversity] = useState('');
+  const [selectMajor, setSelectMajor] = useState('');
+  const [isEmailSubmit, setIsEmailSubmit] = useState(false);
+  const [isEmailConfirmation, setIsEmailConfirmation] = useState(false);
   const navigate = useNavigate();
   const { state } = useLocation();
 
+  const REG_EMAIL =
+    /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
+
+  const onChangeGender = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserGender(e.target.value);
+  };
+
   const onChangeUniversitySelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectValue(e.target.value);
+    setSelectUniversity(e.target.value);
+  };
+
+  const onChangeMajor = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectMajor(e.target.value);
+  };
+
+  const onChangeUserEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserEmail(e.target.value);
   };
 
   const onClickEmailCertification = () => {
-    console.log(userEmail);
-    axios.post('/members/signUp/3/mail', { email: userEmail }).then(res => {
-      console.log(res);
-    });
+    if (userEmail !== '' && majorData !== null) {
+      axios
+        .post('/members/signUp/3/mail', { email: userEmail + '@' + majorData.domain })
+        .then(res => {
+          console.log(res);
+          setIsEmailSubmit(true);
+        });
+    }
+    // console.log(userEmail);
+  };
+
+  const onSubmitSignup = (e: any) => {
+    e.preventDefault();
+    if (majorData !== null) {
+      axios
+        .post('/members/signUp/3', {
+          id: state.userId,
+          pwd: state.userPassword,
+          nickName: userNickname,
+          email: userEmail + '@' + majorData.domain,
+          gender: userGender,
+          schoolId: selectUniversity,
+          departmentId: selectMajor,
+        })
+        .then(res => {
+          console.log(res);
+        });
+    }
   };
 
   useEffect(() => {
@@ -47,15 +95,16 @@ const SignUpProcess3 = () => {
     }
     axios.get('/members/signUp/3').then(res => {
       setUniversityData(res.data);
+      console.log(res.data);
     });
 
-    if (selectValue !== '') {
-      axios.post('/members/signUp/3/' + selectValue).then(res => {
-        console.log(res);
-        return setMajorData(res.data);
+    if (selectUniversity !== '') {
+      axios.post('/members/signUp/3/' + selectUniversity).then(res => {
+        setMajorData(res.data);
+        console.log(res.data);
       });
     }
-  }, [state, selectValue]);
+  }, [state, selectUniversity, isEmailSubmit, selectMajor]);
 
   return (
     <div className="signup-process-3-container">
@@ -74,16 +123,29 @@ const SignUpProcess3 = () => {
             SFOO의 서비스를 위한 개인정보를
             <br /> 입력해주세요.
           </h2>
-          <form className="privacy-form">
+          <form className="privacy-form" onSubmit={onSubmitSignup}>
             <div className="radio-wrap">
               <span>성별</span>
               <label htmlFor="man">
                 남
-                <input type="radio" id="man" name="gender" defaultChecked />
+                <input
+                  type="radio"
+                  id="man"
+                  name="gender"
+                  value="MAN"
+                  onChange={onChangeGender}
+                  defaultChecked
+                />
               </label>
               <label htmlFor="woman">
                 여
-                <input type="radio" id="woman" name="gender" />
+                <input
+                  type="radio"
+                  id="woman"
+                  name="gender"
+                  value="WOMAN"
+                  onChange={onChangeGender}
+                />
               </label>
             </div>
             <label>
@@ -109,55 +171,83 @@ const SignUpProcess3 = () => {
                   <option value="학교 선택" disabled={true}>
                     학교 선택
                   </option>
-                  {universityData.map(el => (
-                    <option key={el.schoolId} value={el.schoolId}>
-                      {el.schoolName}
-                    </option>
-                  ))}
+                  {universityData.map(el => {
+                    return (
+                      <option key={el.schoolId} value={el.schoolId}>
+                        {el.schoolName}
+                      </option>
+                    );
+                  })}
                 </select>
               </label>
               <label>
                 전공
-                <select name="major" defaultValue="전공 선택">
+                <select name="major" defaultValue="전공 선택" onChange={e => onChangeMajor(e)}>
                   <option value="전공 선택" disabled>
                     전공 선택
                   </option>
-                  {majorData.map(el => (
-                    <option key={el.departmentId} value={el.departmentId}>
-                      {el.departmentName}
-                    </option>
-                  ))}
+                  {!!majorData &&
+                    majorData.departments.map(el => {
+                      return (
+                        <option key={el.departmentId} value={el.departmentId}>
+                          {el.departmentName}
+                        </option>
+                      );
+                    })}
                 </select>
               </label>
             </div>
             <label>
               <div className="privacy-input-wrap">
                 <input
+                  className="id-input"
                   type="email"
                   value={userEmail}
                   onChange={onChangeUserEmail}
-                  placeholder="@학교이메일.com"
+                  placeholder="아이디"
                 />
-                <button type="button" onClick={onClickEmailCertification}>
+                <input
+                  className="email-input"
+                  type="email"
+                  value={majorData !== null ? '@' + majorData.domain : '@학교이메일.com'}
+                  disabled
+                />
+                <button
+                  className={
+                    userEmail !== ''
+                      ? 'signup-email-activate-button'
+                      : 'signup-email-disabled-button'
+                  }
+                  type="button"
+                  onClick={onClickEmailCertification}
+                >
                   이메일 인증
                 </button>
               </div>
             </label>
-            <p>인증코드가 발송되었습니다.</p>
-            <label>
-              <div className="privacy-input-wrap">
-                <input
-                  type="text"
-                  value={userAuthenticationCode}
-                  onChange={onChangeUserAuthenticationCode}
-                  placeholder="인증코드 입력"
-                />
-                <button type="button">인증 확인</button>
-              </div>
-            </label>
-            <p>인증되었습니다.</p>
+            {isEmailSubmit && (
+              <>
+                <p>인증코드가 발송되었습니다.</p>
+                <label>
+                  <div className="privacy-input-wrap">
+                    <input
+                      type="text"
+                      value={userAuthenticationCode}
+                      onChange={onChangeUserAuthenticationCode}
+                      placeholder="인증코드 입력"
+                    />
+                    <button className="signup-email-activate-button" type="button">
+                      인증 확인
+                    </button>
+                  </div>
+                </label>
+              </>
+            )}
+            {isEmailConfirmation ? <p>인증되었습니다.</p> : <p></p>}
+            <button className="signup-activate-button " type="submit">
+              완료
+            </button>
           </form>
-          <button className="signup-button">완료</button>
         </SignUpComponent>
       </Background>
     </div>
