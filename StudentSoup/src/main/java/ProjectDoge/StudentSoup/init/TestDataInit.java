140feb4f9e2c3a6cd8ce3ac7c7609c641b5a8492
xogns4909 <xogns4909@naver.com>
2023-03-14 13:39:27 +0900

    -refactor : 개시글 작성 테스트 로직 수정

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/init/TestDataInit.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/init/TestDataInit.java
@@ -352,13 +352,13 @@ public class TestDataInit {
 
         for(int i = 1; i <= 5; i++){
             BoardReplyReqDto boardReplyReqDto = new BoardReplyReqDto(board.getId(), member.getMemberId(), "테스트댓글 " + i, null, null, 0);
-            boardReplyRegisterService.join(boardReplyReqDto);
+            boardReplyRegisterService.join(boardReplyReqDto,"dummyTest1");
         }
 
         for(int i = 1; i <= 5; i++){
             for(int j = 1; j <= 4; j++){
                 BoardReplyReqDto boardReplyReqDto = new BoardReplyReqDto(board.getId(), member.getMemberId(), "테스트 대 댓글 " + j, i, null, 1);
-                boardReplyRegisterService.join(boardReplyReqDto);
+                boardReplyRegisterService.join(boardReplyReqDto,"dummyTest1");
             }
         }
 
