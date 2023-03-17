5bf673a87e24da4772d1ca1d20b15f003ed06d36
xogns4909 <xogns4909@naver.com>
2023-03-17 14:43:52 +0900

    -refactor : checkExpireDate method 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/utils/JwtUtil.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/utils/JwtUtil.java
@@ -1,11 +1,10 @@
 package ProjectDoge.StudentSoup.utils;
 
+import ProjectDoge.StudentSoup.exception.jwt.ExpirationDateException;
 import ProjectDoge.StudentSoup.service.redis.RedisUtil;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,37 +14,61 @@ import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class JwtUtil {
 
     public static String getUserName(String token,String secretKey){
         return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token)
                 .getBody().get("userName",String.class);
     }
+
+    public static boolean checkExpireDate(String token,String secretKey){
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            log.info("claims {}",claims.getBody().getExpiration());
+            return claims.getBody().getExpiration().before(new Date());
+        }
+        catch (Exception e){
+            throw new ExpirationDateException("유효기간이 만료된 토큰입니다.");
+        }
+    }
+
     public static boolean isExpired(String token,String secretKey){
        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getExpiration().before(new Date());
     }
     public static Map<String,String> creatJwt(String userName,String secretKey,Long expiredMs,String refreshKey,Long refreshExpiredMs){
+
         Map<String,String> token = new HashMap<>();
         Claims claims = Jwts.claims();
         claims.put("userName",userName);
 
-        String refreshToken = Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + refreshExpiredMs))
-                .signWith(SignatureAlgorithm.HS256, refreshKey)
-                .compact();
+        String refreshToken = createRefreshToken(refreshKey, refreshExpiredMs, claims);
+        String accessToken = createAccessToken(secretKey, expiredMs, claims);
 
+        token.put("accessToken",accessToken);
+        token.put("refreshToken",refreshToken);
+
+
+        return token;
+    }
+
+    public static String createAccessToken(String secretKey, Long expiredMs, Claims claims) {
         String accessToken = Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + expiredMs))
                 .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
-        token.put("accessToken",accessToken);
-        token.put("refreshToken",refreshToken);
+        return accessToken;
+    }
 
-
-        return token;
+    private static String createRefreshToken(String refreshKey, Long refreshExpiredMs, Claims claims) {
+        String refreshToken = Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + refreshExpiredMs))
+                .signWith(SignatureAlgorithm.HS256, refreshKey)
+                .compact();
+        return refreshToken;
     }
 }
