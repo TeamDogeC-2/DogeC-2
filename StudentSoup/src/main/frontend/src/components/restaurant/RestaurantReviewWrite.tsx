9bb6cb163258459a16e87eab18eb3956429a8fd8
eunsung <esk114477@gmail.com>
2023-03-24 12:51:59 +0900

    -fix : mediaquery 수정

--- StudentSoup/src/main/frontend/src/components/restaurant/RestaurantReviewWrite.tsx
+++ StudentSoup/src/main/frontend/src/components/restaurant/RestaurantReviewWrite.tsx
@@ -4,12 +4,12 @@ import camera from '../../img/camera.svg';
 import Circle_human from '../../img/circle_human.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import { DesktopHeader, MobileHeader } from '../../mediaQuery';
+import { Desktop, Mobile } from '../../mediaQuery';
 
 const RestaurantReviewWrite = () => {
   return (
     <>
-      <DesktopHeader>
+      <Desktop>
         <div className="restaurant-detail-review-write-div">
           <div className="restaurant-detail-review-write-top">
             <div className="restaurant-detail-review-write-text">
@@ -64,36 +64,27 @@ const RestaurantReviewWrite = () => {
           </span>
           <button className="restaurant-detail-review-write-bottom-submit-button">등록하기</button>
         </div>
-      </DesktopHeader>
-      <MobileHeader>
-        <div className="restaurant-tablet-detail-review-write-div">
-          <div className="restaurant-tablet-detail-review-write-top">
-            <div className="restaurant-tablet-detail-review-write-text">
-              <img src={review_basic_color} alt="" />
-              <p>리뷰 작성하기</p>
-            </div>
-            <div className="restaurant-tablet-detail-review-write-caution">
-              ※홍보 및 비방 등 부적절한 평가는 평점 산정에서 제외될 수 있습니다.
-            </div>
-          </div>
-          <div className="restaurant-tablet-detail-review-write-middle">
+      </Desktop>
+      <Mobile>
+        <div className="restaurant-mobile-detail-review-write-div">
+          <div className="restaurant-mobile-detail-review-write-middle">
             <span>맛있게 드셨나요?</span>
             <div>별점 들어갈 곳</div>
             <p>고객님의 리뷰가 다른 고객드에게 도움이 될 수 있어요!</p>
           </div>
-          <div className="restaurant-tablet-detail-review-write-middle-textarea-div">
+          <div className="restaurant-mobile-detail-review-write-middle-textarea-div">
             <textarea
-              className="restaurant-tablet-detail-review-write-middle-textarea"
+              className="restaurant-mobile-detail-review-write-middle-textarea"
               placeholder="업주와 다른 사용자들이 상처받지 않도록 좋은 표현과 주문하신 메뉴 및 매장 서비스에 대해서 작성해주세요 :)"
               maxLength={399}
             ></textarea>
-            <div className="restaurant-tablet-detail-review-write-middle-text-count">
+            <div className="restaurant-mobile-detail-review-write-middle-text-count">
               0/400 (최소 5글자)
             </div>
           </div>
-          <div className="restaurant-tablet-detail-review-write-middle-img-div">
-            <div className="restaurant-tablet-detail-review-write-middle-add-img">
-              <div className="restaurant-tablet-detail-review-write-middle-img-input-div">
+          <div className="restaurant-mobile-detail-review-write-middle-img-div">
+            <div className="restaurant-mobile-detail-review-write-middle-add-img">
+              <div className="restaurant-mobile-detail-review-write-middle-img-input-div">
                 <img src={camera} alt="" />
                 <span>사진 첨부</span>
                 <p>0/4</p>
@@ -101,26 +92,26 @@ const RestaurantReviewWrite = () => {
                   type="file"
                   multiple
                   accept=".png,.jpg,.gif,.jpeg,.bmp,.svg"
-                  className="restaurant-tablet-detail-review-write-middle-img-input"
+                  className="restaurant-mobile-detail-review-write-middle-img-input"
                 />
               </div>
             </div>
-            <div className="restaurant-tablet-detail-review-write-middle-add-img-item-div">
-              <div className="restaurant-tablet-detail-review-write-middle-add-img-item">
+            <div className="restaurant-mobile-detail-review-write-middle-add-img-item-div">
+              <div className="restaurant-mobile-detail-review-write-middle-add-img-item">
                 <img src={Circle_human} alt="" />
                 <FontAwesomeIcon
                   icon={faXmark}
-                  className="restaurant-tablet-detail-review-write-middle-delete-button"
+                  className="restaurant-mobile-detail-review-write-middle-delete-button"
                 />
               </div>
             </div>
           </div>
-          <span className="restaurant-tablet-detail-review-write-bottom-text">
+          <span className="restaurant-mobile-detail-review-write-bottom-text">
             사진은 최대 20MB 이하의 JPG, PNG, GIF 파일 10장까지 첨부 가능합니다.
           </span>
-          <button className="restaurant-tablet-detail-review-write-bottom-submit-button">등록하기</button>
+          <button className="restaurant-mobile-detail-review-write-bottom-submit-button">등록하기</button>
         </div>
-      </MobileHeader>
+      </Mobile>
     </>
   );
 };
