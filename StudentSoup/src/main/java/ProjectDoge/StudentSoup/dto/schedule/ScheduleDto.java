6534fe2db062a3df4ecb41748a67064e20c43141
xogns4909 <xogns4909@naver.com>
2023-04-03 11:52:25 +0900

    -feat

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/dto/schedule/ScheduleDto.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/dto/schedule/ScheduleDto.java
@@ -1,2 +1,21 @@
-package ProjectDoge.StudentSoup.dto.schedule;public class schedulDto {
+package ProjectDoge.StudentSoup.dto.schedule;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class ScheduleDto {
+
+    private String DayOfWeek;
+
+    private int startTime;
+
+    private int endTime;
+
+    private String color;
+
+    private String subject;
 }
