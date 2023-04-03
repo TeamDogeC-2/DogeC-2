27a2ff777a0d297a15a735d3f218ad4706ae4571
xogns4909 <xogns4909@naver.com>
2023-04-03 14:21:38 +0900

    -refactor : 시간표 호출 컨트롤러 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/schedule/ScheduleRegisterController.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/schedule/ScheduleRegisterController.java
@@ -2,14 +2,14 @@ package ProjectDoge.StudentSoup.controller.schedule;
 
 import ProjectDoge.StudentSoup.dto.schedule.ScheduleDto;
 import ProjectDoge.StudentSoup.repository.schedule.ScheduleRepository;
+import ProjectDoge.StudentSoup.service.schedule.ScheduleCallService;
 import ProjectDoge.StudentSoup.service.schedule.ScheduleRegisterService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -19,6 +19,14 @@ public class ScheduleRegisterController {
     private final ScheduleRegisterService scheduleRegisterService;
 
     private final ScheduleRepository scheduleRepository;
+
+    private final ScheduleCallService scheduleCallService;
+    @PostMapping("Schedule/{memberId}")
+    public List<ScheduleDto> callSchedule(@PathVariable Long memberId){
+        List<ScheduleDto> schedules = scheduleCallService.getSchedule(memberId);
+        return schedules;
+    }
+
     @PutMapping("schedule/{memberId}")
     public Long createSchedule(ScheduleDto scheduleDto, @PathVariable Long memberId){
         return scheduleRegisterService.join(scheduleDto, memberId);
