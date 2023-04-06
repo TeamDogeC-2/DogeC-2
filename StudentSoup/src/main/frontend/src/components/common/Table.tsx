fb23747808f9ec14c6d0a8139a1b22247a0f328c
KanghoYoo <kangsick98@gmail.com>
2023-04-06 21:28:28 +0900

    -refactor : 함수 분리 및 고객센터 모바일 화면 구성

--- StudentSoup/src/main/frontend/src/components/common/Table.tsx
+++ StudentSoup/src/main/frontend/src/components/common/Table.tsx
@@ -1,71 +1,152 @@
 import React from 'react';
-
+import {
+  type TableProps,
+  type TableHeadTextType,
+  type BoardDataType,
+} from '../../interfaces/BoardTypes';
+import { Desktop, Mobile } from '../../mediaQuery';
 import './table.scss';
 
-interface TableProps {
-  headings: string[];
-  data: any[];
-}
-
-interface tableHeadTextType {
-  [key: string]: string;
-  title: string;
-  writeDate: string;
-}
-
 const Table = ({ headings, data }: TableProps) => {
-  const tableHeadText: tableHeadTextType = { title: '제목', writeDate: '게시일' };
+  const tableHeadText: TableHeadTextType = {
+    title: '제목',
+    authentication: '작성자',
+    writeDate: '게시일',
+    view: '조회수',
+  };
+  const dateFormatting = (item: BoardDataType) => {
+    const date = new Date(item.writeDate);
+    const dotDateFormat: string =
+      String(date.getFullYear()).slice(2, 4) +
+      '.' +
+      String(date.getMonth() + 1 < 9 ? '0' + String(date.getMonth() + 1) : date.getMonth() + 1) +
+      '.' +
+      String(date.getDate() < 9 ? '0' + String(date.getDate()) : date.getDate()) +
+      ' ' +
+      String(date.getHours()) +
+      ':' +
+      String(date.getMinutes() < 9 ? '0' + String(date.getMinutes()) : date.getMinutes());
+
+    return dotDateFormat;
+  };
+
+  const renderCombinedData = (item: BoardDataType) => {
+    const postDate = dateFormatting(item);
+
+    return (
+      <>
+        <div className="write-data-wrap">
+          <span>작성자: {item.authentication}</span>
+          <span>조회수: {item.view}</span>
+        </div>
+        <span>작성일: {postDate}</span>
+      </>
+    );
+  };
 
   return (
-    <table className="custom-table">
-      <thead>
-        <tr>
-          {headings.map((heading: string) => (
-            <th key={heading} className={`${heading.toLowerCase().replace(' ', '-')}-heading`}>
-              {tableHeadText[heading]}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item: any) => (
-          <tr key={item.boardId}>
-            {headings.map((heading: string) => {
-              if (heading === 'writeDate') {
-                const date = new Date(item[heading]);
-
-                const dateFormat2: string =
-                  String(date.getFullYear()).slice(2, 4) +
-                  '.' +
-                  String(
-                    date.getMonth() + 1 < 9
-                      ? '0' + String(date.getMonth() + 1)
-                      : date.getMonth() + 1,
-                  ) +
-                  '.' +
-                  String(date.getDate() < 9 ? '0' + String(date.getDate()) : date.getDate()) +
-                  ' ' +
-                  String(date.getHours()) +
-                  ':' +
-                  String(date.getMinutes());
-
-                return (
-                  <td key={heading} className={`${heading.toLowerCase().replace(' ', '-')}-data`}>
-                    {dateFormat2}
-                  </td>
-                );
-              } else {
-                return (
-                  <td key={heading} className={`${heading.toLowerCase().replace(' ', '-')}-data`}>
-                    {item[heading]}
-                  </td>
-                );
-              }
-            })}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      <Desktop>
+        <table className="custom-table">
+          <thead>
+            <tr>
+              {headings.map((heading: string) => (
+                <th key={heading} className={`${heading.toLowerCase().replace(' ', '-')}-heading`}>
+                  {tableHeadText[heading]}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(item => (
+              <tr key={item.boardId}>
+                {headings.map((heading: string) => {
+                  if (heading === 'writeDate') {
+                    return (
+                      <td
+                        key={heading}
+                        className={`${heading.toLowerCase().replace(' ', '-')}-data`}
+                      >
+                        {dateFormatting(item)}
+                      </td>
+                    );
+                  } else {
+                    return (
+                      <td
+                        key={heading}
+                        className={`${heading.toLowerCase().replace(' ', '-')}-data`}
+                      >
+                        {item[heading]}
+                      </td>
+                    );
+                  }
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Desktop>
+      <Mobile>
+        <table className="custom-table">
+          <thead>
+            <tr>
+              {headings.map((heading: string) => {
+                if (headings.length <= 2) {
+                  return (
+                    <th
+                      key={heading}
+                      className={`${heading.toLowerCase().replace(' ', '-')}-heading`}
+                    >
+                      {tableHeadText[heading]}
+                    </th>
+                  );
+                } else {
+                  <></>;
+                }
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(item => (
+              <tr
+                key={item.boardId}
+                className={headings.length <= 2 ? 'notice-data' : 'customer-service-data'}
+              >
+                {headings.map((heading: string) => {
+                  if (heading === 'title') {
+                    return (
+                      <td
+                        key={heading}
+                        className={`${heading.toLowerCase().replace(' ', '-')}-data`}
+                      >
+                        {item[heading]}
+                      </td>
+                    );
+                  } else if (headings.length <= 2 && heading === 'writeDate') {
+                    return (
+                      <td
+                        key={heading}
+                        className={`${heading.toLowerCase().replace(' ', '-')}-data`}
+                      >
+                        {dateFormatting(item)}
+                      </td>
+                    );
+                  } else if (headings.length > 2 && heading === 'writeDate') {
+                    return (
+                      <>
+                        <div className="combined-data">{renderCombinedData(item)}</div>
+                      </>
+                    );
+                  } else if (headings.length > 2 && ['authentication', 'view'].includes(heading)) {
+                    return null;
+                  }
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Mobile>
+    </>
   );
 };
 
