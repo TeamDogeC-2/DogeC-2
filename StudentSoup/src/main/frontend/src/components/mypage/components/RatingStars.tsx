cfd185a7d2688f231f4d79a0f0f3e202e643a282
Trophy198 <spsp8426@naver.com>
2023-04-07 02:01:29 +0900

    FEAT : 별점 기능 width ,height props 구현

--- StudentSoup/src/main/frontend/src/components/mypage/components/RatingStars.tsx
+++ StudentSoup/src/main/frontend/src/components/mypage/components/RatingStars.tsx
@@ -4,27 +4,21 @@ import './ratingStars.scss';
 
 interface RatingStarsProps {
   rating: number;
+  width?: string;
+  height?: string;
 }
 
-const RatingStars: React.FC<RatingStarsProps> = ({ rating }) => {
+const RatingStars: React.FC<RatingStarsProps> = ({ rating, width = '11px', height = '11px' }) => {
   const filledStars = Array.from({ length: rating }, (_, index) => index + 1);
   const emptyStars = Array.from({ length: 5 - rating }, (_, index) => index + rating + 1);
 
   return (
     <div className="rating-stars">
       {filledStars.map(index => (
-        <ReviewStarIcon
-          key={index}
-          className="rating-star filled"
-          style={{ width: '11px', height: '11px' }}
-        />
+        <ReviewStarIcon key={index} className="rating-star filled" style={{ width, height }} />
       ))}
       {emptyStars.map(index => (
-        <ReviewStarIcon
-          key={index}
-          className="rating-star empty"
-          style={{ width: '11px', height: '11px' }}
-        />
+        <ReviewStarIcon key={index} className="rating-star empty" style={{ width, height }} />
       ))}
     </div>
   );
