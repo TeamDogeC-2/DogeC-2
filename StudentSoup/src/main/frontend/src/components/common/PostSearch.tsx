9f5def9c7ce39ae4451b3e94a52cfa59c8a8b6b9
KanghoYoo <kangsick98@gmail.com>
2023-04-06 21:32:33 +0900

    - feat : 조건부 API 요청 및 키보드 이벤트 추가

--- StudentSoup/src/main/frontend/src/components/common/PostSearch.tsx
+++ StudentSoup/src/main/frontend/src/components/common/PostSearch.tsx
@@ -1,12 +1,16 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { postBoardNoticeSearch } from '../../apis/auth/BoardAPI';
+import { postBoardNoticeSearch, postBoardServiceSearch } from '../../apis/auth/BoardAPI';
 import { type PostSearchPropsType } from '../../interfaces/BoardTypes';
 import './postsearch.scss';
 
-const PostSearch = ({ items, setItems, setPostPerPage }: PostSearchPropsType) => {
-  const [select, setSelect] = useState('');
+const PostSearch = ({ pageTitle, setItems, setPostPerPage }: PostSearchPropsType) => {
+  const [selected, setSelected] = useState('all');
   const [search, setSearch] = useState('');
+
+  const selectBoxChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelected(e.target.value);
+  };
 
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -14,21 +18,29 @@ const PostSearch = ({ items, setItems, setPostPerPage }: PostSearchPropsType) =>
     setSearch(e.target.value.toLowerCase());
   };
 
-  const selectBoxChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelect(e.target.value);
-  };
-
   const onClickSearch = async () => {
-    postBoardNoticeSearch(search).then(response => {
-      setItems(response.data.content);
-      setPostPerPage(response.data.pageable.pageSize);
-    });
+    if (pageTitle === '공지사항') {
+      postBoardNoticeSearch(search).then(response => {
+        setItems(response.data.content);
+        setPostPerPage(response.data.pageable.pageSize);
+      });
+    } else if (pageTitle === '고객센터') {
+      postBoardServiceSearch(search).then(response => {
+        setItems(response.data.content);
+        setPostPerPage(response.data.pageable.pageSize);
+      });
+    }
   };
 
+  const handleOnKeyDownEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onClickSearch();
+    }
+  };
   return (
     <>
       <div className="search-container">
-        <select defaultValue="all" onChange={selectBoxChange}>
+        <select defaultValue={selected} onChange={selectBoxChange}>
           <option value="all">전체</option>
           <option value="title">제목</option>
         </select>
@@ -37,6 +49,7 @@ const PostSearch = ({ items, setItems, setPostPerPage }: PostSearchPropsType) =>
           placeholder="글 제목, 내용을 적어주세요"
           value={search}
           onChange={onChangeSearch}
+          onKeyDown={handleOnKeyDownEnter}
         />
         <button className="search-button" onClick={onClickSearch}>
           검색
