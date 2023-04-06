72d4f46490fc46de83937d34995afc77173d55e5
Trophy198 <spsp8426@naver.com>
2023-04-06 20:23:26 +0900

    FEAT : 댓글 프리뷰 api 연동

--- StudentSoup/src/main/frontend/src/components/mypage/data/MypageContents.ts
+++ StudentSoup/src/main/frontend/src/components/mypage/data/MypageContents.ts
@@ -1,25 +1,12 @@
 import axios from 'axios';
 import axiosInstance from '../../../apis/auth/AxiosInterceptor';
-
+// 게시글 프리뷰
 interface Board {
   boardId: number;
   title: string;
   writeDate: string;
   likedCount: number;
   viewCount: number;
-}
-
-interface Pageable {
-  sort: {
-    empty: boolean;
-    sorted: boolean;
-    unsorted: boolean;
-  };
-  offset: number;
-  pageNumber: number;
-  pageSize: number;
-  paged: boolean;
-  unpaged: boolean;
 }
 
 export interface PreViewBoardResponse {
@@ -29,24 +16,52 @@ export interface PreViewBoardResponse {
   last: boolean;
   number: number;
   numberOfElements: number;
-  pageable: Pageable;
   size: number;
-  sort: {
-    empty: boolean;
-    sorted: boolean;
-    unsorted: boolean;
-  };
   totalElements: number;
   totalPages: number;
 }
-// 게시글 프리뷰
 export const PreViewBoard = async (
   memberId: number,
-  page: number = 1,
+  page: number = 0,
   size: number = 3,
 ): Promise<PreViewBoardResponse> => {
   const response = await axiosInstance.post(
     '/mypage/board',
+    {
+      memberId,
+    },
+    { params: { page, size } },
+  );
+
+  return response.data;
+};
+
+// 댓글 프리뷰
+interface Reply {
+  boardId: number;
+  content: string;
+  likedCount: number;
+  writeDate: string;
+}
+
+export interface PreViewReplyResponse {
+  content: Reply[];
+  empty: boolean;
+  first: boolean;
+  last: boolean;
+  number: number;
+  numberOfElements: number;
+  size: number;
+  totalElements: number;
+  totalPages: number;
+}
+export const PreViewReply = async (
+  memberId: number,
+  page: number = 0,
+  size: number = 3,
+): Promise<PreViewReplyResponse> => {
+  const response = await axiosInstance.post(
+    '/mypage/boardReply',
     {
       memberId,
     },
