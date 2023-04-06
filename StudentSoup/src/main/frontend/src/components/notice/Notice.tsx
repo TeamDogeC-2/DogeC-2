99d15dd8b945768b1aa3c028dabb78b425404679
KanghoYoo <kangsick98@gmail.com>
2023-04-06 21:23:01 +0900

    -refactor : 중복사용되는 UI를 위해 컴포넌트 분리하여 사용

--- StudentSoup/src/main/frontend/src/components/notice/Notice.tsx
+++ StudentSoup/src/main/frontend/src/components/notice/Notice.tsx
@@ -1,25 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import Background from '../common/Background';
-import MainNavbar from '../common/MainNavbar';
-import './notice.scss';
-import Paginate from '../common/Paginate';
-import PostSearch from '../common/PostSearch';
-import Table from '../common/Table';
 import { postBoardNotice } from '../../apis/auth/BoardAPI';
-import { type NoticePostsDataType } from '../../interfaces/BoardTypes';
+import { type BoardDataType } from '../../interfaces/BoardTypes';
+import NoticeAndService from '../common/NoticeAndService';
 
 export const Notice = () => {
-  const [items, setItems] = useState<NoticePostsDataType[]>([]);
+  const [items, setItems] = useState<BoardDataType[]>([]);
   const [count, setCount] = useState(0);
-  const [currentpage, setCurrentpage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
   const [postPerPage, setPostPerPage] = useState(0);
 
   const [indexOfLastPost, setIndexOfLastPost] = useState(0);
   const [indexOfFirstPost, setIndexOfFirstPost] = useState(0);
-  const [currentPosts, setCurrentPosts] = useState<NoticePostsDataType[]>([]);
+  const [currentPosts, setCurrentPosts] = useState<BoardDataType[]>([]);
+
+  const noticeHeader: string[] = ['title', 'writeDate'];
 
   const handlePageChange = (e: React.SetStateAction<number>) => {
-    setCurrentpage(e);
+    setCurrentPage(e);
   };
 
   useEffect(() => {
@@ -31,39 +28,24 @@ export const Notice = () => {
 
   useEffect(() => {
     setCount(items.length);
-    setIndexOfLastPost(currentpage * postPerPage);
+    setIndexOfLastPost(currentPage * postPerPage);
     setIndexOfFirstPost(indexOfLastPost - postPerPage);
     setCurrentPosts(items.slice(indexOfFirstPost, indexOfLastPost));
-  }, [currentpage, items, indexOfFirstPost, indexOfLastPost, postPerPage]);
+  }, [currentPage, items, indexOfFirstPost, indexOfLastPost, postPerPage]);
 
   return (
-    <>
-      <Background>
-        <MainNavbar />
-        <div className="notice-container">
-          <h1>공지사항</h1>
-          <div className="notice-table-wrap">
-            {items.length !== 0 ? (
-              <Table headings={['title', 'writeDate']} data={currentPosts} />
-            ) : (
-              <div>
-                <h3>검색결과가 없습니다.</h3>
-                <span>
-                  검색어의 철자가 정확한지 확인해주세요. <br /> 비슷한 다른 검색어를 입력해보세요.
-                </span>
-              </div>
-            )}
-          </div>
-          <Paginate
-            page={currentpage}
-            count={count}
-            setPage={handlePageChange}
-            postPerPage={postPerPage}
-          />
-          <PostSearch items={items} setItems={setItems} setPostPerPage={setPostPerPage} />
-        </div>
-      </Background>
-    </>
+    <NoticeAndService
+      items={items}
+      setItems={setItems}
+      currentPosts={currentPosts}
+      currentPage={currentPage}
+      count={count}
+      handlePageChange={handlePageChange}
+      postPerPage={postPerPage}
+      setPostPerPage={setPostPerPage}
+      pageTitle="공지사항"
+      tableHeader={noticeHeader}
+    />
   );
 };
 
