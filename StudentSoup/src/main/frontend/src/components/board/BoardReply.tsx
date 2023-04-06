560fdf2f2c736d5488100da4ab4895d966f92d00
eunsung <esk114477@gmail.com>
2023-04-06 14:58:02 +0900

    -style : 게시글 답글 모바일 UI 추가

--- StudentSoup/src/main/frontend/src/components/board/BoardReply.tsx
+++ StudentSoup/src/main/frontend/src/components/board/BoardReply.tsx
@@ -2,33 +2,66 @@ import './boardReply.scss';
 import Circle_human from '../../img/circle_human.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsis, faHeart, faArrowTurnUp } from '@fortawesome/free-solid-svg-icons';
+import { Desktop, Mobile } from '../../mediaQuery';
 
 const BoardReply = () => {
   return (
-    <div className="board-detail-bottom-reply-div">
-      <div className="board-detail-bottom-reply">
-        <div className="board-detail-bottom-reply-left">
-          <FontAwesomeIcon icon={faArrowTurnUp} className="board-detail-reply-icon" />
-          <img src={Circle_human} alt="" />
-          <div className="board-detail-bottom-reply-top-div">
-            <div className="board-detail-bottom-reply-name-div">
-              <span>
-                유저네임 <p>작성날짜</p>
-              </span>
+    <>
+      <Desktop>
+        <div className="board-detail-bottom-reply-div">
+          <div className="board-detail-bottom-reply">
+            <div className="board-detail-bottom-reply-left">
+              <div className="board-detail-bottom-reply-left-top">
+                <FontAwesomeIcon icon={faArrowTurnUp} className="board-detail-reply-icon" />
+                <img src={Circle_human} alt="" />
+                <span>
+                  유저네임 <p>작성날짜</p>
+                </span>
+              </div>
               <p className="board-detail-bottom-reply-content">댓글내용</p>
             </div>
+            <FontAwesomeIcon icon={faEllipsis} className="board-detail-reply-function-icon" />
           </div>
-        </div>
-        <div className="board-detail-bottom-reply-right">
-          <FontAwesomeIcon icon={faEllipsis} className="board-detail-reply-function-icon" />
-          <div className="board-detail-bottom-reply-right-heart">
-            <FontAwesomeIcon icon={faHeart} className="board-detail-reply-function-heart-icon" />
-            <p>14</p>
+          <div className="board-detail-bottom-reply-right">
+            <div className="board-detail-bottom-reply-right-heart">
+              <FontAwesomeIcon icon={faHeart} className="board-detail-reply-function-heart-icon" />
+              <p>14</p>
+            </div>
           </div>
+          <div className="board-detail-underline" />
         </div>
-      </div>
-      <div className="board-detail-underline" />
-    </div>
+      </Desktop>
+      <Mobile>
+        <div className="board-detail-mobile-bottom-reply-div">
+          <div className="board-detail-mobile-bottom-reply">
+            <div className="board-detail-mobile-bottom-reply-left">
+              <div className="board-detail-mobile-bottom-reply-left-top">
+                <FontAwesomeIcon icon={faArrowTurnUp} className="board-detail-mobile-reply-icon" />
+                <img src={Circle_human} alt="" />
+                <span>
+                  유저네임 <p>작성날짜</p>
+                </span>
+              </div>
+              <p className="board-detail-mobile-bottom-reply-content">댓글내용</p>
+            </div>
+            <FontAwesomeIcon
+              icon={faEllipsis}
+              className="board-detail-mobile-reply-function-icon"
+            />
+          </div>
+          <div className="board-detail-mobile-bottom-reply-right">
+            <div className="board-detail-mobile-bottom-reply-right-heart">
+              <FontAwesomeIcon
+                icon={faHeart}
+                className="board-detail-mobile-reply-function-heart-icon"
+              />
+              <p>14</p>
+            </div>
+          </div>
+          <div className="board-detail-underline" />
+        </div>
+      </Mobile>
+    </>
   );
 };
 
