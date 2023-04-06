48fb8711a60d85142c3a6ac67256dc262b301b10
KanghoYoo <kangsick98@gmail.com>
2023-04-06 21:25:49 +0900

    -feat : 고객센터 검색기능, api 호출 추가

---
+++ StudentSoup/src/main/frontend/src/components/customerservice/CustomerService.tsx
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { postBoardCustomerService } from '../../apis/auth/BoardAPI';
+import { type BoardDataType } from '../../interfaces/BoardTypes';
+import NoticeAndService from '../common/NoticeAndService';
+
+const CustomerService = () => {
+  const [items, setItems] = useState<BoardDataType[]>([]);
+  const [count, setCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage, setPostPerPage] = useState(0);
+
+  const [indexOfLastPost, setIndexOfLastPost] = useState(0);
+  const [indexOfFirstPost, setIndexOfFirstPost] = useState(0);
+  const [currentPosts, setCurrentPosts] = useState<BoardDataType[]>([]);
+
+  const serviceHeader: string[] = ['title', 'authentication', 'writeDate', 'view'];
+
+  const handlePageChange = (e: React.SetStateAction<number>) => {
+    setCurrentPage(e);
+  };
+
+  useEffect(() => {
+    postBoardCustomerService().then(response => {
+      setItems(response.data.content);
+      setPostPerPage(response.data.pageable.pageSize);
+    });
+  }, []);
+
+  useEffect(() => {
+    setCount(items.length);
+    setIndexOfLastPost(currentPage * postPerPage);
+    setIndexOfFirstPost(indexOfLastPost - postPerPage);
+    setCurrentPosts(items.slice(indexOfFirstPost, indexOfLastPost));
+  }, [currentPage, items, indexOfFirstPost, indexOfLastPost, postPerPage]);
+
+  return (
+    <NoticeAndService
+      items={items}
+      setItems={setItems}
+      currentPosts={currentPosts}
+      currentPage={currentPage}
+      count={count}
+      handlePageChange={handlePageChange}
+      postPerPage={postPerPage}
+      setPostPerPage={setPostPerPage}
+      pageTitle="고객센터"
+      tableHeader={serviceHeader}
+    />
+  );
+};
+
+export default CustomerService;
