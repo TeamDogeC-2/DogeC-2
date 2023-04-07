22262025d1c7281590cd4f5fe149f58eaa928dc2
xogns4909 <xogns4909@naver.com>
2023-04-07 21:13:01 +0900

    -refactor : 임시저장파일 삭제 api 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/board/BoardTemporaryImageController.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/board/BoardTemporaryImageController.java
@@ -1,7 +1,9 @@
 package ProjectDoge.StudentSoup.controller.board;
 
+import ProjectDoge.StudentSoup.service.board.BoardTemporaryFileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +16,15 @@ import java.util.List;
 @Slf4j
 public class BoardTemporaryImageController {
 
-    private final BoardTemporaryFileResisterService boardTemporaryFileResisterService;
+    private final BoardTemporaryFileService boardTemporaryFileResisterService;
 
     @PostMapping("boards/image/{memberId}")
     public String registerImage(List<MultipartFile> multipartFileList, @PathVariable Long memberId){
        return boardTemporaryFileResisterService.join(memberId,multipartFileList);
+    }
+
+    @DeleteMapping("boards/image/{memberId}")
+    public String deleteImage(@PathVariable Long memberId){
+       return boardTemporaryFileResisterService.deleteImage(memberId);
     }
 }
