9d876deb9cd1d57bb560341c7b20e57d7fde6dda
xogns4909 <xogns4909@naver.com>
2023-04-07 20:20:26 +0900

    -feat : findBymemberId 메소드 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/file/TemporaryFileRepository.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/repository/file/TemporaryFileRepository.java
@@ -3,5 +3,5 @@ package ProjectDoge.StudentSoup.repository.file;
 import ProjectDoge.StudentSoup.entity.file.TemporaryImageFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TemporaryFileRepository extends JpaRepository<TemporaryImageFile,Long> {
+public interface TemporaryFileRepository extends JpaRepository<TemporaryImageFile,Long>,TemporaryFileRepositoryCustom {
 }
