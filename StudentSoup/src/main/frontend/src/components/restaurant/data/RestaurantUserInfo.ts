9b4621a11aa7186765f51960a45904f27d185676
eunsung <esk114477@gmail.com>
2023-04-12 14:06:28 +0900

    -feat : 음식점 정보 데이터 추가

---
+++ StudentSoup/src/main/frontend/src/components/restaurant/data/RestaurantUserInfo.ts
@@ -0,0 +1,18 @@
+import axiosInstance from '../../../apis/auth/AxiosInterceptor';
+
+export interface UserInfoType {
+  memberId: number;
+  schoolId: number;
+  schoolName: string;
+  departmentId: number;
+  departmentName: string;
+  fileName: string;
+  id: string;
+  nickname: string;
+  email: string;
+  registrationDate: string;
+  memberClassification: string;
+}
+export const RestaurantUserInfo = async () => {
+  return await axiosInstance.post('/member/info');
+};
