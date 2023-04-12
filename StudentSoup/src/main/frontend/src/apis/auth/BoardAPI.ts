31d8a40ea09f2f8ab56ad8bc7c879c9ac81eeb3b
KanghoYoo <kangsick98@gmail.com>
2023-04-12 15:31:01 +0900

    - feat : user 정보 API 추가

--- StudentSoup/src/main/frontend/src/apis/auth/BoardAPI.ts
+++ StudentSoup/src/main/frontend/src/apis/auth/BoardAPI.ts
@@ -1,11 +1,20 @@
-import axios from 'axios';
+import axios, { type AxiosResponse } from 'axios';
+import axiosInstance from './AxiosInterceptor';
 
-export const postBoardCategory = async (category: string) => {
+export const postBoardCategory = async (category: string): Promise<AxiosResponse> => {
   const response = await axios.post(`/board/${category}`);
   return response;
 };
 
-export const postBoardCategorySearch = async (category: string, search: string) => {
+export const postBoardCategorySearch = async (
+  category: string,
+  search: string,
+): Promise<AxiosResponse> => {
   const response = await axios.post(`/board/${category}?title=${search}`);
+  return response;
+};
+
+export const postUserInfo = async (): Promise<AxiosResponse> => {
+  const response = await axiosInstance.post('/member/info');
   return response;
 };
