dbc9e239eb7a9621aa1ca3f1cb3d45ba1db09ffd
KanghoYoo <kangsick98@gmail.com>
2023-04-13 21:41:07 +0900

    - feat : Mobile 게시판 컴포넌트 가독성과 관리를 위해서 분리

---
+++ StudentSoup/src/main/frontend/src/components/board/boardcomponents/MobileBoard.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+
+const MobileBoard = (props: any) => {
+  const { currentPage, category, bestBoardItems, hotBoardItems, currentPosts } = props;
+  return (
+    <div className="mobile-board-wrap">
+      {currentPage === 1 && category === 'ALL'
+        ? bestBoardItems.map((post: any) => {
+            return (
+              <>
+                <div key={post.boardId} className="board-table-div best-post">
+                  <div className="board-underline">
+                    <span>[{post.tag}]</span>
+                    <span
+                      className={post.authentication === 'Y' ? 'authentication-post' : undefined}
+                    >
+                      {post.title}
+                    </span>
+                  </div>
+                  <div className="board-info">
+                    <div className="board-info-left">
+                      <p>{post.nickname}</p>
+                      <p>조회수</p>
+                      <p>{post.view}</p>
+                      <p>좋아요수</p>
+                      <p>{post.likedCount}</p>
+                    </div>
+                    <div className="board-info-right">
+                      <p>{post.writeDate}</p>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })
+        : null}
+      {currentPage === 1 && category === 'ALL'
+        ? hotBoardItems.map((post: any) => {
+            return (
+              <>
+                <div key={post.boardId} className="board-table-div best-post">
+                  <div className="board-underline">
+                    <span>[{post.tag}]</span>
+                    <span
+                      className={post.authentication === 'Y' ? 'authentication-post' : undefined}
+                    >
+                      {post.title}
+                    </span>
+                  </div>
+                  <div className="board-info">
+                    <div className="board-info-left">
+                      <p>{post.nickname}</p>
+                      <p>조회수</p>
+                      <p>{post.view}</p>
+                      <p>좋아요수</p>
+                      <p>{post.likedCount}</p>
+                    </div>
+                    <div className="board-info-right">
+                      <p>{post.writeDate}</p>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })
+        : null}
+      {!!currentPosts &&
+        currentPosts.map((post: any) => {
+          return (
+            <>
+              <div key={post.boardId} className="board-table-div">
+                <div className="board-underline">
+                  <span>[{post.tag}]</span>
+                  <span className={post.authentication === 'Y' ? 'authentication-post' : undefined}>
+                    {post.title}
+                  </span>
+                </div>
+                <div className="board-info">
+                  <div className="board-info-left">
+                    <p>{post.nickname}</p>
+                    <p>조회수</p>
+                    <p>{post.view}</p>
+                    <p>좋아요수</p>
+                    <p>{post.likedCount}</p>
+                  </div>
+                  <div className="board-info-right">
+                    <p>{post.writeDate}</p>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+    </div>
+  );
+};
+
+export default MobileBoard;
