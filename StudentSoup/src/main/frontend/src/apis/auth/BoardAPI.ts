782b1d20e82d05ef22ed7f3b3ccf37855bd589b2
KanghoYoo <kangsick98@gmail.com>
2023-04-13 21:30:09 +0900

    - feat : 게시판 가져오기 API 추가

--- StudentSoup/src/main/frontend/src/apis/auth/BoardAPI.ts
+++ StudentSoup/src/main/frontend/src/apis/auth/BoardAPI.ts
@@ -1,14 +1,9 @@
 import axios, { type AxiosResponse } from 'axios';
 import axiosInstance from './AxiosInterceptor';
 
-export const postBoardCategory = async (category: string): Promise<AxiosResponse> => {
-  const response = await axios.post(`/board/${category}`);
-  return response;
-};
-
-export const postBoardCategorySearch = async (
+export const postBoardCategory = async (
   category: string,
-  search: string,
+  search: string = '',
 ): Promise<AxiosResponse> => {
   const response = await axios.post(`/board/${category}?title=${search}`);
   return response;
@@ -16,5 +11,27 @@ export const postBoardCategorySearch = async (
 
 export const postUserInfo = async (): Promise<AxiosResponse> => {
   const response = await axiosInstance.post('/member/info');
+  return response;
+};
+
+export const postBoards = async (
+  schoolId: number,
+  memberId: number,
+  departmentId: number,
+  column: string = '',
+  value: string = '',
+  category: string,
+  sorted: number,
+  page: number,
+  size: number = 12,
+) => {
+  const response = await axiosInstance.post(
+    `/boards?category=${category}&column=${column}&value=${value}&sorted=${sorted}&page=${page}&size=${size}`,
+    {
+      schoolId,
+      memberId,
+      departmentId,
+    },
+  );
   return response;
 };
