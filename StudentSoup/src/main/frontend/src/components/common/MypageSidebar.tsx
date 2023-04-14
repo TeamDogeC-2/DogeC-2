f88e63b087e6f9745d8a942594803c150fb315af
Trophy198 <spsp8426@naver.com>
2023-04-14 15:15:00 +0900

    Fix : 사이드바가 오른쪽 메뉴바를 인식하지 못햇던점 수정

--- StudentSoup/src/main/frontend/src/components/common/MypageSidebar.tsx
+++ StudentSoup/src/main/frontend/src/components/common/MypageSidebar.tsx
@@ -1,22 +1,26 @@
-import React, { useState } from 'react';
+import React, { forwardRef } from 'react';
 import './mypageSidebar.scss';
 
 interface MypageSidebarProps {
   isOpen: boolean;
-  onToggle: () => void;
+  toggleSidebar: (e: any) => void;
 }
 
-const MypageSidebar: React.FC<MypageSidebarProps> = ({ isOpen, onToggle }) => {
-  return (
-    <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-      <ul>
-        <li>홈</li>
-        <li>시간표</li>
-        <li>나의 게시판/댓글</li>
-        <li>나의 리뷰</li>
-      </ul>
-    </div>
-  );
-};
+const MypageSidebar = forwardRef<HTMLDivElement, MypageSidebarProps>(
+  ({ isOpen, toggleSidebar }, ref) => {
+    return (
+      <div ref={ref} className={`sidebar ${isOpen ? 'open' : ''}`}>
+        <ul>
+          <li onClick={toggleSidebar}>홈</li>
+          <li onClick={toggleSidebar}>시간표</li>
+          <li onClick={toggleSidebar}>나의 게시판/댓글</li>
+          <li onClick={toggleSidebar}>나의 리뷰</li>
+        </ul>
+      </div>
+    );
+  },
+);
+
+MypageSidebar.displayName = 'MypageSidebar';
 
 export default MypageSidebar;
