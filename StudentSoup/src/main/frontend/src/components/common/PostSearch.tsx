f0fd6d6c650f38c2e6ae96be6050475eb387a3ad
KanghoYoo <kangsick98@gmail.com>
2023-04-21 21:57:16 +0900

    - feat : 목록, 글쓰기, 게시판 관리 버튼 추가 및 props로 전달 받아 API요청, 회원 등급에 따른 조건부 렌더링, 함수 일부 변경 및 필요없는 import 삭제

--- StudentSoup/src/main/frontend/src/components/common/PostSearch.tsx
+++ StudentSoup/src/main/frontend/src/components/common/PostSearch.tsx
@@ -1,13 +1,24 @@
-import axios from 'axios';
-import React, { useState } from 'react';
-import { postBoardCategory } from '../../apis/auth/BoardAPI';
+import React, { useEffect, useState } from 'react';
 import { type PostSearchPropsType } from '../../interfaces/BoardTypes';
 import './postsearch.scss';
-import Swal from 'sweetalert2';
 
-const PostSearch = ({ pageTitle, setItems, setPostPerPage, currentPage }: PostSearchPropsType) => {
-  const [selected, setSelected] = useState('all');
-  const [search, setSearch] = useState('');
+const PostSearch = ({
+  pageTitle,
+  setCurrentPage,
+  handlePostBoardApi,
+  userInformation,
+}: PostSearchPropsType) => {
+  const [selected, setSelected] = useState<string>('all');
+  const [search, setSearch] = useState<string>('');
+
+  const handleClickList = () => {
+    console.log('클릭됨');
+
+    setSelected('all');
+    setSearch('');
+    setCurrentPage(1);
+    postBoardApi('');
+  };
 
   const selectBoxChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSelected(e.target.value);
@@ -19,46 +30,38 @@ const PostSearch = ({ pageTitle, setItems, setPostPerPage, currentPage }: PostSe
     setSearch(e.target.value.toLowerCase());
   };
 
-  const onClickSearch = async () => {
-    if (pageTitle === '공지사항') {
-      postBoardCategory('ANNOUNCEMENT', currentPage, 12, search)
-        .then(response => {
-          setItems(response.data.content);
-          setPostPerPage(response.data.pageable.pageSize);
-        })
-        .catch(() => {
-          Swal.fire(
-            '서버 통신 실패',
-            '공지사항 목록 불러오기를 실패하였습니다. 다시 시도해 주세요.',
-            'error',
-          );
-        });
-    } else if (pageTitle === '고객센터') {
-      postBoardCategory('CUSTOMERSERVICE', currentPage, 12, search)
-        .then(response => {
-          setItems(response.data.content);
-          setPostPerPage(response.data.pageable.pageSize);
-        })
-        .catch(() => {
-          Swal.fire(
-            '서버 통신 실패',
-            '고객센터 목록 불러오기를 실패하였습니다. 다시 시도해 주세요.',
-            'error',
-          );
-        });
-    }
+  const postBoardApi = (search: string) => {
+    handlePostBoardApi(search);
+  };
+
+  const onClickSearch = () => {
+    postBoardApi(search);
   };
 
   const handleOnKeyDownEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
-      onClickSearch();
+      return onClickSearch();
     }
   };
 
   return (
     <>
+      <div className="board-control-wrap">
+        <button onClick={handleClickList}>목록</button>
+        {userInformation.memberClassification === 'STUDENT' ? (
+          pageTitle === '공지사항' ? null : (
+            <button>글쓰기</button>
+          )
+        ) : null}
+        {userInformation.memberClassification === 'ADMIN' ? (
+          <>
+            <button>글쓰기</button>
+            <button>게시판 관리</button>
+          </>
+        ) : null}
+      </div>
       <div className="search-container">
-        <select defaultValue={selected} onChange={selectBoxChange}>
+        <select key={selected} defaultValue={selected} onChange={selectBoxChange}>
           <option value="all">전체</option>
           <option value="title">제목</option>
         </select>
