eb249ce34f51e3b5cb72da5e03ea38c707c3dd32
KanghoYoo <kangsick98@gmail.com>
2023-04-21 21:54:42 +0900

    - feat : 조건부 렌더링 조건 변경 및 props 변경

--- StudentSoup/src/main/frontend/src/components/common/NoticeAndService.tsx
+++ StudentSoup/src/main/frontend/src/components/common/NoticeAndService.tsx
@@ -9,48 +9,47 @@ import { type NoticeAndServiceProps } from '../../interfaces/BoardTypes';
 
 const NoticeAndService = ({
   items,
-  setItems,
   currentPage,
   count,
   handlePageChange,
   postPerPage,
-  setPostPerPage,
   pageTitle,
   tableHeader,
+  setCurrentPage,
+  handlePostBoardApi,
+  userInformation,
 }: NoticeAndServiceProps) => {
   return (
-    <>
-      <Background>
-        <MainNavbar />
-        <div className="notice-service-container">
-          <h1>{pageTitle}</h1>
-          <div className="notice-service-table-wrap">
-            {items.length !== 0 ? (
-              <Table headings={tableHeader} data={items} />
-            ) : (
-              <div>
-                <h3>검색결과가 없습니다.</h3>
-                <span>
-                  검색어의 철자가 정확한지 확인해주세요. <br /> 비슷한 다른 검색어를 입력해보세요.
-                </span>
-              </div>
-            )}
-          </div>
-          <Paginate
-            page={currentPage}
-            count={count}
-            setPage={handlePageChange}
-            postPerPage={postPerPage}
-          />
-          <PostSearch
-            pageTitle={pageTitle}
-            setItems={setItems}
-            setPostPerPage={setPostPerPage}
-            currentPage={currentPage}
-          />
+    <Background>
+      <MainNavbar />
+      <div className="notice-service-container">
+        <h1>{pageTitle}</h1>
+        <div className="notice-service-table-wrap">
+          {count !== 0 ? (
+            <Table headings={tableHeader} data={items} />
+          ) : (
+            <div>
+              <h3>검색결과가 없습니다.</h3>
+              <span>
+                검색어의 철자가 정확한지 확인해주세요. <br /> 비슷한 다른 검색어를 입력해보세요.
+              </span>
+            </div>
+          )}
         </div>
-      </Background>
-    </>
+        <Paginate
+          page={currentPage}
+          count={count}
+          setPage={handlePageChange}
+          postPerPage={postPerPage}
+        />
+        <PostSearch
+          pageTitle={pageTitle}
+          setCurrentPage={setCurrentPage}
+          handlePostBoardApi={handlePostBoardApi}
+          userInformation={userInformation}
+        />
+      </div>
+    </Background>
   );
 };
 
