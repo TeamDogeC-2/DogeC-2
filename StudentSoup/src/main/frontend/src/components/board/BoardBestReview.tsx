2411c2406cb734fc69942f45f67dae782024713f
eunsung <esk114477@gmail.com>
2023-04-22 16:22:24 +0900

    -feat : 게시글 베스트리뷰 모바일 데이터 연결

--- StudentSoup/src/main/frontend/src/components/board/BoardBestReview.tsx
+++ StudentSoup/src/main/frontend/src/components/board/BoardBestReview.tsx
@@ -43,24 +43,31 @@ const BoardBestReview = ({ bestReview }: Props) => {
         </div>
       </Desktop>
       <Mobile>
-        <div className="board-detail-mobile-bottom-best-review-div">
+        <div id={bestReview.boardReplyId} className="board-detail-mobile-bottom-best-review-div">
           <div className="board-detail-mobile-bottom-best-review">
             <div className="board-detail-mobile-bottom-best-review-left">
               <div className="board-detail-mobile-bottom-best-review-left-top">
-                <img src={Circle_human} alt="" />
+                <img
+                  src={
+                    bestReview.memberProfileImageName
+                      ? `/image/${bestReview.memberProfileImageName}`
+                      : Circle_human
+                  }
+                  alt=""
+                />
                 <span className="board-detail-mobile-bottom-best-text">BEST</span>
                 <span>
-                  유저네임 <p>작성날짜</p>
+                  {bestReview.nickname} <p>{bestReview.writeDate}</p>
                 </span>
               </div>
-              <p className="board-detail-mobile-bottom-best-review-content">댓글 내용</p>
+              <p className="board-detail-mobile-bottom-best-review-content">{bestReview.content}</p>
             </div>
             <FontAwesomeIcon icon={faEllipsis} className="board-detail-mobile-function-icon" />
           </div>
           <div className="board-detail-mobile-bottom-best-review-best-div">
             <div className="board-detail-mobile-bottom-best-review-right-heart">
               <FontAwesomeIcon icon={faHeart} className="board-detail-mobile-function-heart-icon" />
-              <p>14</p>
+              <p>{bestReview.likeCount}</p>
             </div>
           </div>
           <div className="board-detail-mobile-underline" />
