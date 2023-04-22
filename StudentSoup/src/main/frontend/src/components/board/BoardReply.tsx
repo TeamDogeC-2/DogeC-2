2a557a8fcdb4a9471f26d3812e4824de73536c9c
eunsung <esk114477@gmail.com>
2023-04-22 16:17:55 +0900

    -feat : 게시글 대댓글 데이터 연결

--- StudentSoup/src/main/frontend/src/components/board/BoardReply.tsx
+++ StudentSoup/src/main/frontend/src/components/board/BoardReply.tsx
@@ -4,32 +4,44 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsis, faHeart, faArrowTurnUp } from '@fortawesome/free-solid-svg-icons';
 import { Desktop, Mobile } from '../../mediaQuery';
 
-const BoardReply = () => {
+interface Props {
+  reply: any;
+}
+
+const BoardReply = ({ reply }: Props) => {
+  console.log(reply);
   return (
     <>
       <Desktop>
-        <div className="board-detail-bottom-reply-div">
-          <div className="board-detail-bottom-reply">
-            <div className="board-detail-bottom-reply-left">
-              <div className="board-detail-bottom-reply-left-top">
-                <FontAwesomeIcon icon={faArrowTurnUp} className="board-detail-reply-icon" />
-                <img src={Circle_human} alt="" />
-                <span>
-                  유저네임 <p>작성날짜</p>
-                </span>
+        {reply.map((reply: any) => (
+          <>
+            <div id={reply.boardReplyId} className="board-detail-bottom-reply-div">
+              <div className="board-detail-bottom-reply">
+                <div className="board-detail-bottom-reply-left">
+                  <div className="board-detail-bottom-reply-left-top">
+                    <FontAwesomeIcon icon={faArrowTurnUp} className="board-detail-reply-icon" />
+                    <img src={Circle_human} alt="" />
+                    <span>
+                      {reply.nickname} <p>{reply.writeDate}</p>
+                    </span>
+                  </div>
+                  <p className="board-detail-bottom-reply-content">{reply.content}</p>
+                </div>
+                <FontAwesomeIcon icon={faEllipsis} className="board-detail-reply-function-icon" />
               </div>
-              <p className="board-detail-bottom-reply-content">댓글내용</p>
+              <div className="board-detail-bottom-reply-right">
+                <div className="board-detail-bottom-reply-right-heart">
+                  <FontAwesomeIcon
+                    icon={faHeart}
+                    className="board-detail-reply-function-heart-icon"
+                  />
+                  <p>{reply.likeCount}</p>
+                </div>
+              </div>
+              <div className="board-detail-underline" />
             </div>
-            <FontAwesomeIcon icon={faEllipsis} className="board-detail-reply-function-icon" />
-          </div>
-          <div className="board-detail-bottom-reply-right">
-            <div className="board-detail-bottom-reply-right-heart">
-              <FontAwesomeIcon icon={faHeart} className="board-detail-reply-function-heart-icon" />
-              <p>14</p>
-            </div>
-          </div>
-          <div className="board-detail-underline" />
-        </div>
+          </>
+        ))}
       </Desktop>
       <Mobile>
         <div className="board-detail-mobile-bottom-reply-div">
