6a804c6a81c495b9980bc10c51696d06e59c5afa
eunsung <esk114477@gmail.com>
2023-04-22 16:18:41 +0900

    -feat : 게시글 대댓글로 리스트 전달

--- StudentSoup/src/main/frontend/src/components/board/BoardReview.tsx
+++ StudentSoup/src/main/frontend/src/components/board/BoardReview.tsx
@@ -3,32 +3,44 @@ import Circle_human from '../../img/circle_human.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsis, faHeart } from '@fortawesome/free-solid-svg-icons';
 import { Desktop, Mobile } from '../../mediaQuery';
+import BoardReply from './BoardReply';
 
-const BoardReview = () => {
+interface Props {
+  review: any;
+}
+
+const BoardReview = ({ review }: Props) => {
   return (
     <>
       <Desktop>
-        <div className="board-detail-bottom-review-div">
-          <div className="board-detail-bottom-review">
-            <div className="board-detail-bottom-review-left">
-              <div className="board-detail-bottom-review-left-top">
-                <img src={Circle_human} alt="" />
-                <span>
-                  유저네임 <p>작성날짜</p>
-                </span>
+        {review.map((review: any) => (
+          <>
+            <div id={review.boardReplyId} className="board-detail-bottom-review-div">
+              <div className="board-detail-bottom-review">
+                <div className="board-detail-bottom-review-left">
+                  <div className="board-detail-bottom-review-left-top">
+                    <img src={Circle_human} alt="" />
+                    <span>
+                      {review.nickname} <p>{review.writeDate}</p>
+                    </span>
+                  </div>
+                  <p className="board-detail-bottom-review-content">{review.content}</p>
+                </div>
+                <FontAwesomeIcon icon={faEllipsis} className="board-detail-function-icon" />
               </div>
-              <p className="board-detail-bottom-review-content">댓글내용</p>
+              <div className="board-detail-bottom-review-right">
+                <div className="board-detail-bottom-review-right-heart">
+                  <FontAwesomeIcon icon={faHeart} className="board-detail-function-heart-icon" />
+                  <p>{review.likeCount}</p>
+                </div>
+              </div>
+              <div className="board-detail-underline" />
+              {review.boardNestedReplyDtoList.length !== 0 && (
+                <BoardReply reply={review.boardNestedReplyDtoList} />
+              )}
             </div>
-            <FontAwesomeIcon icon={faEllipsis} className="board-detail-function-icon" />
-          </div>
-          <div className="board-detail-bottom-review-right">
-            <div className="board-detail-bottom-review-right-heart">
-              <FontAwesomeIcon icon={faHeart} className="board-detail-function-heart-icon" />
-              <p>14</p>
-            </div>
-          </div>
-          <div className="board-detail-underline" />
-        </div>
+          </>
+        ))}
       </Desktop>
       <Mobile>
         <div className="board-detail-mobile-bottom-review-div">
