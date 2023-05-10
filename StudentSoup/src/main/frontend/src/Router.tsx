6137839994c53e60220d2811f77d38a4082aefaf
KanghoYoo <kangsick98@gmail.com>
2023-05-10 20:04:14 +0900

    - refactor : 헤더 공통 사용부분 수정후 라우터로 묶어서 사용

--- StudentSoup/src/main/frontend/src/Router.tsx
+++ StudentSoup/src/main/frontend/src/Router.tsx
@@ -1,6 +1,7 @@
 import Board from 'components/board/Board';
 import BoardDetail from 'components/board/BoardDetail';
 import BoardWrite from 'components/board/BoardWrite';
+import MainNavbar from 'components/common/MainNavbar';
 import NoticeAndServiceDetail from 'components/common/NoticeAndServiceDetail';
 import PrivateRoute from 'components/common/PrivateRoute';
 import CustomerService from 'pages/customerservice/CustomerService';
@@ -13,6 +14,7 @@ import MypageScheduler from 'pages/mypage/MypageScheduler';
 import Notice from 'pages/notice/Notice';
 import Restaurant from 'pages/restaurant/Restaurant';
 import RestaurantDetail from 'pages/restaurant/RestaurantDetail';
+import RestaurantNavbar from 'pages/restaurant/RestaurantNavbar';
 import SignUpProcess1 from 'pages/signup/SignUpProcess1';
 import SignUpProcess2 from 'pages/signup/SignUpProcess2';
 import SignUpProcess3 from 'pages/signup/SignUpProcess3';
@@ -23,22 +25,28 @@ const Router = () => {
   return (
     <Routes>
       <Route path="*" element={<Err404 />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/login/findAccount" element={<FindAccount />} />
-      <Route path="/signup/process/1" element={<SignUpProcess1 />} />
-      <Route path="/signup/process/2" element={<SignUpProcess2 />} />
-      <Route path="/signup/process/3" element={<SignUpProcess3 />} />
-      <Route path="/notice" element={<Notice />} />
-      <Route path="/notice/detail/:boardId" element={<NoticeAndServiceDetail />} />
-      <Route path="/customerservice" element={<CustomerService />} />
-      <Route path="/customerservice/detail/:boardId" element={<NoticeAndServiceDetail />} />
+      <Route path="/" element={<MainNavbar />}>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/login/findAccount" element={<FindAccount />} />
+        <Route path="/signup/process/1" element={<SignUpProcess1 />} />
+        <Route path="/signup/process/2" element={<SignUpProcess2 />} />
+        <Route path="/signup/process/3" element={<SignUpProcess3 />} />
+        <Route path="/notice" element={<Notice />} />
+        <Route path="/notice/detail/:boardId" element={<NoticeAndServiceDetail />} />
+        <Route path="/customerservice" element={<CustomerService />} />
+        <Route path="/customerservice/detail/:boardId" element={<NoticeAndServiceDetail />} />
+      </Route>
       <Route element={<PrivateRoute />}>
-        <Route path="/restaurant/:school" element={<Restaurant />} />
-        <Route path="/restaurant/detail" element={<RestaurantDetail />} />
-        <Route path="/board" element={<Board />} />
-        <Route path="/board/detail/:boardId" element={<BoardDetail />} />
-        <Route path="/board/write" element={<BoardWrite />} />
+        <Route element={<RestaurantNavbar />}>
+          <Route path="/restaurant/:school" element={<Restaurant />} />
+          <Route path="/restaurant/detail" element={<RestaurantDetail />} />
+        </Route>
+        <Route element={<MainNavbar />}>
+          <Route path="/board" element={<Board />} />
+          <Route path="/board/detail/:boardId" element={<BoardDetail />} />
+          <Route path="/board/write" element={<BoardWrite />} />
+        </Route>
         <Route path="/mypage" element={<MypageMain />} />
         <Route path="/mypage/scheduler" element={<MypageScheduler />} />
       </Route>
