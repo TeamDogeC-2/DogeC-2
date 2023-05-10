30c32d3e9c349bf535497b537313cd8bac95cf81
KanghoYoo <kangsick98@gmail.com>
2023-05-10 21:12:13 +0900

    - refactor : 파일 네임 소문자로 되어 삭제후 추가

--- StudentSoup/src/main/frontend/src/apis/api/userAPI.ts
+++
@@ -1,6 +0,0 @@
-import axios, { type AxiosResponse } from 'axios';
-
-export const getDepartment = async (schoolId: number): Promise<AxiosResponse> => {
-  const res = await axios.post(`/members/signUp/3/${schoolId}`);
-  return res;
-};
