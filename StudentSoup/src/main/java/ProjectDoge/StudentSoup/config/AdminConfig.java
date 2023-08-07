bcf7e70b398b94c820ddd8ac586babef6c5de334
xogns4909 <xogns4909@naver.com>
2023-08-08 02:13:18 +0900

    Feat : 어드민 인증 인터셉터 추가

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/config/AdminConfig.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/config/AdminConfig.java
@@ -1,6 +1,7 @@
 package ProjectDoge.StudentSoup.config;
 
 import ProjectDoge.StudentSoup.interceptor.AdminInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,11 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class AdminConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(new AdminInterceptor())
-//                .order(1)
-//                .addPathPatterns("/admin", "/admin/**")
-//                .excludePathPatterns("/css/**", "/*.ico", "/error", "/js/**");
-//    }
+    @Autowired
+    private AdminInterceptor adminInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(adminInterceptor)
+                .order(0)  // 이 순서를 Spring Security 보다 먼저 실행되도록 설정
+                .addPathPatterns("/admin/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error", "/js/**");
+    }
 }
--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/interceptor/AdminInterceptor.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/interceptor/AdminInterceptor.java
@@ -1,29 +1,39 @@
 package ProjectDoge.StudentSoup.interceptor;
 
-import ProjectDoge.StudentSoup.entity.member.MemberClassification;
-import ProjectDoge.StudentSoup.exception.admin.MemberClassificationNotAdminException;
+import ProjectDoge.StudentSoup.utils.JwtUtil;
+import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
+@Configuration
 @Slf4j
 public class AdminInterceptor implements HandlerInterceptor {
 
+    @Value("${jwt.secret}")
+    private String secretKey;
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-        String requestURI = request.getRequestURI();
-
-        log.info("인증 체크 인터셉터 실행 {}", requestURI);
-        HttpSession session = request.getSession(false);
-
-        if (session == null || !session.getAttribute(SessionConst.LOGIN_MEMBER).equals(MemberClassification.ADMIN.toString())){
-            log.info("미인증 사용자 요청");
-            // 로그인으로 redirect
-            throw new MemberClassificationNotAdminException("로그인 한 회원은 운영자가 아닙니다.");
+        log.info("어드민 인증로직이 실행되었습니다.");
+        String token = request.getHeader("Authorization");
+        if(token == null || token.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return false;
+        }
+        log.info(token+ " "+ secretKey);
+        // 당신의 JWT 복호화 로직
+        String claims = JwtUtil.getRole(token.split(" ")[1],secretKey);
+        log.info((claims));
+        if(claims == null || !claims.equals("ADMIN")) {
+            log.info("문제가 많아요 ");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Unauthorized");
+            response.flushBuffer();
+            throw new Exception("Unauthorized access request");
         }
         return true;
     }
