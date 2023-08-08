8c6fd1b333f7746fd3318e22870e77b0eca8350b
xogns4909 <xogns4909@naver.com>
2023-08-09 00:16:46 +0900

    Refactor  : 음식점 생성시 알람서비스 로직 수정

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/commonmodule/FcmService.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/commonmodule/FcmService.java
@@ -1,12 +1,15 @@
 package ProjectDoge.StudentSoup.commonmodule;
 
+import com.google.firebase.messaging.Notification;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class FcmService {
     private final FirebaseMessaging firebaseMessaging;
 
@@ -15,22 +18,30 @@ public class FcmService {
         this.firebaseMessaging = firebaseMessaging;
     }
 
-    public String sendEmail(String email, String subject, String content) {
-        // 여기서 FCM을 이용하여 메일 내용을 푸시 알림으로 보냅니다.
-        Message message = Message.builder()
-                .putData("email", email)
-                .putData("subject", subject)
-                .putData("content", content)
-                .setTopic("school-created") // 토픽을 적절하게 설정하세요
+    public void sendRestaurantCreationNotification(Long restaurantId, String restaurantName, String schoolName) {
+        // 제목과 본문 설정
+        Notification notification = Notification.builder()
+                .setTitle("새 음식점 추가 알림")
+                .setBody(schoolName + " 주변 " + restaurantName + " 음식점이 추가 되었습니다.")
                 .build();
 
-        String response;
+        // 메시지 구성
+        Message message = Message.builder()
+                .setNotification(notification)
+                .putData("restaurantId", restaurantId.toString())
+                .putData("restaurantName", restaurantName)
+                .setTopic(schoolName) // 해당 토픽에 대한 구독자들에게 메시지를 보냅니다.
+                .build();
+
+        // 메시지 전송
         try {
-            response = firebaseMessaging.send(message);
+            log.info("알림 전송 메소드가 실행되었습니다.");
+            String response = firebaseMessaging.send(message);
+            log.info("message ===="+ message);
+            log.info("response ====" + " " +response);
         } catch (FirebaseMessagingException e) {
             e.printStackTrace();
-            response = "Failed to send email notification";
+            // 여기서 필요한 오류 처리를 할 수 있습니다.
         }
-        return response;
     }
 }
