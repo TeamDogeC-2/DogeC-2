44f91a78dcff1222ac9b7b6f29668a739ff553b9
xogns4909 <xogns4909@naver.com>
2023-08-10 02:07:55 +0900

    Fix : AdminRestuarantMenuController 변경시 버그 수정 response Entity -> Respons Dto 수정,@Model 삭제

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/admin/AdminRestaurantMenuController.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/controller/admin/AdminRestaurantMenuController.java
@@ -1,5 +1,6 @@
 package ProjectDoge.StudentSoup.controller.admin;
 
+import ProjectDoge.StudentSoup.dto.restaurantmenu.RestaurantMenuDto;
 import ProjectDoge.StudentSoup.dto.restaurantmenu.RestaurantMenuFormDto;
 import ProjectDoge.StudentSoup.dto.restaurantmenu.RestaurantMenuUpdateDto;
 import ProjectDoge.StudentSoup.entity.restaurant.Restaurant;
@@ -31,53 +32,46 @@ public class AdminRestaurantMenuController {
     private final AdminRestaurantMenuService adminRestaurantMenuService;
 
     @GetMapping("admin/{restaurantId}/restaurantMenus")
-    public List<Object> RestaurantMenuList(@PathVariable Long restaurantId, Model model){
+    public List<Object> RestaurantMenuList(@PathVariable Long restaurantId){
         List<Object> result = new ArrayList<>();
         List<RestaurantMenu> restaurantMenus = restaurantMenuRepository.findByRestaurantId(restaurantId);
-        result.add(restaurantMenus);
+        List<RestaurantMenuDto> restaurantMenuDtos = new ArrayList<>();
+        for(RestaurantMenu restaurantMenu : restaurantMenus){
+            restaurantMenuDtos.add(new RestaurantMenuDto().createRestaurantMenu(restaurantMenu,false));
+        }
+        result.add(restaurantMenuDtos);
         result.add(restaurantId);
         return result;
     }
 
 
     @GetMapping("admin/restaurantMenu")
-    public List<Object> createRestaurantMenu(@RequestParam Long restaurantId,Model model){
-        Restaurant restaurant = restaurantFindService.findOne(restaurantId);
-        List<Object> result = new ArrayList<>();
-        result.add(RestaurantMenuCategory.values());
-        result.add(result);
-        return result;
+    public RestaurantMenuCategory[] createRestaurantMenu(@RequestParam Long restaurantId){
+        RestaurantMenuCategory[] restaurantMenu = RestaurantMenuCategory.values();
+        return restaurantMenu;
     }
     @PostMapping("admin/restaurantMenu")
-    public ResponseEntity<String> createRestaurantMenu(@ModelAttribute RestaurantMenuFormDto form , @RequestPart MultipartFile multipartFile, RedirectAttributes redirect){
+    public ResponseEntity<String> createRestaurantMenu(@RequestBody RestaurantMenuFormDto form , @RequestPart MultipartFile multipartFile, RedirectAttributes redirect){
         restaurantMenuRegisterService.join(form,multipartFile);
-
-
         return ResponseEntity.ok("okay");
     }
     @GetMapping("admin/restaurantMenu/edit/{restaurantMenuId}")
-    public String editRestaurantMenu(@PathVariable Long restaurantMenuId,Model model){
+    public RestaurantMenuUpdateDto editRestaurantMenu(@PathVariable Long restaurantMenuId){
         RestaurantMenuUpdateDto restaurantMenuUpdateDto = adminRestaurantMenuService.adminFindUpdateRestaurantMenu(restaurantMenuId);
-        Restaurant restaurant = restaurantFindService.findOne(restaurantMenuUpdateDto.getRestaurantId());
-        model.addAttribute("restaurantMenuForm",restaurantMenuUpdateDto);
-        model.addAttribute("restaurant",restaurant);
-        model.addAttribute("restaurantMenuId",restaurantMenuId);
-        model.addAttribute("menuCategory",RestaurantMenuCategory.values());
-        return "admin/restaurant/updateRestaurantMenu";
+        return restaurantMenuUpdateDto;
     }
     @PostMapping ("admin/restaurantMenu/edit/{restaurantMenuId}")
-    public String editRestaurantMenu(@PathVariable Long restaurantMenuId,@RequestPart MultipartFile multipartFile,RestaurantMenuUpdateDto restaurantMenuUpdateDto,RedirectAttributes redirect){
+    public Long editRestaurantMenu(@PathVariable Long restaurantMenuId,@RequestPart MultipartFile multipartFile,RestaurantMenuUpdateDto restaurantMenuUpdateDto,RedirectAttributes redirect){
         adminRestaurantMenuService.adminUpdateRestaurantMenu(restaurantMenuId,multipartFile,restaurantMenuUpdateDto);
-        redirect.addAttribute("restaurantId",restaurantMenuUpdateDto.getRestaurantId());
 
-        return "redirect:/admin/{restaurantId}/restaurantMenus";
+        return restaurantMenuId;
     }
     @GetMapping ("admin/restaurantMenu/{restaurantMenuId}/{restaurantId}")
-    public String deleteRestaurantMenu(@PathVariable Long restaurantMenuId,@PathVariable Long restaurantId,RedirectAttributes redirect){
+    public Long deleteRestaurantMenu(@PathVariable Long restaurantMenuId,@PathVariable Long restaurantId,RedirectAttributes redirect){
         restaurantMenuRepository.deleteById(restaurantMenuId);
         redirect.addAttribute("restaurantId",restaurantId);
 
-        return "redirect:/admin/{restaurantId}/restaurantMenus";
+        return restaurantId;
     }
 
 }
