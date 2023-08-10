84f886d1a11b5dc636fd20b11341446f5d76eb62
Trophy198 <spsp8426@naver.com>
2023-08-10 09:47:57 +0900

    ADD : 음식점 조회 컴포넌트 추가

---
+++ StudentSoup/src/main/frontend/src/pages/admin/AdminRestaurantView.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './adminrestaurantview.scss';
+import AdminNavbar from './AdminNavbar';
+
+const AdminRestaurantView = () => {
+  return (
+    <div className="adminpage-maincontainer">
+      <AdminNavbar />
+      <div>s</div>
+    </div>
+  );
+};
+
+export default AdminRestaurantView;
