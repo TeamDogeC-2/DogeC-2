704095261c11f6e0c2bb73290d0bf88e0cdb9c55
Trophy198 <spsp8426@naver.com>
2023-08-11 14:16:50 +0900

    FEAT : 음식점 메뉴 수정 모드 추가

--- StudentSoup/src/main/frontend/src/pages/admin/AdminMenuModal.tsx
+++ StudentSoup/src/main/frontend/src/pages/admin/AdminMenuModal.tsx
@@ -6,9 +6,23 @@ interface MenuModalProps {
   isOpen: boolean;
   onClose: () => void;
   restaurantId: number;
+  isEditMode: boolean;
+  selectedMenu: {
+    menuId: number;
+    menuName: string;
+    menuCategory: string;
+    cost: number;
+    fileName: string | null;
+  } | null;
 }
 type CategoryType = 'Main' | 'Side' | 'Drink';
-const AdminMenuModal: React.FC<MenuModalProps> = ({ isOpen, onClose, restaurantId }) => {
+const AdminMenuModal: React.FC<MenuModalProps> = ({
+  isOpen,
+  onClose,
+  restaurantId,
+  isEditMode,
+  selectedMenu,
+}) => {
   const [name, setName] = useState('');
   const [category, setCategory] = useState('');
   const [price, setPrice] = useState('');
@@ -16,10 +30,23 @@ const AdminMenuModal: React.FC<MenuModalProps> = ({ isOpen, onClose, restaurantI
   const [previewImage, setPreviewImage] = useState<string | null>(null);
   const [categories, setCategories] = useState<CategoryType[]>([]);
   const fileInputRef = useRef<HTMLInputElement>(null);
+  console.log(isEditMode);
+  useEffect(() => {
+    if (isEditMode && selectedMenu) {
+      setName(selectedMenu.menuName);
+      setCategory(selectedMenu.menuCategory);
+      setPrice(selectedMenu.cost.toString());
+    }
+  }, [isEditMode, selectedMenu]);
   const categoryMapping: Record<CategoryType, string> = {
     Main: '메인메뉴',
     Side: '사이드 메뉴',
     Drink: '음료 및 주류',
+  };
+  const reverseCategoryMapping: Record<string, CategoryType> = {
+    주메뉴: 'Main',
+    사이드메뉴: 'Side',
+    '음료 및 주류': 'Drink',
   };
   const initializeForm = () => {
     setName('');
@@ -37,14 +64,24 @@ const AdminMenuModal: React.FC<MenuModalProps> = ({ isOpen, onClose, restaurantI
 
       formData.append('restaurantId', restaurantId.toString());
       formData.append('name', name);
-      formData.append('restaurantMenuCategory', category);
+      if (isEditMode && selectedMenu) {
+        const categoryType = reverseCategoryMapping[selectedMenu.menuCategory]; // 카테고리 변환
+        formData.append('restaurantMenuCategory', categoryType);
+      } else {
+        formData.append('restaurantMenuCategory', category);
+      }
       formData.append('cost', price);
 
       if (image) {
         formData.append('multipartFile', image);
       }
 
-      await axiosInstance.post('/admin/restaurantMenu', formData, {
+      let url = '/admin/restaurantMenu';
+      if (isEditMode && selectedMenu) {
+        url = `/admin/restaurantMenu/edit/${selectedMenu.menuId}`; // 수정 모드 URL
+      }
+
+      await axiosInstance.post(url, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
@@ -52,13 +89,14 @@ const AdminMenuModal: React.FC<MenuModalProps> = ({ isOpen, onClose, restaurantI
 
       Swal.fire({
         title: '성공',
-        text: '메뉴가 성공적으로 등록되었습니다.',
+        text: isEditMode
+          ? '메뉴가 성공적으로 수정되었습니다.'
+          : '메뉴가 성공적으로 등록되었습니다.',
         icon: 'success',
         confirmButtonText: '확인',
       }).then(result => {
         onClose();
         if (result.isConfirmed) {
-          getRestaurantMenus();
           initializeForm();
         }
       });
@@ -97,8 +135,9 @@ const AdminMenuModal: React.FC<MenuModalProps> = ({ isOpen, onClose, restaurantI
           value={name}
           onChange={e => setName(e.target.value)}
         />
-        <select value={category} onChange={e => setCategory(e.target.value)}>
-          <option value="">카테고리를 선택해주세요</option>
+
+        <select value={category} onChange={e => setCategory(e.target.value)} disabled={isEditMode}>
+          <option value="">{isEditMode ? category : '카테고리를 선택해주세요.'}</option>
           {categories.map((option, index) => (
             <option key={index} value={option}>
               {categoryMapping[option]}
@@ -137,7 +176,14 @@ const AdminMenuModal: React.FC<MenuModalProps> = ({ isOpen, onClose, restaurantI
         )}
         <div className="adminmenumodal-bottom-button">
           <button onClick={handleSubmit}>저장</button>
-          <button onClick={onClose}>취소</button>
+          <button
+            onClick={() => {
+              onClose();
+              initializeForm();
+            }}
+          >
+            취소
+          </button>
         </div>
       </div>
     </div>
