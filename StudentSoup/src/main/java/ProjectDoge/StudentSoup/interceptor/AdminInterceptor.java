67b517669ce69823f2a13adff20bbe1c999e7c50
xogns4909 <xogns4909@naver.com>
2023-08-13 19:23:43 +0900

    UPDATE :광법위한 EXCEPTION -> IOEXCEPTION 수정

--- StudentSoup/src/main/java/ProjectDoge/StudentSoup/interceptor/AdminInterceptor.java
+++ StudentSoup/src/main/java/ProjectDoge/StudentSoup/interceptor/AdminInterceptor.java
@@ -10,6 +10,8 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Configuration
 @Slf4j
 public class AdminInterceptor implements HandlerInterceptor {
@@ -17,7 +19,7 @@ public class AdminInterceptor implements HandlerInterceptor {
     @Value("${jwt.secret}")
     private String secretKey;
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
         log.info("어드민 인증로직이 실행되었습니다.");
         String token = request.getHeader("Authorization");
         if(token == null || token.isEmpty()) {
@@ -29,11 +31,10 @@ public class AdminInterceptor implements HandlerInterceptor {
         String claims = JwtUtil.getRole(token.split(" ")[1],secretKey);
         log.info((claims));
         if(claims == null || !claims.equals("ADMIN")) {
-            log.info("문제가 많아요 ");
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             response.getWriter().write("Unauthorized");
             response.flushBuffer();
-            throw new Exception("Unauthorized access request");
+            throw new IOException("Unauthorized access request");
         }
         return true;
     }
