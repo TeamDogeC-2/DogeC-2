fb9a292b0fa18747a39fd399b5dd6d9762bbb38e
Trophy198 <spsp8426@naver.com>
2023-08-17 18:47:01 +0900

    FEAT : 회원 삭제 및 회원별 로그보기 기능 추가

--- StudentSoup/src/main/frontend/src/pages/admin/Adminpage.tsx
+++ StudentSoup/src/main/frontend/src/pages/admin/Adminpage.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import './adminpage.scss';
 import axiosInstance from 'apis/utils/AxiosInterceptor';
 import AdminNavbar from './AdminNavbar';
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
 
 interface MemberType {
   departmentId: number;
@@ -19,9 +21,9 @@ interface MemberType {
 
 const Adminpage = () => {
   const [members, setMembers] = useState<MemberType[]>([]);
-
-  useEffect(() => {
-    axiosInstance
+  const navigate = useNavigate();
+  const MemberList = async () => {
+    await axiosInstance
       .get('admin/members', { params: { field: null, value: '' } })
       .then(res => {
         setMembers(res.data.members);
@@ -29,36 +31,77 @@ const Adminpage = () => {
       .catch(err => {
         console.error(err);
       });
+  };
+  useEffect(() => {
+    MemberList();
   }, []);
+  const handleUserClick = async (memberName: string, memberId: number) => {
+    Swal.fire({
+      title: `${memberName}`,
+      html: `
+        <div>
+          <button id="edit-menu" class="custom-swal-button">로그 보기</button>
+          <button id="delete-restaurant" class="custom-swal-button">회원 삭제</button>
+        </div>
+      `,
+      showConfirmButton: false,
+      focusConfirm: false,
+      willOpen: () => {
+        document.getElementById('edit-menu')?.addEventListener('click', () => {
+          navigate('/admin/userlog', { state: { memberId, memberName } });
+          Swal.close();
+        });
+        document.getElementById('delete-restaurant')?.addEventListener('click', () => {
+          Swal.fire({
+            title: `${memberName} 회원을 삭제하시겠습니까?`,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: '확인',
+            cancelButtonText: '취소',
+          }).then(async () => {
+            await axiosInstance.get(`/admin/member/delete/${memberId}`).then(() => {
+              Swal.fire(`성공적으로 ${memberName} 회원을 삭제하였습니다.`, '', 'success');
+              MemberList();
+            });
+          });
+        });
+      },
+    });
+  };
 
   return (
-    <div className="adminpage-maincontainer">
+    <Fragment>
       <AdminNavbar />
-      <table className="adminpage-table">
-        <thead>
-          <tr>
-            <th>닉네임</th>
-            <th>이메일</th>
-            <th>가입일</th>
-            <th>학교이름</th>
-            <th>학과이름</th>
-            <th>분류</th>
-          </tr>
-        </thead>
-        <tbody>
-          {members.map(member => (
-            <tr key={member.memberId}>
-              <td>{member.nickname}</td>
-              <td>{member.email}</td>
-              <td>{member.registrationDate}</td>
-              <td>{member.schoolName}</td>
-              <td>{member.departmentName}</td>
-              <td>{member.memberClassification}</td>
+      <div className="adminpage-maincontainer">
+        <table className="adminpage-table">
+          <thead>
+            <tr>
+              <th>닉네임</th>
+              <th>이메일</th>
+              <th>가입일</th>
+              <th>학교이름</th>
+              <th>학과이름</th>
+              <th>분류</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {members.map(member => (
+              <tr
+                onClick={async () => await handleUserClick(member.nickname, member.memberId)}
+                key={member.memberId}
+              >
+                <td>{member.nickname}</td>
+                <td>{member.email}</td>
+                <td>{member.registrationDate}</td>
+                <td>{member.schoolName}</td>
+                <td>{member.departmentName}</td>
+                <td>{member.memberClassification}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </Fragment>
   );
 };
 
