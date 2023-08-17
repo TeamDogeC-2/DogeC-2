7de5e077f11281455cb21be7b99a29c426620956
Trophy198 <spsp8426@naver.com>
2023-08-17 18:48:07 +0900

    Design : 어드민 페이지 navbar 구조 및 디자인 변경

--- StudentSoup/src/main/frontend/src/pages/admin/AdminNavbar.tsx
+++ StudentSoup/src/main/frontend/src/pages/admin/AdminNavbar.tsx
@@ -1,50 +1,203 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './adminnavbar.scss';
-import { useNavigate } from 'react-router-dom';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import { DesktopHeader, Mobile, MobileHeader } from 'mediaQuery';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight, faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const AdminNavbar = () => {
   const [selectedMenu, setSelectedMenu] = useState<string | null>(null);
-  const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const handleClickMenu = (e: React.MouseEvent) => {
+    e.stopPropagation();
 
-  const handleMenuClick = (menu: string | null) => {
-    if (selectedMenu === menu) {
-      setSelectedMenu(null);
-    } else {
-      setSelectedMenu(menu);
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const searchRef = useRef<HTMLUListElement | null>(null);
+
+  const onCheckClickOutside = (e: MouseEvent) => {
+    if (isMenuOpen && searchRef.current && !searchRef.current.contains(e.target as Node)) {
+      setIsMenuOpen(!isMenuOpen);
     }
   };
+  useEffect(() => {
+    document.addEventListener('mousedown', onCheckClickOutside);
 
-  const handleAddRestaurantClick = () => {
-    navigate('/admin/restaurant');
-  };
+    return () => {
+      document.removeEventListener('mousedown', onCheckClickOutside);
+    };
+  }, [isMenuOpen]);
 
-  const handleSchoolClick = () => {
-    handleMenuClick(null);
-    navigate('/admin/schools');
-  };
-  const handleDepartmentClick = () => {
-    handleMenuClick(null);
-    navigate('/admin/departments');
-  };
-  const handleUserInfoClick = () => {
-    handleMenuClick(null);
-    navigate('/admin');
-  };
   return (
-    <div className="adminpage-navbar">
-      <div onClick={handleUserInfoClick}>회원관리</div>
-      <div onClick={handleSchoolClick}>학교관리</div>
-      <div onClick={handleDepartmentClick}>학과관리</div>
-      <div onClick={() => handleMenuClick('음식점 관리')}>
-        음식점 관리
-        {selectedMenu === '음식점 관리' && (
-          <div className="sub-menu">
-            <div onClick={handleAddRestaurantClick}>음식점 등록</div>
-            <div onClick={() => navigate('/admin/restaurants')}>음식점 조회</div>
+    <>
+      <DesktopHeader>
+        <nav className="navbar-items">
+          <Link to="/" className="navbar-logo-links">
+            <i className="navbar-logo">SOUP</i>
+          </Link>
+          <ul className="nav-menu">
+            <li className="nav-li">
+              <Link to="/admin/satisfaction" className="nav-links">
+                <i>설문지 통계</i>
+              </Link>
+            </li>
+            <li className="nav-li">
+              <Link to="/admin" className="nav-links">
+                <i>회원관리</i>
+              </Link>
+            </li>
+            <li className="nav-li">
+              <Link to="/admin/schools" className="nav-links">
+                <i>학교관리</i>
+              </Link>
+            </li>
+            <li className="nav-li">
+              <Link to="/admin/departments" className="nav-links">
+                <i>학과관리</i>
+              </Link>
+            </li>
+            <li className="nav-li">
+              <Link to="/admin/restaurant" className="nav-links">
+                <i>음식점 추가</i>
+              </Link>
+            </li>
+            <li className="nav-li">
+              <Link to="/admin/restaurants" className="nav-links">
+                <i>음식점 조회</i>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </DesktopHeader>
+      <MobileHeader>
+        <nav className="mobile-navbar-items">
+          <Link to="/" className="navbar-logo-links">
+            <i className="navbar-logo">SOUP</i>
+          </Link>
+          <div className="mobile-nav-menu">
+            {isMenuOpen ? (
+              <FontAwesomeIcon icon={faXmark} className="mobile-nav-menu-icon" />
+            ) : (
+              <FontAwesomeIcon
+                icon={faBars}
+                className="mobile-nav-menu-icon"
+                onMouseDown={handleClickMenu}
+              />
+            )}
           </div>
-        )}
-      </div>
-    </div>
+          <ul
+            ref={searchRef}
+            className={isMenuOpen ? 'mobile-nav-menu-list active' : 'mobile-nav-menu-list'}
+          >
+            <li>
+              <Link to="/admin" className="mobile-nav-link">
+                <div className="mobile-nav-list">
+                  <i className="mobile-nav-listItme">회원관리</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-nav-icons" />
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/admin/schools" className="mobile-nav-link">
+                <div className="mobile-nav-list">
+                  <i className="mobile-nav-listItme">학교관리</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-nav-icons" />
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/admin/departments" className="mobile-nav-link">
+                <div className="mobile-nav-list">
+                  <i className="mobile-nav-listItme">학과관리</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-nav-icons" />
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/admin/restaurant" className="mobile-nav-link">
+                <div className="mobile-nav-list">
+                  <i className="mobile-nav-listItme">음식점 추가</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-nav-icons" />
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/admin/restaurants" className="mobile-nav-link">
+                <div className="mobile-nav-list">
+                  <i className="mobile-nav-listItme">음식점 조회</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-nav-icons" />
+                </div>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </MobileHeader>
+      <Mobile>
+        <nav className="mobile-navbar-items">
+          <Link to="/" className="navbar-logo-links">
+            <i className="navbar-logo">SOUP</i>
+          </Link>
+          <div className="mobile-nav-menu">
+            {isMenuOpen ? (
+              <FontAwesomeIcon icon={faXmark} className="mobile-nav-menu-icon" />
+            ) : (
+              <FontAwesomeIcon
+                icon={faBars}
+                className="mobile-nav-menu-icon"
+                onMouseDown={handleClickMenu}
+              />
+            )}
+          </div>
+          <ul
+            ref={searchRef}
+            className={isMenuOpen ? 'mobile-nav-menu-list active' : 'mobile-nav-menu-list'}
+          >
+            <li>
+              <Link to="/admin" className="mobile-nav-link">
+                <div className="mobile-nav-list">
+                  <i className="mobile-nav-listItme">회원관리</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-nav-icons" />
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/admin/schools" className="mobile-nav-link">
+                <div className="mobile-nav-list">
+                  <i className="mobile-nav-listItme">학교관리</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-nav-icons" />
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/admin/departments" className="mobile-nav-link">
+                <div className="mobile-nav-list">
+                  <i className="mobile-nav-listItme">학과관리</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-nav-icons" />
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/admin/restaurant" className="mobile-nav-link">
+                <div className="mobile-nav-list">
+                  <i className="mobile-nav-listItme">음식점 추가</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-nav-icons" />
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link to="/admin/restaurants" className="mobile-nav-link">
+                <div className="mobile-nav-list">
+                  <i className="mobile-nav-listItme">음식점 조회</i>
+                  <FontAwesomeIcon icon={faAngleRight} className="mobile-nav-icons" />
+                </div>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </Mobile>
+      <Outlet />
+    </>
   );
 };
 
